c3ebbd09b1a0653e7fdab5ff8c9f1ac483dbb07a
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-17 16:53:47 +0100

    Exclude src

--- modules/profile/manifests/localmirror.pp
+++ modules/profile/manifests/localmirror.pp
@@ -10,6 +10,7 @@ class profile::localmirror {
 		location          => 'http://mirror.sal01.datacentred.co.uk:80/mirror/gb.archive.ubuntu.com/ubuntu',
 		release           => 'precise',
 		repos             => 'main restricted',
+		include_src	  => 'false',
 
 	}
 	
@@ -17,6 +18,7 @@ class profile::localmirror {
 		location          => 'http://mirror.sal01.datacentred.co.uk:80/mirror/gb.archive.ubuntu.com/ubuntu',
 		release           => 'precise-updates',
 		repos             => 'main restricted',
+		include_src	  => 'false',
 
 	}
 
