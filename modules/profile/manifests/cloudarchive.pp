a46b4542339e1728932742e227491153d5b1049c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-17 13:19:55 +0100

    Change resource name

--- modules/profile/manifests/cloudarchive.pp
+++ modules/profile/manifests/cloudarchive.pp
@@ -1,4 +1,4 @@
-apt::source { 'ubuntu cloud archive':
+apt::source { 'ubuntu_cloud_archive':
 	location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
 	release           => 'precise-updates/grizzly',
 	repos             => 'main',
