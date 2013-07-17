1276d98a3f6b1e12e199eeac69505a9ac266dbe1
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-17 13:16:52 +0100

    Rename cloudarchive

--- modules/profile/manifests/cloudarchive-apt.pp
+++
@@ -1,6 +0,0 @@
-apt::source { 'ubuntu cloud archive':
-	location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-	release           => 'precise-updates/grizzly',
-	repos             => 'main',
-}
-
