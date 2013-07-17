fd54729eb4145a7bc1d05b1444e08ff97eb923cb
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-17 15:58:20 +0100

    Add key to class

--- modules/profile/manifests/cloudarchive.pp
+++ modules/profile/manifests/cloudarchive.pp
@@ -6,6 +6,9 @@ class profile::cloudarchive {
 		location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
 		release           => 'precise-updates/grizzly',
 		repos             => 'main',
+		key        => 'EC4926EA',
+      		key_server => 'keyserver.ubuntu.com',
+
 	}
 
 }
