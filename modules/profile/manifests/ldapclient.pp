392885a47c71d5f71761af205256cec832d28f21
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-18 16:41:33 +0100

    Convert ldap to hiera

--- modules/profile/manifests/ldapclient.pp
+++ modules/profile/manifests/ldapclient.pp
@@ -1,9 +1,11 @@
 class profile::ldapclient {
 
+	$ldapbasedn = hiera(ldap_base_dn)
+	$ldapuri = hiera(ldap_uri)
+
 	class { 'ldap':
-		# FIXME Use hiera for both of these
-		base => 'dc=sal01,dc=datacentred,dc=co,dc=uk',
-		uri  => 'ldap://10.1.5.10',
+		base => $ldapbasedn,
+		uri  => $ldapuri,
 	}
 
 }
