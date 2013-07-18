4f38743ef935e76512143c039f2ac79a17abf15c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-18 16:52:10 +0100

    Convert localmirror to hiera

--- modules/profile/manifests/localmirror.pp
+++ modules/profile/manifests/localmirror.pp
@@ -3,9 +3,6 @@ class profile::localmirror {
 	$mirrorserver = hiera(ubuntu_mirror_server)
 	$mirrorpath = hiera(ubuntu_mirror_path)
 
-# FIXME convert to Hiera
-# Use seperate hiera values for mirror server and mirror path
-
 	class { 'apt': 
 		purge_sources_list => true,
 		purge_sources_list_d => true,
@@ -21,7 +18,7 @@ class profile::localmirror {
 	}
 	
 	apt::source { 'local_precise_updates_mirror':
-		location          => 'http://mirror.sal01.datacentred.co.uk:80/mirror/gb.archive.ubuntu.com/ubuntu',
+		location          => "$mirrorserver/$mirrorpath",
 		release           => 'precise-updates',
 		repos             => 'main restricted',
 		include_src	  => false,
