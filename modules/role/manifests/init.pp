6775dc8ea8960876ee317954978674f260ba3512
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-18 08:05:26 +0100

    Add openstack roles to roles

--- modules/role/manifests/init.pp
+++ modules/role/manifests/init.pp
@@ -5,3 +5,15 @@ class role::generic {
 class role::dev inherits role::generic {
 	include profile::dev
 }
+
+class role::os-controller inherits role::generic {
+	include profile::openstack-controller
+}
+
+class role::os-network-controller inherits role::generic {
+	include profile::os-network-controlller
+}
+
+class role::os-compute inherits role::generic
+	include profile::os-compute
+}
