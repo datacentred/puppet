9f6657211a67571c06d1e222f5c94207dd943376
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-18 12:44:47 +0100

    Add start of git master class

---
+++ modules/profile/manifests/git-master.pp
@@ -0,0 +1,11 @@
+class profile::git-master {
+
+	package { 'git' 
+		ensure => installed,
+	}
+
+# FIXME need to ensure our post commit hooks are also in place on the master
+# This will currently be a hook to deploy if change is in dev branch and a hook to push upstream to github
+# This will also need keys installing to do git push to github
+
+}
