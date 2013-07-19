c3af996fa6b4a517b85c17e78540beb0923e3faa
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-07-19 16:50:11 +0100

    Move mysql root password to hiera

--- modules/profile/manifests/mysqlserver.pp
+++ modules/profile/manifests/mysqlserver.pp
@@ -1,7 +1,9 @@
 class profile::mysqlserver {
 	
+	$mysqlrootpw = hiera(mysql_root_pw)
+
 	class { 'mysql::server':
-		config_hash => { 'root_password' => 'foo' }
+		config_hash => { 'root_password' => $mysqlrootpw }
 	}
 
 } 
