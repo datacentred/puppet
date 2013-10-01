222d8191193e01b98c81394f6829975096d80abe
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-01 08:58:13 +0100

    Add users

---
+++ modules/profile/manifests/users.pp
@@ -0,0 +1,15 @@
+class profile::users {
+	
+	group { "devops":
+		ensure => present;
+		gid => 1000;
+	}
+
+	user { "mattj":
+		ensure => present;
+		managehome => true;
+		shell => '/bin/bash';
+		gid => devops;
+		uid => 1000;
+	}
+
