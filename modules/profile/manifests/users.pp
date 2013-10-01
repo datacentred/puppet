4b754f78e03439da144c3557a6e9c9d4dcd012ce
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-01 11:16:24 +0100

    Change to virtual users module

--- modules/profile/manifests/users.pp
+++ modules/profile/manifests/users.pp
@@ -1,34 +1,15 @@
 define profile::users (
-	
-	$shell = '/bin/blah'
 
-) {
+include userlist
+
 	group { "devops":
 		ensure => present,
 		gid => 1000,
 	}
 
-	user { "mattj":
-		ensure => present,
-		managehome => true,
-		groups => devops,
-		uid => 1000,
-	}
-
-	user { "nick":
-		ensure => present,
-		managehome => true,
-		shell => '/bin/bash',
-		groups => devops,
-		uid => 1001,
-	}
-
-	user { "dariush":
-		ensure => present,
-		managehome => true,
-		shell => '/bin/bash',
-		groups => devops,
-		uid => 1002
-	}
+	realize (
+		Users-virtual::Localuser["mattj"],
+	)
+	
 }
 
