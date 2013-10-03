c907bf1e721774c40c497a7d49d2a74f84f94570
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-03 17:16:52 +0100

    Add password field

--- modules/users/manifests/userlist.pp
+++ modules/users/manifests/userlist.pp
@@ -7,7 +7,7 @@ include users::virtual
 	@users::virtual::account { 'matt':
 		uid	=>  '1001',
 		gid	=>  '1000',
-		pass	=>  '',
+		pass	=>  '$6$qplx30n1$UyzxF3zIRuAm8cNtHtFL5V59HuezXzE3FStG8ermMrTsrowW5ACk6lGsn4GU2y0qTCcq0m13CYPltAY9X8v3i1',
 		sshkey  =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqHgq7htvOfOyNkZ8i505A6bEib9FiMNZGejce63jwo3SboRc1v25KQiXdRkuMIhLNZCgKM7aSErIZoPQoNxxHO1qvGsuOT3yld0jqXZHYp7L8To75y5gDp911N8enKVC3GMduPeXA8HH7RhNlPd6fUWmgmp4tBbd1iKnIZZDJ74P0mhKY47jbLV0SyvBli62WcqjKLH6DIgYPx73w31zti+HJCpPpUhzx19saJM2LbwGZ6HUL8FegXhIgcsV9vsB67tGZO/c2KFl+fRQgpYUTi6/17A19KbQP1GvYSiKb2uNhvYxwnU4D7zSZtlN7oXXiZ0xeI9MEExT24xd+sMmV',
 	}
 
