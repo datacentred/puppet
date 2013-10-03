4ce212421feecd234025267e9388f8a982e71fce
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-03 16:19:15 +0100

    Add comments

--- modules/profile/manifests/sshconfig.pp
+++ modules/profile/manifests/sshconfig.pp
@@ -1,3 +1,6 @@
+# This uses the custom types from the augueasproviders module
+# so that needs to be installed on the puppetmaster for this to work.
+
 class profile::sshconfig {
 
 	sshd_config { "AllowGroups":
