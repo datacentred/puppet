5a7eeb798a4773af4eb30872bdc85a8b7aedafd2
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-03 19:46:41 +0100

    Nullmailer class

---
+++ modules/profile/manifests/mail.pp
@@ -0,0 +1,13 @@
+class profile::mail {
+
+	smarthostuser = hiera('smarthostuser')
+	smarthostpass = hiera('smarthostpass')
+	smarthost    = hiera('smarthost')
+	sysmailaddress = hiera('sysmailaddress')
+
+	class {'nullmailer':
+    		adminaddr => "$sysmailaddress"
+		remoterelay => "$smarthost"
+		remoteopts => "--user=$smarthostuser --pass=$smarthostpass"
+	}
+}
