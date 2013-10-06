aed72a6239c914aca410a153e5b01646b2baaa7b
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-06 10:37:13 +0100

    add args to virtual repo

--- modules/repos/manifests/init.pp
+++ modules/repos/manifests/init.pp
@@ -1,12 +1,14 @@
 class repos::virtual {
 	
-	define repo ($location,$release,$repos) {
+	define repo ($location,$release,$repos,$key="",$key_server="") {
 
 		apt::source { "$title":
                 	location          => "$location",
                 	release           => "$release",
                 	repos             => "$repos",
                 	include_src       => false,
+			key		  => $key,
+			key_server	  => $key_server,
 		}
 	}
 
