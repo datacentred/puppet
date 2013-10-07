6300244dba498aa14c2ecb6d22da4976ef059e9f
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-07 11:46:58 +0100

    Sort out formatting

--- modules/profile/manifests/ceph.pp
+++ modules/profile/manifests/ceph.pp
@@ -1,24 +1,24 @@
 class profile::ceph {
 
-	user { "cephadmin":
-		comment => 'Ceph user created by Puppet',
-		ensure => 'present',
-		managehome => 'true',
-		shell => '/bin/bash',
-	}
+    user { "cephadmin":
+        comment => 'Ceph user created by Puppet',
+        ensure => 'present',
+        managehome => 'true',
+        shell => '/bin/bash',
+    }
 
-	file { "/home/cephadmin/.ssh":
-		ensure => 'directory',
-		require => User['cephadmin'],
-		owner => 'cephadmin',
-		mode => '0700',
-	}
+    file { "/home/cephadmin/.ssh":
+        ensure => 'directory',
+        require => User['cephadmin'],
+        owner => 'cephadmin',
+        mode => '0700',
+    }
 
-	file { "/etc/sudoers.d/ceph":
-		mode => 440,
-		owner => root,
-		group => root,
-		source => "puppet:///modules/profile/ceph/sudoers.ceph"
-	}
+    file { "/etc/sudoers.d/ceph":
+        mode => 440,
+        owner => root,
+        group => root,
+        source => "puppet:///modules/profile/ceph/sudoers.ceph"
+    }
 
 }
