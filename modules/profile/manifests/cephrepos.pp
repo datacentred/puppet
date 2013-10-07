261aaf5e3a0d109b032d103fb4f94b252ddb88b0
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-07 20:30:00 +0100

    Fix formatting

--- modules/profile/manifests/cephrepos.pp
+++ modules/profile/manifests/cephrepos.pp
@@ -4,6 +4,6 @@ class profile::cephrepos {
 # do something like check if Ceph is already installed
 # and then install the appropriate repo
 
-	realize (Repos::Virtual::Repo['ceph_c_mirror'])
+  realize (Repos::Virtual::Repo['ceph_c_mirror'])
 
-}	
+}
