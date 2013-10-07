bd4eb8b729118517e715ac404c3c723d2b56b976
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-07 11:46:17 +0100

    Sort out formatting

--- modules/profile/manifests/baserepos.pp
+++ modules/profile/manifests/baserepos.pp
@@ -1,14 +1,14 @@
 class profile::baserepos {
 
-	include profile::dpkg
-	include repos::repolist
+    include profile::dpkg
+    include repos::repolist
 
-	class { 'apt': 
-		purge_sources_list   => true,
-		purge_sources_list_d => true,
-	}
+    class { 'apt':
+        purge_sources_list   => true,
+        purge_sources_list_d => true,
+    }
 
-	Repos::Virtual::Repo <| tag == baserepos |>
+    Repos::Virtual::Repo <| tag == baserepos |>
 
 }
 
