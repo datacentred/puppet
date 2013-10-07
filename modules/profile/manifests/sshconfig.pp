289a206b66278f0d69f40f5c938e8ad560ef2f67
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-07 20:43:33 +0100

    Typo

--- modules/profile/manifests/sshconfig.pp
+++ modules/profile/manifests/sshconfig.pp
@@ -3,23 +3,22 @@
 
 class profile::sshconfig {
 
-	service { ssh: 
-		ensure => true,
-		enable => true,
-		hasrestart => true
-	}
+  service { 'ssh':
+    ensure     => true,
+    enable     => true,
+    hasrestart => true
+  }
 
-	sshd_config { "AllowGroups":
-		ensure => present,
-		value => "sysadmin",
-		notify => Service["ssh"]
-	}
+  sshd_config { 'AllowGroups':
+    ensure => present,
+    value  => 'sysadmin',
+    notify => Service['ssh']
+  }
 
-	sshd_config { "PermitRootLogin":
-		ensure => present,
-		value => "no",
-		notify => Service["ssh"]
-	}
-
+  sshd_config { 'PermitRootLogin':
+    ensure => present,
+    value  => 'no',
+    notify => Service['ssh']
+  }
 
 }
