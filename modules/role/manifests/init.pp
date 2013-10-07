c5936e6384b089a00daa3dd1d2218d0b5fc44101
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-07 18:33:55 +0100

    Fix formatting

--- modules/role/manifests/init.pp
+++ modules/role/manifests/init.pp
@@ -1,19 +1,19 @@
 class role::generic {
-	include profile::base
+  include profile::base
 }
 
 class role::cephnode inherits role::generic {
-	include profile::cephnode
+  include profile::cephnode
 }
 
 class role::os-controller inherits role::generic {
-	include profile::openstack-controller
+  include profile::openstack-controller
 }
 
 class role::os-network-controller inherits role::generic {
-	include profile::os-network-controlller
+  include profile::os-network-controlller
 }
 
 class role::os-compute inherits role::generic {
-	include profile::os-compute
+  include profile::os-compute
 }
