6e979c3d4aefeaf3734aeccf4538de2a58f9902c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-07 18:31:38 +0100

    Fix formatting

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,35 +1,35 @@
 class users::virtual {
-	define account ($uid,$gid,$pass="",$sshkey="") {
+  define account ($uid,$gid,$pass="",$sshkey="") {
 
-		user { $title:
-			ensure 		=> "present",
-			uid		=> $uid,
-			gid 		=> $gid,
-			shell 		=> "/bin/bash",
-			home 		=> "/home/$title",
-			comment 	=> $realname,
-			password	=> $pass,
-			managehome 	=> true,
-		}
+    user { $title:
+      ensure     => 'present',
+      uid        => $uid,
+      gid        => $gid,
+      shell      => '/bin/bash',
+      home       => "/home/$title",
+      comment    => $realname,
+      password   => $pass,
+      managehome => true,
+    }
 
-		if ( $sshkey != "" ) {
-			ssh_authorized_key { $title:
-				ensure 	=> "present",
-				type	=> "ssh-rsa",
-				key	=> "$sshkey",
-				user	=> "$title",
-				require => File["/home/$title/.ssh"],
-				name	=> "$title",
-			}
-		}
+    if ( $sshkey != "" ) {
+      ssh_authorized_key { $title:
+        ensure  => 'present',
+        type    => 'ssh-rsa',
+        key     => "$sshkey",
+        user    => "$title",
+        require => File["/home/$title/.ssh"],
+        name    => "$title",
+      }
+    }
 
-		file { "/home/$title/.ssh":
-			path 	=> "/home/$title/.ssh",
-			ensure 	=> directory,
-			owner	=> "$uid",
-			group	=> "$gid",
-			mode	=> '0700',
-			require => User["$title"],
-		}
-	}
+    file { "/home/$title/.ssh":
+      path    => "/home/$title/.ssh",
+      ensure  => directory,
+      owner   => "$uid",
+      group   => "$gid",
+      mode    => '0700',
+      require => User["$title"],
+    }
+  }
 }
