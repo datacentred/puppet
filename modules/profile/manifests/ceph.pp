ec4e866cdb168695c73d65c95f5342105c70ce6f
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-07 12:57:34 +0100

    Fix formatting

--- modules/profile/manifests/ceph.pp
+++ modules/profile/manifests/ceph.pp
@@ -1,23 +1,23 @@
 class profile::ceph {
 
     user { 'cephadmin':
-        comment => 'Ceph user created by Puppet',
-        ensure => 'present',
+        comment    => 'Ceph user created by Puppet',
+        ensure     => 'present',
         managehome => 'true',
-        shell => '/bin/bash',
+        shell      => '/bin/bash',
     }
 
     file { '/home/cephadmin/.ssh':
-        ensure => 'directory',
+        ensure  => 'directory',
         require => User['cephadmin'],
-        owner => 'cephadmin',
-        mode => '0700',
+        owner   => 'cephadmin',
+        mode    => '0700',
     }
 
     file { '/etc/sudoers.d/ceph':
-        mode => 440,
-        owner => root,
-        group => root,
+        mode   => 440,
+        owner  => root,
+        group  => root,
         source => 'puppet:///modules/profile/ceph/sudoers.ceph'
     }
 
