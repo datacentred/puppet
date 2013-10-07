6b7848e93e77f1fd05d962a835c378a226df18f1
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-07 11:43:31 +0100

    Sort out formatting

--- modules/profile/manifests/admins.pp
+++ modules/profile/manifests/admins.pp
@@ -2,11 +2,11 @@ class profile::admins {
 
 include users::userlist
 
-	group { "sysadmin":
-		ensure => present,
-		gid => 1000,
-	}
-	
-	Users::Virtual::Account <| gid == '1000' |>
+    group { 'sysadmin':
+        ensure => present,
+        gid    => 1000,
+    }
+
+    Users::Virtual::Account <| gid == '1000' |>
 }
 
