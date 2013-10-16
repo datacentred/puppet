dd9b4f67b68f032241e4a1db7f57618c8f286154
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-16 15:48:52 +0100

    Test

---
+++ modules/profile/manifests/adminstest.pp
@@ -0,0 +1,12 @@
+class profile::admins {
+
+include dc_users::userlist
+
+    group { 'sysadmin':
+        ensure => present,
+        gid    => 1000,
+    }
+
+    Dc_users::Virtual::Account <| gid == '1000' |>
+}
+
