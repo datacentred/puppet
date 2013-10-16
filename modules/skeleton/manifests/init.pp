0e004954d161170ecb88a4ded949668ae15a4e6e
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-16 15:41:51 +0100

    Add skeleton as a template

---
+++ modules/skeleton/manifests/init.pp
@@ -0,0 +1,13 @@
+# Class: skeleton
+#
+# This is a template
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
