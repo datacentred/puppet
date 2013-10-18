6ce6ac65b5a1e4e465b454fb458071277f7b008a
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-18 15:49:34 +0100

    Fix typo

--- modules/dc_mirrors/manifests/mirrorlist.pp
+++ modules/dc_mirrors/manifests/mirrorlist.pp
@@ -5,7 +5,7 @@ include dc_mirrors::virtual
   $ubuntu_mirror_url = hiera(ubuntu_mirror_url)
 
   @dc_mirrors::virtual::mirror { 'ubuntu_precise_mirror':
-    location   => "$ubuntu_mirror_url",
+    mirror   => "$ubuntu_mirror_url",
     release    => 'precise',
     components => 'main restricted universe multiverse',
     tag        => basemirrors
