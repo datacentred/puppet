67067e0d7716b10f310b15eb75ad2ab431ad29aa
Nick Jones <nick.jones@datacentred.co.uk>
2013-10-24 17:01:38 +0100

    Cleanup and addition of profile class for HP Blades

--- modules/dc_profile/manifests/virtualbox.pp
+++ modules/dc_profile/manifests/virtualbox.pp
@@ -3,7 +3,8 @@ class dc_profile::virtualbox {
   include dc_profile::virtualboxrepo
 
   package { 'virtualbox-4.3' :
-    ensure => installed, require => Dc_repos::Virtual::Repo['local_virtualbox_mirror']
+    ensure => installed,
+    require => Dc_repos::Virtual::Repo['local_virtualbox_mirror']
   }
 
 }
