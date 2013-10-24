bfa17fd0988f4cc87a50b63f9f4b38d03f3a3bcb
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-24 13:41:45 +0100

    Fix formatting

--- modules/dc_mirrors/manifests/mirrorlist.pp
+++ modules/dc_mirrors/manifests/mirrorlist.pp
@@ -4,11 +4,25 @@ include dc_mirrors::virtual
 
   $ubuntu_mirror_url = hiera(ubuntu_mirror_url)
 
-  @dc_mirrors::virtual::mirror { 'ubuntu_precise_mirror':
+  #  @dc_mirrors::virtual::mirror { 'ubuntu_precise_mirror':
+  # mirrorurl  => "$ubuntu_mirror_url",
+  # release    => 'precise',
+  # components => ['main', 'restricted', 'universe', 'multiverse'],
+  # tag        => basemirrors
+  #}
+
+  $ubuntumirrors = {
+    'ubuntu_precise_mirror'          => { release => 'precise',},
+    'ubuntu_precise_security_mirror' => { release => 'precise-security',},
+    'ubuntu_precise_updates_mirror'  => { release => 'precise-updates',},
+  }
+
+  $ubuntumirrorsdefaults = {
     mirrorurl  => "$ubuntu_mirror_url",
-    release    => 'precise',
     components => ['main', 'restricted', 'universe', 'multiverse'],
     tag        => basemirrors
   }
+
+  create_resources("@dc_mirrors::virtual::mirror", $ubuntumirrors, $defaults)
 
 }
