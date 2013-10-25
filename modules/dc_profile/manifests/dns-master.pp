138c4ef2401be8782ef4ddf6b1db7024922e763a
Nick Jones <nick.jones@datacentred.co.uk>
2013-10-25 19:11:27 +0100

    Added DNS submodule from DC fork, added basic DNS master profile class.

--- modules/dc_profile/manifests/dns-master.pp
+++ modules/dc_profile/manifests/dns-master.pp
@@ -2,4 +2,11 @@ class dc_profile::dns-master {
 
   include dns
 
+  dns::zone { 'sal01.datacentred.co.uk':
+    soa     => "$fqdn",
+    soaip   => "$ipaddress_eth0",
+    ttl     => '10800',
+    refresh => '86400',
+  }
+
 }
