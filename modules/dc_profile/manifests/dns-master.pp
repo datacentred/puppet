16e2e549bdb933f87cb339154772cc9376a99e17
Nick Jones <nick.jones@datacentred.co.uk>
2013-10-25 18:53:24 +0100

    Removed Foreman DNS submodule

--- modules/dc_profile/manifests/dns-master.pp
+++ modules/dc_profile/manifests/dns-master.pp
@@ -1,18 +1,5 @@
 class dc_profile::dns-master {
-  include dc_bind
-  dc_bind::server::conf { '/etc/bind/named.conf':
-    directory                 => '/var/cache/bind',
-    listen_on_addr            => [ 'any' ],
-    listen_on_v6_addr         => [ 'any' ],
-    forwarders                => [ '8.8.8.8', '8.8.4.4' ],
-    allow_query               => [ 'localnets' ],
-    allow_recursion           => [ '10.0.1.0/24', '10.1.1.0/24', '10.1.10.0/24' ],
-    zones                     => {
-    'sal01.datacentred.co.uk' => [
-      'type master',
-      'file "/var/lib/bind/db.sal01.datacentred.co.uk"',
-      'allow-update { key "foreman"; }'
-    ]
-    },
-  }
+
+  include dns
+
 }
