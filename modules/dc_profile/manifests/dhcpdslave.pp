520cdc7f0d1487008ab3e9fd1fdd1d82b2e8dfd3
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-28 10:02:50 +0000

    Add dhcpd changes

---
+++ modules/dc_profile/manifests/dhcpdslave.pp
@@ -0,0 +1,34 @@
+class dc_profile::dhcpdslave {
+
+  $localtimeservers = hiera(localtimeservers)
+  $nameservers      = hiera_array(nameservers)
+  $pxeserver        = hiera(pxeserver)
+  $masterserver_ip   = hiera(dhcpdmasterip)
+
+  class { 'dhcp':
+    dnsdomain => [
+      'sal01.datacentred.co.uk',
+      '0.0.10.in-addr.arpa',
+      ],
+    nameservers  => [$nameservers],
+    ntpservers   => ["$localtimeservers"],
+    interfaces   => ['eth0'],
+    #dnsupdatekey => "/etc/bind/keys.d/$ddnskeyname",
+    #require      => Bind::Key[ $ddnskeyname ],
+    pxeserver    => "$pxeserver",
+    pxefilename  => 'pxelinux.0',
+  }
+
+  class { dhcp::failover:
+    role               => "secondary",
+    peer_address => $masterserver_ip,
+  }
+
+  dhcp::pool { 'test.sal01.datacentred.co.uk':
+    network => '10.0.1.0',
+    mask    => '255.255.255.0',
+    range   => '10.0.1.11 10.0.1.249',
+    gateway => '10.0.1.1',
+  }
+
+}
