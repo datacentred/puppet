88c56960119537bc760416d82d526fb5f194ab23
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-10-29 12:33:55 +0000

    Change file names

--- modules/dc_dhcpdpools/manifests/poollist.pp
+++ modules/dc_dhcpdpools/manifests/poollist.pp
@@ -1,13 +1,34 @@
-# This is the class containing the list of unrealized virtual dhcpd pools, don't use this class directly.
+# This is the class containing the list of unrealized virtual dhcp pools
+# don't use this class directly.
 
 class dc_dhcpdpools::poollist {
 
 include dc_dhcpdpools::virtual
 
-  @dc_dhcpdpools::virtual::dhcpdpool { 'test':
-    network => '10.0.1.0',
+# FIXME Guest wireless network
+# FIXME Seperate wireless and wired VLAN's for office
+
+  @dc_dhcpdpools::virtual::dhcpdpool { 'office':
+    network => '10.1.2.0',
     mask    => '255.255.255.0',
-    range   => '10.0.1.11 10.0.1.249',
-    gateway => '10.0.1.1'
+    range   => '10.0.2.11 10.0.2.249',
+    gateway => '10.1.2.1',
+    tag     => 'vlan2'
+  }
+
+  @dc_dhcpdpools::virtual::dhcpdpool { 'rack-infr':
+    network => '10.1.6.0',
+    mask    => '255.255.255.0',
+    range   => '10.1.6.11 10.0.1.249',
+    gateway => '10.1.6.1',
+    tag     => 'vlan6'
+  }
+
+  @dc_dhcpdpools::virtual::dhcpdpool { 'production-netserv':
+    network => '10.1.10.0',
+    mask    => '255.255.255.0',
+    range   => '10.1.10.11 10.0.1.249',
+    gateway => '10.1.10.1',
+    tag     => 'vlan10'
   }
 }
