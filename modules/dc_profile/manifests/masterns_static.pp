ef0a4619a7e22a4a4638deb0d86b0d3319fab80b
Nick Jones <nick.jones@datacentred.co.uk>
2013-10-31 12:51:44 +0000

    Amended eth0 as bond0

--- modules/dc_profile/manifests/masterns_static.pp
+++ modules/dc_profile/manifests/masterns_static.pp
@@ -2,14 +2,14 @@ class dc_profile::masterns_static {
   
   class { "network::interfaces":
     interfaces => {
-      "eth0" => {
+      "bond0" => {
         "method"  => "static",
         "address" => "10.10.192.250",
         "netmask" => "255.255.255.0",
         "gateway" => "10.10.192.1",
       }
     },
-    auto => ["eth0"],
+    auto => ["bond0"],
   }
 
 }
