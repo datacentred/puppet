0ba57d90e0d1b53b88843cc75679b17d5df92dbd
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-01 14:04:11 +0000

    Switch to using custom foreman_proxy module

--- modules/dc_profile/manifests/foreman_proxy.pp
+++ modules/dc_profile/manifests/foreman_proxy.pp
@@ -1,11 +1,7 @@
 class dc_profile::foreman_proxy {
 
-  realize Dc_repos::Virtual::Repo['local_foreman_mirror']
-
-  package { 'foreman-proxy':
-    ensure  => installed,
-    require => Dc_repos::Virtual::Repo['local_foreman_mirror'],
-    name    => 'foreman-proxy',
+  class { 'dc_foreman_proxy' :
+    use_dns => true,
   }
 
 }
