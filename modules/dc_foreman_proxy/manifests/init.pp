84a0e173bf5cf6ff2215cff1947514850e17baac
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-01 14:41:55 +0000

    Add file ownership change

--- modules/dc_foreman_proxy/manifests/init.pp
+++ modules/dc_foreman_proxy/manifests/init.pp
@@ -6,12 +6,15 @@
 #
 # Actions:
 #
-# Requires:
+# Requires: puppetlabs/stdlib
 #
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class dc_foreman_proxy ( $use_dns = "false", $use_dhcp = "false", $dns_key= "/etc/bind/rndc.key") {
+class dc_foreman_proxy ( $use_dns = false, $use_dhcp = false, $dns_key= '/etc/bind/rndc.key') {
+
+  validate_bool($use_dns)
+  validate_bool($use_dhcp)
 
   realize Dc_repos::Virtual::Repo['local_foreman_mirror']
 
@@ -26,6 +29,15 @@ class dc_foreman_proxy ( $use_dns = "false", $use_dhcp = "false", $dns_key= "/et
     group   => root,
     mode    => '0640',
     content => template('dc_foreman_proxy/settings.yml.erb');
+  }
+
+  if $use_dns == true {
+    file { '/etc/bind/rndc.key':
+      ensure  => present,
+      require => Package['foreman-proxy'],
+      owner   => root,
+      group   => foreman-proxy,
+    }
   }
 
 }
