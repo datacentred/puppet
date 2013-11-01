c86799570c8619509e93e0ff3d36f41c3042c3d6
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-01 14:00:41 +0000

    Fix formatting

---
+++ modules/dc_foreman_proxy/manifests/init.pp
@@ -0,0 +1,36 @@
+# Class: dc_foreman_proxy
+#
+# Foreman_proxy
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class dc_foreman_proxy ( $use_dns = "", $use_dhcp = "", $dns_key= "") {
+
+  $use_dns  = false
+  $use_dhcp = false
+  $dns_key  = '/etc/bind/rndc.key'
+
+  realize Dc_repos::Virtual::Repo['local_foreman_mirror']
+
+  package { 'foreman-proxy':
+    ensure  => installed,
+    require => Dc_repos::Virtual::Repo['local_foreman_mirror'],
+    name    => 'foreman-proxy',
+  }
+
+  file { '/etc/foreman-proxy/settings.yml':
+    owner   => root,
+    group   => root,
+    mode    => '0640',
+    content => template('dc_foreman_proxy/settings.yml.erb');
+  }
+
+}
+
