519f0207aa1c9ef0584ccda72116deabcbab44d0
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-01 11:02:11 +0000

    First pass at foreman proxy for DNS

---
+++ modules/dc_profile/manifests/f_proxy.pp
@@ -0,0 +1,16 @@
+class dc_profile::f_proxy {
+
+  class { 'foreman_proxy':
+    repo                => 'stable',
+    ssl                 => 'false',
+    trusted_hosts       => [],
+    manage_sudoersd     => 'false',
+    puppetca            => 'false',
+    tftp                => 'false',
+    dhcp                => 'false',
+    dns                 => 'true',
+    keyfile             => '/etc/bind/rndc.key',
+    register_in_foreman => 'false',
+  }
+
+}
