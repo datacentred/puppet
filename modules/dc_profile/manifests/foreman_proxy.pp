c2c31223200d98c817188c8651f37bc7caf598dc
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-01 13:15:54 +0000

    New foreman proxy class

--- modules/dc_profile/manifests/foreman_proxy.pp
+++ modules/dc_profile/manifests/foreman_proxy.pp
@@ -3,9 +3,9 @@ class dc_profile::foreman_proxy {
   realize Dc_repos::Virtual::Repo['local_foreman_mirror']
 
   package { 'foreman-proxy':
+    ensure  => installed,
     require => Dc_repos::Virtual::Repo['local_foreman_mirror'],
     name    => 'foreman-proxy',
-    ensure  => installed,
   }
 
 }
