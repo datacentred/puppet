67762e793d18121d1312301e6ee20c9120a10d10
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-06 16:23:43 +0000

    Add directory handling

--- modules/dc_profile/manifests/tftpserver.pp
+++ modules/dc_profile/manifests/tftpserver.pp
@@ -2,7 +2,12 @@ class dc_profile::tftpserver {
 
   $tftpdir = hiera('tftpdir')
 
+  file { "$tftpdir":
+    ensure => directory
+  }
+
   class { 'tftp':
+    require   => File["$tftpdir"],
     directory => "$tftpdir",
     inetd     => false,
   }
