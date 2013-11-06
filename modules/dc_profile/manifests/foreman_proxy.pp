0ea476f0939164be8bd031e266d497a76c204ebf
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-06 16:30:37 +0000

    Add handling for tftp

--- modules/dc_profile/manifests/foreman_proxy.pp
+++ modules/dc_profile/manifests/foreman_proxy.pp
@@ -2,12 +2,15 @@ class dc_profile::foreman_proxy {
 
   $omapi_key    = hiera(omapi_key)
   $omapi_secret = hiera(omapi_secret)
+  $tftproot     = hiera(tftproot)
 
   class { 'dc_foreman_proxy' :
     use_dns      => true,
     use_dhcp     => true,
+    use_tftp     => true,
     omapi_key    => "$omapi_key",
-    omapi_secret => "$omapi_secret"
+    omapi_secret => "$omapi_secret",
+    tftproot     => "$tftproot"
   }
 
 }
