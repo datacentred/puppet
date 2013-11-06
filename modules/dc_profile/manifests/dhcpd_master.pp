7053717e4672a5f00d9b4d47f31e4e2fd6db7ca9
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-06 13:36:59 +0000

    Add omapi handling

--- modules/dc_profile/manifests/dhcpd_master.pp
+++ modules/dc_profile/manifests/dhcpd_master.pp
@@ -4,17 +4,22 @@ class dc_profile::dhcpd_master {
   $nameservers      = hiera(nameservers)
   $pxeserver        = hiera(pxeserver)
   $slaveserver_ip   = hiera(dhcpdslaveip)
+  $omapi_key        = hiera(omapi_key)
+  $omapi_secret     = hiera(omapi_secret)
+
 
   class { 'dhcp':
     dnsdomain => [
       'sal01.datacentred.co.uk',
       '0.0.10.in-addr.arpa',
       ],
-    nameservers         => [$nameservers],
-    ntpservers          => [$localtimeservers],
-    interfaces          => ['bond0'],
-    pxeserver           => "$pxeserver",
-    pxefilename         => 'pxelinux.0',
+    nameservers  => [$nameservers],
+    ntpservers   => [$localtimeservers],
+    interfaces   => ['bond0'],
+    pxeserver    => "$pxeserver",
+    pxefilename  => 'pxelinux.0',
+    omapi_key    => "$omapi_key",
+    omapi_secret => "$omapi_secret"
   }
 
   dhcp::pool { 'platform-services':
