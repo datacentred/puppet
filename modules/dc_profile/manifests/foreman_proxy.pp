06e7b403a2dbf6e142a60939a78e642c101140c1
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-11-06 16:06:51 +0000

    Remove omapi variables and put them in profile

--- modules/dc_profile/manifests/foreman_proxy.pp
+++ modules/dc_profile/manifests/foreman_proxy.pp
@@ -1,8 +1,13 @@
 class dc_profile::foreman_proxy {
 
+  $omapi_key    = hiera(omapi_key)
+  $omapi_secret = hiera(omapi_secret)
+
   class { 'dc_foreman_proxy' :
-    use_dns  => true,
-    use_dhcp => true,
+    use_dns      => true,
+    use_dhcp     => true,
+    omapi_key    => "$omapi_key",
+    omapi_secret => "$omapi_secret"
   }
 
 }
