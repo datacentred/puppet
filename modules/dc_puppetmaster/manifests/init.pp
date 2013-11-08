2b32cd80658f7e7bfb0a0936242d3c24ce5f2078
Simon Murray <simon.murray@datacentred.co.uk>
2013-11-08 14:06:08 +0000

    clever install of puppet master service, which restores foreman certs

--- modules/dc_puppetmaster/manifests/init.pp
+++ modules/dc_puppetmaster/manifests/init.pp
@@ -15,14 +15,40 @@
 #
 class dc_puppetmaster {
 
-  exec { 'puppet_master_clean_certs':
-    command => 'rm -rf /var/lib/puppet/ssl',
+  # Save the old agent certificates that have been signed by
+  # the master master.  These must be moved out of the way
+  # so that the puppetmaster-passenger deb creates a new
+  # CA certificate for apache SSL
+  exec { 'puppet_master_save_agent_certs':
+    command => 'mv /var/lib/puppet/ssl /var/lib/puppet/ssl.orig',
     path    => '/bin',
     before  => Package['puppetmaster-passenger'],
   }
 
+  # Install the packages
   package { 'puppetmaster-passenger':
     ensure  => present,
+  }
+
+  # If the puppetmaster-passenger package was installed
+  # then copy the new CA certificates into the agent
+  # certificates folder, delete the new master certificates
+  # and move the agent certificates back into place
+  exec { 'puppet_master_install_master_ca':
+    command     => 'cp -a /var/lib/puppet/ssl/ca /var/lib/puppet/ssl.orig',
+    path        => '/bin',
+    subscribe   => Package['puppetmaster-passenger'],
+    refreshonly => true,
+  } ~>
+  exec { 'puppet_master_delete_master_certs':
+    command     => 'rm -rf /var/lib/puppet/ssl',
+    path        => '/bin',
+    refreshonly => true,
+  } ~>
+  exec { 'puppet_master_restore_agent_certs':
+    command     => 'mv /var/lib/puppet/ssl.orig /var/lib/puppet/ssl',
+    path        => '/bin',
+    refreshonly => true,
   }
 
   # Seems to collapse due to not being able to chmod
