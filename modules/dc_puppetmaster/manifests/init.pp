40e2d56c313221f47d117d87f35a960f2f91b663
Simon Murray <simon.murray@datacentred.co.uk>
2013-11-11 11:01:29 +0000

    just cut ties with the new puppet master CA

--- modules/dc_puppetmaster/manifests/init.pp
+++ modules/dc_puppetmaster/manifests/init.pp
@@ -15,12 +15,14 @@
 #
 class dc_puppetmaster {
 
-  # Save the old agent certificates that have been signed by
-  # the master master.  These must be moved out of the way
-  # so that the puppetmaster-passenger deb creates a new
-  # CA certificate for apache SSL
-  exec { 'puppet_master_save_agent_certs':
-    command => 'mv /var/lib/puppet/ssl /var/lib/puppet/ssl.orig',
+  $puppet_ssl_dir = '/var/lib/puppet/ssl'
+
+  # As this host is under the control of another master initially
+  # it will have and old set of certs.  The new CA will not be
+  # generated if these exist.  However, on subsequent runs we already
+  # have valid CA certificates, so move them out of the way for now
+  exec { 'puppet_master_save_certs':
+    command => "mv ${puppet_ssl_dir} ${puppet_ssl_dir}.bak",
     path    => '/bin',
     before  => Package['puppetmaster-passenger'],
   }
@@ -30,37 +32,22 @@ class dc_puppetmaster {
     ensure  => present,
   }
 
-  # If the puppetmaster-passenger package was installed
-  # then copy the new CA certificates into the agent
-  # certificates folder, delete the new master certificates
-  # and move the agent certificates back into place
-  exec { 'puppet_master_install_master_ca':
-    command     => 'cp -a /var/lib/puppet/ssl/ca /var/lib/puppet/ssl.orig',
+  # Okay so if the install did happen we'll have a new CA certificate
+  # and can delete the old stuff, otherwise restore the old CA
+  exec { 'puppet_master_restore_certs':
+    command     => "bash -c 'if [ -e ${puppet_ssl_dir} ]; then \
+                      rm -rf ${puppet_ssl_dir}.bak; \
+                    else \
+                      mv ${puppet_ssl_dir}.bak ${puppet_ssl_dir}; \
+                    fi'",
     path        => '/bin',
-    subscribe   => Package['puppetmaster-passenger'],
-    refreshonly => true,
-  } ~>
-  exec { 'puppet_master_delete_master_certs':
-    command     => 'rm -rf /var/lib/puppet/ssl',
-    path        => '/bin',
-    refreshonly => true,
-  }
-
-  # The initial move is unconditional as we don't know if
-  # the install will happen or not, so we make the move
-  # back unconditional too, but only after the install and
-  # delete steps if executed
-  exec { 'puppet_master_restore_agent_certs':
-    command     => 'mv /var/lib/puppet/ssl.orig /var/lib/puppet/ssl',
-    path        => '/bin',
-    require     => Exec['puppet_master_delete_master_certs'],
+    require     => Package['puppetmaster-passenger'],
   }
 
   # Seems to collapse due to not being able to chmod
   # /var/lib/puppet/reports to 755, being owned by
   # root.  Maybe a bit agressive with the recursion
   # but what the hell, we're on a tight budget
-
   file { '/var/lib/puppet/reports':
     ensure  => directory,
     owner   => 'puppet',
