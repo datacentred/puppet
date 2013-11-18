131cf7cd8ddabca34512fbb6e0f5a7c81baa59a3
Simon Murray <simon.murray@datacentred.co.uk>
2013-11-18 11:25:35 +0000

    puppetdb: commit to run on metal as vagrant isnt up to it

--- modules/dc_java/manifests/init.pp
+++ modules/dc_java/manifests/init.pp
@@ -1,6 +1,11 @@
 #
 class dc_java {
 
+  # puppetdb requires java, but doesn't install it or impose constraints
+  # probably so you can chose a proprietary version if you like.  We just
+  # use OpenJDK for now
+  Class['dc_java'] -> Class['puppetdb']
+
   package { 'default-jre':
     ensure => present,
   }
