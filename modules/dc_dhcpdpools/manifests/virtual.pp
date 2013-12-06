8ffa3e3978cf6a1db5709dac8c827df258bc9824
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-12-06 09:34:43 +0000

    Fix virtual pools

--- modules/dc_dhcpdpools/manifests/virtual.pp
+++ modules/dc_dhcpdpools/manifests/virtual.pp
@@ -1,12 +1,13 @@
 class dc_dhcpdpools::virtual {
 
+
   define dhcpdpool ($network,$mask,$range,$gateway) {
 
     dhcp::pool { "$title":
-      network => "$network",
-      mask    => "$mask",
-      range   => "$range",
-      gateway => "$gateway",
+      network => $network,
+      mask    => $mask,
+      range   => $range,
+      gateway => $gateway,
     }
   }
 }
