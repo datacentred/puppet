3f90fd9304981f6204380a84f2aa7933d9e8a388
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-12-09 11:59:22 +0000

    Add role for platform services master and hpblade

--- modules/dc_role/manifests/init.pp
+++ modules/dc_role/manifests/init.pp
@@ -2,8 +2,15 @@ class dc_role::generic {
   include dc_profile::base
 }
 
-class dc_role::dns_master inherits dc_role::generic {
+class dc_role::hpblade {
+  include dc_profile::hpblade
+}
+
+class dc_role::platformservices_master inherits dc_role::generic {
   include dc_profile::dns_master
+  include dc_profile::dhcp_master
+  include dc_profile::tftpserver
+  include dc_profile::foreman_net_proxy
 }
 
 class dc_role::dns_slave inherits dc_role::generic {
