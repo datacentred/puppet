6b2584e5d67c45f6e94a500d21cf2ebabf23ae54
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-12-10 13:49:56 +0000

    Add per pool handling of pxe config

--- modules/dc_dhcpdpools/manifests/poollist.pp
+++ modules/dc_dhcpdpools/manifests/poollist.pp
@@ -11,11 +11,13 @@ include dc_dhcpdpools::virtual
   }
 
   @dc_dhcpdpools::virtual::dhcpdpool { 'platform_services':
-    network => '10.10.192.0',
-    mask    => '255.255.255.0',
-    range   => '10.10.192.16 10.10.192.247',
-    gateway => '10.10.192.1',
-    tag     => vlan192
+    network     => '10.10.192.0',
+    mask        => '255.255.255.0',
+    range       => '10.10.192.16 10.10.192.247',
+    gateway     => '10.10.192.1',
+    pxeserver   => '10.1.5.10',
+    pxefilename => '/pxelinux.0',
+    tag         => vlan192
   }
 
   @dc_dhcpdpools::virtual::dhcpdpool { 'ipmi':
