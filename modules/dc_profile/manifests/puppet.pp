e01e256daa0471f5a987fc824a83b8e2dae78434
Simon Murray <simon.murray@datacentred.co.uk>
2013-12-12 11:00:25 +0000

    add in generic puppet control of puppet

---
+++ modules/dc_profile/manifests/puppet.pp
@@ -0,0 +1,8 @@
+# All nodes get this class as it allows control over the puppet
+# configuration file, which is important for foreman and its
+# proxies as they need access to the the host's private key
+class dc_profile::puppet {
+
+  class { '::puppet': }
+
+}
