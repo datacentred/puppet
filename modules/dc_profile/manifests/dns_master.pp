25160500b8bee9186872732f32cd538d6c7e8794
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2013-12-12 16:01:31 +0000

    Move configuration out of dns module and into profile

--- modules/dc_profile/manifests/dns_master.pp
+++ modules/dc_profile/manifests/dns_master.pp
@@ -2,6 +2,11 @@ class dc_profile::dns_master {
 
   include dc_dns::dnszone
 
+  class { 'dns':
+    forwarders => hiera(forwarders),
+    recursors  => hiera(client_networks),
+  }
+
   Dc_dns::Virtual::Dnszone <| |>
 
 }
