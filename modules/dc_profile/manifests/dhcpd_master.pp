676ee4ac769bf984a4cad14ecc6831b20f38a6ed
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-02 15:46:03 +0000

    Add DDNS handling

--- modules/dc_profile/manifests/dhcpd_master.pp
+++ modules/dc_profile/manifests/dhcpd_master.pp
@@ -8,6 +8,7 @@ class dc_profile::dhcpd_master {
   $slaveserver_ip   = hiera(dhcpdslaveip)
   $omapi_key        = hiera(omapi_key)
   $omapi_secret     = hiera(omapi_secret)
+  $rndc_key         = hiera(test_rndc_key)
 
 
   class { 'dhcp':
@@ -15,11 +16,16 @@ class dc_profile::dhcpd_master {
       'sal01.datacentred.co.uk',
       '0.0.10.in-addr.arpa',
       ],
-    nameservers                                  => [$nameservers],
-    ntpservers                                   => [$localtimeservers],
-    interfaces                                   => ['bond0'],
-    omapi_key                                    => 'omapi_key',
-    omapi_secret                                 => "$omapi_secret",
+    nameservers  => [$nameservers],
+    ntpservers   => [$localtimeservers],
+    interfaces   => ['bond0'],
+    omapi_key    => 'omapi_key',
+    omapi_secret => "$omapi_secret",
+    ddns         => true,
+  }
+
+  class { dhcp::ddns:
+    key => "$rndc_key",
   }
 
   class { dhcp::failover:
