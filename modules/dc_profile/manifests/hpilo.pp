f424d3d86cbb05e056a800fdb0921573e2798c0b
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-03 16:57:37 +0000

    New hpilo class for testing

--- modules/dc_profile/manifests/hpilo.pp
+++ modules/dc_profile/manifests/hpilo.pp
@@ -1,15 +1,9 @@
 class dc_profile::hpilo {
 
   include stdlib
-  $nameservers = values(hiera(nameservers))
 
   class { 'hpilo':
-    shared      => false,
     dhcp        => true,
-    dns         => $nameservers[0],
-    ilouser     => 'admin',
-    ilouserpass => 'password',
-    autoip      => false,
   }
 }
 
