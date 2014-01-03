ce72fdcd92080c74f5101f068e597e4219ab7f6f
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-03 15:17:39 +0000

    Remove ilo for testing

--- modules/dc_profile/manifests/hpblade.pp
+++ modules/dc_profile/manifests/hpblade.pp
@@ -1,21 +1,10 @@
 class dc_profile::hpblade {
 
-  include stdlib
   include dc_profile::hpsupportrepo
-  $nameservers = values(hiera(nameservers))
 
   package { ['hpacucli', 'cciss-vol-status' ]:
     ensure  => installed,
     require => Dc_repos::Virtual::Repo['local_hpsupport_mirror']
-  }
-
-  class { 'hpilo':
-    shared      => false,
-    dhcp        => true,
-    dns         => $nameservers[0],
-    ilouser     => 'admin',
-    ilouserpass => 'password',
-    autoip      => false,
   }
 }
 
