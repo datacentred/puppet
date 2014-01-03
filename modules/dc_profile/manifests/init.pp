84a3e2c49dd7a3b7caf36a8907166165bb4590df
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-03 11:34:30 +0000

    replace ntp with generic

--- modules/dc_profile/manifests/init.pp
+++ modules/dc_profile/manifests/init.pp
@@ -2,7 +2,7 @@ class dc_profile::base {
 
   include dc_profile::vim
   include dc_profile::admins
-  include dc_profile::ntpclient
+  include dc_profile::ntpgeneric
   include dc_profile::baserepos
   include dc_profile::sudoers
   include dc_profile::rootpw
