9577607b56a887ed6610f6e449c9497206278d97
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-03 15:03:11 +0000

    Add ilo handling

--- modules/dc_profile/manifests/hpblade.pp
+++ modules/dc_profile/manifests/hpblade.pp
@@ -1,11 +1,20 @@
 class dc_profile::hpblade {
 
+  include hpilo
   include dc_profile::hpsupportrepo
 
-  package { ['hpacucli', 'cciss-vol-status']:
+  package { ['hpacucli', 'cciss-vol-status', 'hponcfg' ]:
     ensure  => installed,
     require => Dc_repos::Virtual::Repo['local_hpsupport_mirror']
   }
 
+  class { 'hpilo':
+    require     => Package['hponcfg'],
+    dhcp        => true,
+    dns         => hiera(dnsmaster),
+    ilouser     => 'admin',
+    ilouserpass => 'password',
+    autoip      => false,
+  }
 }
 
