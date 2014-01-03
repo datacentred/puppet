d48061e82de9f523ef75ac90a9734a86da6b7510
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-03 15:08:06 +0000

    hponcfg is already declared in the ilo submodule

--- modules/dc_profile/manifests/hpblade.pp
+++ modules/dc_profile/manifests/hpblade.pp
@@ -3,13 +3,12 @@ class dc_profile::hpblade {
   include hpilo
   include dc_profile::hpsupportrepo
 
-  package { ['hpacucli', 'cciss-vol-status', 'hponcfg' ]:
+  package { ['hpacucli', 'cciss-vol-status' ]:
     ensure  => installed,
     require => Dc_repos::Virtual::Repo['local_hpsupport_mirror']
   }
 
   class { 'hpilo':
-    require     => Package['hponcfg'],
     dhcp        => true,
     dns         => hiera(dnsmaster),
     ilouser     => 'admin',
