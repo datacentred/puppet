aedacffa32124cc351db76d82afbae5489debe35
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-14 15:08:31 +0000

    Make sure we create the top level directory first

--- modules/dc_profile/manifests/external_facts.pp
+++ modules/dc_profile/manifests/external_facts.pp
@@ -1,6 +1,13 @@
 class dc_profile::external_facts {
 
   file { '/etc/facter/facts.d':
+    ensure  => directory,
+    require => File['/etc/facter'],
+    owner   => root,
+    group   => root
+  }
+  
+  file { '/etc/facter':
     ensure     => directory,
     owner      => root,
     group      => root
