1586ee28da6155f46c7c170dfafa480a2169268c
Simon Murray <simon.murray@datacentred.co.uk>
2014-01-17 09:39:53 +0000

    new better way of handling multiple definitions of puppet class

--- modules/dc_profile/manifests/puppet.pp
+++ modules/dc_profile/manifests/puppet.pp
@@ -4,7 +4,7 @@
 class dc_profile::puppet {
 
   # Puppet master will provide this so avoid duplication
-  if !defined(Class['::dc_profile::puppetmaster']) {
+  if $::fqdn != $::puppetmaster {
     class { '::puppet': }
   }
 
