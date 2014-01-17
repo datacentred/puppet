b5bee64d440e7ddf6f32bf83a3bf644e2654ab0c
Simon Murray <simon.murray@datacentred.co.uk>
2014-01-17 09:46:11 +0000

    remove gpg based encryption

--- modules/dc_profile/manifests/puppetmaster_hieragpg.pp
+++
@@ -1,8 +0,0 @@
-# Enable hiera with encryption support
-# install the keys as puppet:puppet first
-# in /etc/puppet/keys
-class dc_profile::puppetmaster_hieragpg {
-
-  class { '::dc_hiera_yamlgpg': }
-
-}
