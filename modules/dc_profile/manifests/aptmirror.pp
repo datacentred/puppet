86d73640b92af13e3ca6e9a2900dd30e96bb435c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-21 16:53:37 +0000

    Fix paths

--- modules/dc_profile/manifests/aptmirror.pp
+++ modules/dc_profile/manifests/aptmirror.pp
@@ -3,17 +3,17 @@ class dc_profile::aptmirror {
   $storagedir = hiera(storagedir)
 
   if $hostgroup == 'Production/Platform Services/HA Raid' {
-    $base_path = "$storagedir/apt-mirror/mirror"
+    $base_path = "$storagedir/apt-mirror"
   }
   else {
-    $base_path = '/var/spool/apt-mirror/mirror'
+    $base_path = '/var/spool/apt-mirror'
   }
 
   include dc_mirrors::mirrorlist
   include apache
 
   apache::site { 'mirror':
-    docroot => "$base_path",
+    docroot => "$base_path/mirror",
     require => [ File["$base_path"], Class['apt_mirror']],
     admin   => hiera(sysmailaddress)
   }
