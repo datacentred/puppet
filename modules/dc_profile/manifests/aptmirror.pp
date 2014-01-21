4b8fcfc1288ccca82b601b04d12f705afff03f26
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-21 16:54:20 +0000

    Fix formatting

--- modules/dc_profile/manifests/aptmirror.pp
+++ modules/dc_profile/manifests/aptmirror.pp
@@ -3,7 +3,7 @@ class dc_profile::aptmirror {
   $storagedir = hiera(storagedir)
 
   if $hostgroup == 'Production/Platform Services/HA Raid' {
-    $base_path = "$storagedir/apt-mirror"
+    $base_path = "${storagedir}/apt-mirror"
   }
   else {
     $base_path = '/var/spool/apt-mirror'
@@ -13,8 +13,8 @@ class dc_profile::aptmirror {
   include apache
 
   apache::site { 'mirror':
-    docroot => "$base_path/mirror",
-    require => [ File["$base_path"], Class['apt_mirror']],
+    docroot => "${base_path}/mirror",
+    require => [ File["${base_path}"], Class['apt_mirror']],
     admin   => hiera(sysmailaddress)
   }
 
@@ -23,9 +23,9 @@ class dc_profile::aptmirror {
   }
 
   class { 'apt_mirror':
-    base_path => "$base_path",
+    base_path => "${base_path}",
     var_path  => '/var/spool/apt-mirror/var',
-    require   => File["$base_path"],
+    require   => File["${base_path}"],
   }
 
   Dc_mirrors::Virtual::Mirror <| |>
