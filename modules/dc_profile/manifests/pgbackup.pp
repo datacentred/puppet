387892002aa6408ffe88473e4d0b2b6eb5794ad2
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-22 17:08:19 +0000

    Add split function

--- modules/dc_profile/manifests/pgbackup.pp
+++ modules/dc_profile/manifests/pgbackup.pp
@@ -6,9 +6,12 @@ class dc_profile::pgbackup {
   realize Dc_repos::Virtual::Repo['local_postgres_mirror']
 
   if $::barman_key {
+
+    $key_elements = split($::barman_key, ' ')
+
     @@ssh_authorized_key { "barman_key_${hostname}" :
       ensure  => present,
-      key     => "$::barman_key",
+      key     => $key_elements[1],
       user    => 'postgres',
       options => "from=$::ipaddress",
       tag     => barman,
