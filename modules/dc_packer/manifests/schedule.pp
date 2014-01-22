4e698b835ce43c35315598be71c9fa23a84cefa4
Nick Jones <nick.jones@datacentred.co.uk>
2014-01-22 17:27:46 +0000

    Don't clobber the packer_cache directory (used to store downloaded ISOs and gues additions)

--- modules/dc_packer/manifests/schedule.pp
+++ modules/dc_packer/manifests/schedule.pp
@@ -7,25 +7,25 @@
 class dc_packer::schedule {
 
   # Fetches the preseed template from Foreman
-  cron { fetch_preseed:
-    command => "/home/packer/bin/preseedme.rb",
-    user    => "packer",
+  cron { 'fetch_preseed':
+    command => '/home/packer/bin/preseedme.rb',
+    user    => 'packer',
     minute  => 0,
     hour    => 2,
   }
   
   # The actual Packer run itself
-  cron { run_packer:
-    command => "cd /home/packer && bin/packer build -only=dcdevbox-vbox /home/packer/templates/dcdevboxbuild.json 2>&1 >> /home/packer/output/packerbuild.log",
-    user    => "packer",
+  cron { 'run_packer':
+    command => 'cd /home/packer && bin/packer build -only=dcdevbox-vbox /home/packer/templates/dcdevboxbuild.json 2>&1 >> /home/packer/output/packerbuild.log',
+    user    => 'packer',
     minute  => 0,
     hour    => 3,
   }
   
   # Make sure the latest-virtualbox symlink points to the most recent build
-  cron { symlink_latest:
+  cron { 'symlink_latest':
     command => "cd /home/packer/output && rm latest-virtualbox && ln -s $(ls -lrt | awk '{ print $9 }' | grep vbox | tail -1) latest-virtualbox",
-    user    => "packer",
+    user    => 'packer',
     minute  => 0,
     hour    => 5,
   }
