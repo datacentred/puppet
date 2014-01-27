70bce15d33eefe72d967bb134f4dbefc1c82b5f0
Nick Jones <nick.jones@datacentred.co.uk>
2014-01-27 14:29:36 +0000

    Ensure we install the latest stable rsyslog package as opposed to a specific version (required due to earlier downgrade)

--- modules/dc_rsyslog/manifests/install.pp
+++ modules/dc_rsyslog/manifests/install.pp
@@ -1,7 +1,6 @@
 # Install rsyslog from local mirror
 class dc_rsyslog::install {
-  # Initialise the rsyslog mirror
   package { 'rsyslog':
-    ensure  => '7.4.8-0adiscon1',
+    ensure  => latest,
   }
 }
