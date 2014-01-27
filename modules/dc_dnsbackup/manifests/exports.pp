74384ae55262a1349a596d8ab471d3ea2c951559
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-01-27 15:36:29 +0000

    Add parentheses

--- modules/dc_dnsbackup/manifests/exports.pp
+++ modules/dc_dnsbackup/manifests/exports.pp
@@ -14,17 +14,17 @@ class dc_dnsbackup::exports{
 
   $storagedir = hiera(storagedir)
 
-  @@file { "$::hostname-dnsbackup":
+  @@file { "${::hostname}-dnsbackup":
     ensure  => directory,
-    path    => "${storagedir}/backups/$::hostname-dnsbackup",
+    path    => "${storagedir}/backups/${::hostname}-dnsbackup",
     require => File["${storagedir}/backups"],
     tag     => 'backups',
   }
 
-  @@nfs::server::export { "${storagedir}/backups/$::hostname-dnsbackup":
+  @@nfs::server::export { "${storagedir}/backups/${::hostname}-dnsbackup":
     ensure  => present,
-    require => File["$::hostname-dnsbackup"],
-    clients => "$::ipaddress(rw,insecure,async,no_root_squash,no_subtree_check)",
+    require => File["${::hostname}-dnsbackup"],
+    clients => "${::ipaddress}(rw,insecure,async,no_root_squash,no_subtree_check)",
     tag     => 'backups',
   }
 }
