d614b8f12990d5737c7251ee9301f669ec50fcdc
Nick Jones <nick.jones@datacentred.co.uk>
2014-01-30 17:18:28 +0000

    Simplify dc_collectd module for now.

--- modules/dc_collectd/manifests/init.pp
+++ modules/dc_collectd/manifests/init.pp
@@ -1,10 +1,36 @@
+#
 # Class: dc_collectd
 #
-# collectd - gather statistics and fire them into carbon for graphing
-# and analysis
+# Install and configure collectd with a standard set of plugins.
 #
-class dc_collectd {
-  class { 'dc_collectd::install': } ->
-  class { 'dc_collectd::plugins': } ->
-  Class ['dc_collectd']
+class dc_collectd (
+  $graphite_server = '',
+  $interfaces = split($::interfaces, ',')
+) {
+
+  include dc_repos::repolist
+  realize (Dc_repos::Virtual::Repo['local_collectd_mirror'])
+
+  class { '::collectd':
+    require      => Dc_repos::Virtual::Repo['local_collectd_mirror'],
+    purge        => true,
+    recurse      => true,
+    purge_config => true,
+  }
+
+  class { 'collectd::plugin::load': }
+
+  class { 'collectd::plugin::memory': }
+
+  class { 'collectd::plugin::interface': 
+    interfaces => $interfaces,
+  }
+
+  # Configure collectd to send its output to carbon
+  class { 'collectd::plugin::write_graphite':
+    graphitehost => $graphite_server,
+  }
+
+  contain 'collectd'
+
 }
