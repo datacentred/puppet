65cb4b82053bac448a3bd7cb8cd360947f7f4e04
Nick Jones <nick.jones@datacentred.co.uk>
2014-01-31 16:50:51 +0000

    SNMP stuffe

---
+++ modules/dc_collectd/manifests/snmp.pp
@@ -0,0 +1,37 @@
+class dc_collectd::snmp {
+  $snmpconf = '/etc/collectd/conf.d/snmp.conf'
+
+  concat { $snmpconf:
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  concat::fragment { 'snmpconf_header':
+    target  => $snmpconf,
+    content => template('dc_collectd/snmpconf_header.erb'),
+    order   => '01',
+  }
+
+  define snmptarget_query ($host,$ip,$snmpconf) {
+    concat::fragment { "$title":
+      target  => "$snmpconf",
+      content => template('dc_collectd/snmpconf_main.erb'),
+    }
+  }
+
+  @snmptarget_query { 'sg300':
+    snmpconf => $snmpconf,
+    host     => '10.10.10.2',
+    ip       => '10.10.10.2',
+  }
+
+  @snmptarget_query { '3560g':
+    snmpconf => $snmpconf,
+    host     => 'ark-rack3-3560g-top',
+    ip       => '10.10.32.2',
+  }
+  
+  Snmptarget_query <| |>
+}
