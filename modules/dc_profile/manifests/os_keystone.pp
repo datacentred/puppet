0ce57c4f763d9a404009aaccb482381144a6c32f
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-02-03 12:17:53 +0000

    Fix typo

--- modules/dc_profile/manifests/os_keystone.pp
+++ modules/dc_profile/manifests/os_keystone.pp
@@ -4,7 +4,7 @@ class dc_profile::os_keystone {
   $keystone_db_host = hiera(keystone_db_host)
 
   class { 'keystone':
-    require        => [ Dc_repos::Virtual::Repo['local_cloudarchive_repo'], Dc_postgresql::Db['keystone'] ],
+    require        => [ Dc_repos::Virtual::Repo['local_cloudarchive_mirror'], Dc_postgresql::Db['keystone'] ],
     verbose        => true,
     catalog_type   => 'sql',
     admin_token    => hiera(keystone_admin_uuid),
