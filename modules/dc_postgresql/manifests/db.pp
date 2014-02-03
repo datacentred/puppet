b99c5c2296da594e3a6967521b07376a0dc1c018
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-02-03 16:34:57 +0000

    More pg_hba.conf fixing

--- modules/dc_postgresql/manifests/db.pp
+++ modules/dc_postgresql/manifests/db.pp
@@ -1,4 +1,4 @@
-define dc_postgresql::db ($user,$password,$access_address='127.0.0.1'){
+define dc_postgresql::db ($user,$password,$access_address='127.0.0.1/32'){
 
   postgresql::server::db { $title:
     user      => $user,
