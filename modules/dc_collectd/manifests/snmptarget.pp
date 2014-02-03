214f583b7b89aef5a426068925dc3ee6484db998
Nick Jones <nick.jones@datacentred.co.uk>
2014-02-03 11:53:05 +0000

    Install some MIBS, they're useful...

--- modules/dc_collectd/manifests/snmptarget.pp
+++ modules/dc_collectd/manifests/snmptarget.pp
@@ -6,14 +6,19 @@ class dc_collectd::snmptarget {
   
   # The devrack Cisco SG300
   @dc_collectd::virtual::snmpvirtual { 'sg300':
-    host     => 'sg300',
-    ip       => '10.10.10.2',
+    host => 'sg300',
+    ip   => '10.10.10.2',
   }
 
   # Cisco 3560G at the top of Ark's Rack 3
   @dc_collectd::virtual::snmpvirtual { 'ark-rack3-3560g-top':
-    host     => 'ark-rack3-3560g-top',
-    ip       => '10.10.32.2',
+    host => 'ark-rack3-3560g-top',
+    ip   => '10.10.32.2',
+  }
+
+  @dc_collectd::virtual::snmpvirtual { 'ark-rack3-3560g-bottom':
+    host => 'ark-rack3-3560g-bottom',
+    ip   => '10.10.32.3',
   }
 
   Dc_collectd::Virtual::Snmpvirtual <| |>
