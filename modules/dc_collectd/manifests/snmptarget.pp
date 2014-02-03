1600a4e9ef639737caa9421bf962bc0cde9706be
Nick Jones <nick.jones@datacentred.co.uk>
2014-02-03 11:16:43 +0000

    Seperated virtual resource definition into its own class; Seperate class for SNMP targets; Some amount of cleanup.

---
+++ modules/dc_collectd/manifests/snmptarget.pp
@@ -0,0 +1,21 @@
+# Definition: dc_collectd::snmptarget
+# List of SNMP targets we'd like to configure 
+# collectd's snmp plugin to monitor for us
+#
+class dc_collectd::snmptarget {
+  
+  # The devrack Cisco SG300
+  @dc_collectd::virtual::snmpvirtual { 'sg300':
+    host     => 'sg300',
+    ip       => '10.10.10.2',
+  }
+
+  # Cisco 3560G at the top of Ark's Rack 3
+  @dc_collectd::virtual::snmpvirtual { 'ark-rack3-3560g-top':
+    host     => 'ark-rack3-3560g-top',
+    ip       => '10.10.32.2',
+  }
+
+  Dc_collectd::Virtual::Snmpvirtual <| |>
+
+}
