20c51a17183707795633275b5e3a81fc8ac7315e
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-02-05 11:02:00 +0000

    Add db class

--- modules/dc_mariadb/manifests/db.pp
+++ modules/dc_mariadb/manifests/db.pp
@@ -1,8 +1,10 @@
 define dc_mariadb::db ($dbname,$user,$password){
 
-  mysql::db { "$dbname":
-    user      => $user,
-    password  => $password,
+  mysql::db { "${dbname}":
+    user     => $user,
+    password => $password,
+    host     => 'localhost',
+    grant    => ['SELECT', 'UPDATE'],
   }
 
 }
