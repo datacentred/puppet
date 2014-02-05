ed0e8cadb82e73f136ea302d23c0e8f484aaf7dc
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-02-05 11:04:12 +0000

    Add db creation

--- modules/dc_profile/manifests/keystone_mariadb.pp
+++ modules/dc_profile/manifests/keystone_mariadb.pp
@@ -1,9 +1,15 @@
 class dc_profile::keystone_mariadb {
 
   $keystone_mariaroot_pw = hiera(keystone_mariaroot_pw)
+  $keystone_db_pw        = hiera(keystone_db_pw)
 
   class { 'dc_mariadb':
     maria_root_pw       => $keystone_mariaroot_pw,
+  }
+
+  dc_mariadb::db { 'keystone':
+    user           => 'keystone',
+    password       => $keystone_db_pw,
   }
 
 }
