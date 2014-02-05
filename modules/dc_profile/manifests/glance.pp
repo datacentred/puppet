8838d8f0223837aab2263fd381ad52e809669f83
Simon Murray <simon.murray@datacentred.co.uk>
2014-02-05 15:53:10 +0000

    integration with keystone

--- modules/dc_profile/manifests/glance.pp
+++ modules/dc_profile/manifests/glance.pp
@@ -1,10 +1,8 @@
 # Glance API and registry server
 class dc_profile::glance {
 
-  realize Dc_repos::Virtual::Repo['local_cloudarchive_mirror']
-
-  $keystone_host = 'keystone.sal01.datacentred.co.uk'
-  $keystone_glance_password = 'Deojun8OmEji'
+  $keystone_host = get_exported_var('', 'keystone_host', ['localhost'])
+  $keystone_glance_password = hiera(keystone_glance_password)
 
   $glance_api_db   = hiera(glance_api_db)
   $glance_api_user = hiera(glance_api_user)
@@ -14,31 +12,15 @@ class dc_profile::glance {
   $glance_reg_user = hiera(glance_reg_user)
   $glance_reg_pass = hiera(glance_reg_pass)
 
-  @@keystone_user { 'glance':
-    ensure   => present,
-    enabled  => true,
-    password => $keystone_glance_password,
-    tenant   => 'services',
-  }
+  contain dc_mariadb::server
 
-  # Okay so here is the sad part, because of all the includes and inherits in
-  # the 3rd party code, these packages get installed well before the apt
-  # repositories are updated and we get old versions, which causes the install
-  # to fail!
-  Dc_repos::Virtual::Repo['local_cloudarchive_mirror'] -> Package['mysql-client']
-  Dc_repos::Virtual::Repo['local_cloudarchive_mirror'] -> Package['glance']
-  Dc_repos::Virtual::Repo['local_cloudarchive_mirror'] -> Package['python-keystone']
-  Dc_repos::Virtual::Repo['local_cloudarchive_mirror'] -> Package['mysql-server']
-
-  contain mysql::server
-
-  mysql::db { $glance_api_db:
+  dc_mariadb::db { $glance_api_db:
     user     => $glance_api_user,
     password => $glance_api_pass,
     require  => Class['mysql::server'],
   }
 
-  mysql::db { $glance_reg_db:
+  dc_mariadb::db { $glance_reg_db:
     user     => $glance_reg_user,
     password => $glance_reg_pass,
     require  => Class['mysql::server'],
@@ -54,10 +36,7 @@ class dc_profile::glance {
     sql_connection    => "mysql://${glance_api_user}:${glance_api_pass}@localhost/${glance_api_db}",
     use_syslog        => true,
     enabled           => true,
-    require           => [
-      Mysql::Db[$glance_api_db],
-      Dc_repos::Virtual::Repo['local_cloudarchive_mirror'],
-    ],
+    require           => Dc_mariadb::Db[$glance_api_db],
   }
   contain 'glance::api'
 
@@ -74,11 +53,13 @@ class dc_profile::glance {
     sql_connection    => "mysql://${glance_reg_user}:${glance_reg_pass}@localhost/${glance_reg_db}",
     use_syslog        => true,
     enabled           => true,
-    require           => [
-      Mysql::Db[$glance_reg_db],
-      Dc_repos::Virtual::Repo['local_cloudarchive_mirror'],
-    ],
+    require           => Dc_mariadb::Db[$glance_reg_db],
   }
   contain 'glance::registry'
+
+  # TODO: Temporary backend while boot-strapping CEPH
+  class { 'glance::backend::file':
+  }
+  contain 'glance::backend::file'
 
 }
