f521fb8eb0ba23f1a8f7d828f846df52ec31ff61
Simon Murray <simon.murray@datacentred.co.uk>
2014-02-06 13:32:08 +0000

    add some dynamism into the mix

--- modules/dc_profile/manifests/glance.pp
+++ modules/dc_profile/manifests/glance.pp
@@ -11,28 +11,45 @@ class dc_profile::glance {
   $glance_api_db   = hiera(glance_api_db)
   $glance_api_user = hiera(glance_api_user)
   $glance_api_pass = hiera(glance_api_pass)
+  $glance_api_host = hiera(glance_api_host)
 
   $glance_reg_db   = hiera(glance_reg_db)
   $glance_reg_user = hiera(glance_reg_user)
   $glance_reg_pass = hiera(glance_reg_pass)
+  $glance_reg_host = hiera(glance_reg_host)
 
   $glance_port = "9292"
 
-  class { 'dc_mariadb':
-    maria_root_pw => $glance_db_root_pw,
-  }
-  contain 'dc_mariadb'
+  $glance_api_database = "mysql://${glance_api_user}:${glance_api_pass}@${glance_api_host}/${glance_api_db}"
+  $glance_reg_database = "mysql://${glance_reg_user}:${glance_reg_pass}@${glance_reg_host}/${glance_reg_db}"
 
-  dc_mariadb::db { $glance_api_db:
-    user     => $glance_api_user,
-    password => $glance_api_pass,
-    require  => Class['Dc_mariadb'],
-  }
+  # Optionally install the database package
+  if '127.0.0.1' in [$glance_api_host, $glance_reg_host] {
 
-  dc_mariadb::db { $glance_reg_db:
-    user     => $glance_reg_user,
-    password => $glance_reg_pass,
-    require  => Class['Dc_mariadb'],
+    class { 'dc_mariadb':
+      maria_root_pw => $glance_db_root_pw,
+    }
+    contain 'dc_mariadb'
+
+    # Optionally install each database
+    # TODO: I suspect this could be a shared class to be
+    #       included in case you wished to provision on
+    #       another host
+    if $glance_api_host == '127.0.0.1' {
+      dc_mariadb::db { $glance_api_db:
+        user     => $glance_api_user,
+        password => $glance_api_pass,
+        require  => Class['Dc_mariadb'],
+      }
+    }
+    if $glance_reg_host == '127.0.0.1' {
+        dc_mariadb::db { $glance_reg_db:
+        user     => $glance_reg_user,
+        password => $glance_reg_pass,
+        require  => Class['Dc_mariadb'],
+      }
+    }
+
   }
 
   class { 'glance::api':
@@ -42,7 +59,7 @@ class dc_profile::glance {
     keystone_tenant   => 'services',
     keystone_user     => 'glance',
     keystone_password => $keystone_glance_password,
-    sql_connection    => "mysql://${glance_api_user}:${glance_api_pass}@127.0.0.1/${glance_api_db}",
+    sql_connection    => $glance_api_database,
     use_syslog        => true,
     enabled           => true,
     require           => Dc_mariadb::Db[$glance_api_db],
@@ -59,7 +76,7 @@ class dc_profile::glance {
     keystone_tenant   => 'services',
     keystone_user     => 'glance',
     keystone_password => $keystone_glance_password,
-    sql_connection    => "mysql://${glance_reg_user}:${glance_reg_pass}@127.0.0.1/${glance_reg_db}",
+    sql_connection    => $glance_reg_database,
     use_syslog        => true,
     enabled           => true,
     require           => Dc_mariadb::Db[$glance_reg_db],
@@ -73,9 +90,9 @@ class dc_profile::glance {
 
   @@keystone_endpoint { "${os_region}/glance":
     ensure        => present,
-    public_url    => "http://${::fqdn}:${glance_port}",
-    admin_url     => "http://${::fqdn}:${glance_port}",
-    internal_url  => "http://${::fqdn}:${glance_port}",
+    public_url    => "http://${::fqdn}:${glance_port}/v2/%(tenant_id)s",
+    admin_url     => "http://${::fqdn}:${glance_port}/v2/%(tenant_id)s",
+    internal_url  => "http://${::fqdn}:${glance_port}/v2/%(tenant_id)s",
     tag           => 'glance_endpoint',
   }
 
--- modules/dc_profile/manifests/nova.pp
+++ modules/dc_profile/manifests/nova.pp
@@ -1,6 +1,8 @@
 # Nova controller node
 class dc_profile::nova {
 
+  $os_region           = hiera(os_region)
+
   $nova_mq_username    = hiera(nova_mq_username)
   $nova_mq_password    = hiera(nova_mq_password)
   $nova_mq_port        = hiera(nova_mq_port)
@@ -8,6 +10,7 @@ class dc_profile::nova {
 
   $glance_api_servers  = get_exported_var('', 'glance_api_server', ['localhost:9292'])
 
+  $nova_db_root_pw     = hiera(nova_db_root_pw)
   $nova_db_user        = hiera(nova_db_user)
   $nova_db_pass        = hiera(nova_db_pass)
   $nova_db_host        = hiera(nova_db_host)
@@ -22,10 +25,30 @@ class dc_profile::nova {
 
   $neutron_secret      = hiera(neutron_secret)
 
+  $ec2_port = '8773'
+  $nova_port = '8774'
+
   # Hard coded exported variable name
   $nova_mq_ev = 'nova_mq_node'
 
   $nova_database = "mysql://${nova_db_user}:${nova_db_pass}@${nova_db_host}/${nova_db}"
+
+  # Check to see if we need to install the data base locally
+  if $nova_db_host == '127.0.0.1' {
+
+    class { 'dc_mariadb':
+      maria_root_pw => $nova_db_root_pw,
+    }
+    contain 'dc_mariadb'
+
+    dc_mariadb::db { $nova_db:
+      user     => $nova_db_user,
+      password => $nova_db_pass,
+      require  => Class['Dc_mariadb'],
+      before   => Class['::nova'],
+    }
+
+  }
 
   class { '::nova':
     database_connection => $nova_database,
@@ -37,6 +60,7 @@ class dc_profile::nova {
     rabbit_virtual_host => $nova_mq_vhost,
     use_syslog          => true,
   }
+  contain 'nova'
 
   class { '::nova::api':
     enabled                              => true,
@@ -46,6 +70,23 @@ class dc_profile::nova {
     enabled_apis                         => $nova_enabled_apis,
     auth_host                            => $keystone_host,
     neutron_metadata_proxy_shared_secret => $neutron_secret,
+  }
+  contain 'nova::api'
+
+  @@keystone_endpoint { "${os_region}/nova":
+    ensure        => present,
+    public_url    => "http://${::fqdn}:${nova_port}/v2/%(tenant_id)s",
+    admin_url     => "http://${::fqdn}:${nova_port}/v2/%(tenant_id)s",
+    internal_url  => "http://${::fqdn}:${nova_port}/v2/%(tenant_id)s",
+    tag           => 'nova_endpoint',
+  }
+
+  @@keystone_endpoint { "${os_region}/nova_ec2":
+    ensure        => present,
+    public_url    => "http://${::fqdn}:${ec2_port}/services/Cloud",
+    admin_url     => "http://${::fqdn}:${ec2_port}/services/Admin",
+    internal_url  => "http://${::fqdn}:${ec2_port}/services/Cloud",
+    tag           => 'nova_endpoint',
   }
 
 }
