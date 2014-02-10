3bca8cf9874164f0415a121b588985a829ba8747
Nick Jones <nick.jones@datacentred.co.uk>
2014-02-10 10:30:20 +0000

    Updated to use hiera value for sysadmin email address

--- modules/dc_kibana/manifests/apache.pp
+++ modules/dc_kibana/manifests/apache.pp
@@ -1,7 +1,7 @@
 class dc_kibana::apache {
   apache::vhost { 'logstash': 
     docroot     => '/var/www/kibana',
-    serveradmin => 'admin@datacentred.co.uk',
+    serveradmin => hiera(sysmailaddress),
     require     => File['/var/www/kibana'],
   }
 }
