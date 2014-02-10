aeb0751fd9fd8f30d82578ad1f72412aaff41816
Nick Jones <nick.jones@datacentred.co.uk>
2014-02-10 14:15:04 +0000

    Remove redundant requires

--- modules/dc_gdash/manifests/hostgraph.pp
+++ modules/dc_gdash/manifests/hostgraph.pp
@@ -13,27 +13,22 @@ define dc_gdash::hostgraph (
   file { $hostpath: 
     ensure  => directory,
     recurse => true,
-    require => File [ $tplpath ],
   }
 
   file { "${hostpath}/dash.yaml": 
     content => template('dc_gdash/dash.yaml.erb'),
-    require => File[ $hostpath ], 
   }
 
   file { "${hostpath}/load.graph": 
     content => template('dc_gdash/load.graph.erb'),
-    require => File[ $hostpath ], 
   }
 
   file { "${hostpath}/memory.graph": 
     content => template('dc_gdash/memory.graph.erb'),
-    require => File[ $hostpath ], 
   }
 
   file { "${hostpath}/nettraf.graph": 
     content => template('dc_gdash/nettraf.graph.erb'), 
-    require => File[ $hostpath ], 
   }
 
 }
