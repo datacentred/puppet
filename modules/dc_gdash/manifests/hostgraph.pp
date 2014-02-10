4fd5c87f2983a2a624cfadc8bb6f0ffb319c045d
Nick Jones <nick.jones@datacentred.co.uk>
2014-02-10 14:02:41 +0000

    Updated folder structure

--- modules/dc_gdash/manifests/hostgraph.pp
+++ modules/dc_gdash/manifests/hostgraph.pp
@@ -2,12 +2,18 @@ define dc_gdash::hostgraph (
   $hostname = $title 
 ) {
 
-  $tplpath = '/var/www/gdash/graph_templates'
+  $tplpath = '/var/www/gdash/graph_templates/hosts'
   $hostpath = "${tplpath}/${hostname}"
+
+  file { $tplpath:
+    ensure  => directory,
+    recurse => true,
+  }
 
   file { $hostpath: 
     ensure  => directory,
     recurse => true,
+    require => File [ $tplpath ],
   }
 
   file { "${hostpath}/dash.yaml": 
