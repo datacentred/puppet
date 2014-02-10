50908b5ef702e5567f5b9c6c3e8400b5d95f6380
Nick Jones <nick.jones@datacentred.co.uk>
2014-02-10 13:20:21 +0000

    Updated template paths

--- modules/dc_gdash/manifests/hostgraph.pp
+++ modules/dc_gdash/manifests/hostgraph.pp
@@ -1,27 +1,33 @@
-define dc_gdash::hostgraph () {
+define dc_gdash::hostgraph (
+  $hostname = $title 
+) {
 
-  $tplpath = '/var/www/gdash/graph_templates/'
-  $hostpath = "${tplpath}/${hostname}/collectd/"
+  $tplpath = '/var/www/gdash/graph_templates'
+  $hostpath = "${tplpath}/${hostname}/collectd"
 
   file { $hostpath: 
-    ensure => directory, 
-    require => File[$tplpath] 
+    ensure  => directory,
+    recurse => true,
   }
 
   file { "${hostpath}/dash.yaml": 
-    content => template('dc_gdash/dash.yaml.erb') 
+    content => template('dc_gdash/dash.yaml.erb'),
+    require => File[ $hostpath ], 
   }
 
   file { "${hostpath}/load.graph": 
-    content => template('dc_gdash/load.graph.erb') 
+    content => template('dc_gdash/load.graph.erb'),
+    require => File[ $hostpath ], 
   }
 
-  file { "{$hostpath}/memory.graph": 
-    content => template('dc_gdash/memory.graph.erb') 
+  file { "${hostpath}/memory.graph": 
+    content => template('dc_gdash/memory.graph.erb'),
+    require => File[ $hostpath ], 
   }
 
-  file { "{$hostpath}/nettraf.graph": 
-    content => template('dc_gdash/nettraf.graph.erb') 
+  file { "${hostpath}/nettraf.graph": 
+    content => template('dc_gdash/nettraf.graph.erb'), 
+    require => File[ $hostpath ], 
   }
 
 }
