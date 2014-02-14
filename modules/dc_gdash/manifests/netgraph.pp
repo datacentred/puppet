ae812e074541a53e09e003037474426358fdf471
Nick Jones <nick.jones@datacentred.co.uk>
2014-02-14 14:40:33 +0000

    Include netgraph.pp

---
+++ modules/dc_gdash/manifests/netgraph.pp
@@ -0,0 +1,18 @@
+define dc_gdash::netgraph (
+  $interface = $title,
+  $hostname,
+) {
+
+  $tplpath = '/var/www/gdash/graph_templates'
+  $hostpath = "${tplpath}/hosts/${hostname}"
+
+  file { "${hostpath}":
+    ensure  => directory,
+    recurse => true,
+  }
+
+  file { "${hostpath}/nettraf.graph": 
+    content => template('dc_gdash/nettraf.graph.erb'), 
+  }
+
+}
