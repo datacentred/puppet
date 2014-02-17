06ebc97e64db99cf36406cb8bd39d840972f06b6
Nick Jones <nick.jones@datacentred.co.uk>
2014-02-17 14:54:33 +0000

    Remove duplicate resource declaration, improve description

--- modules/dc_gdash/manifests/nettraf.pp
+++ modules/dc_gdash/manifests/nettraf.pp
@@ -10,11 +10,6 @@ define dc_gdash::nettraf (
   $tplpath = '/var/www/gdash/graph_templates'
   $hostpath = "${tplpath}/hosts/${hostname}"
 
-  file { "${hostpath}":
-    ensure  => directory,
-    recurse => true,
-  }
-
   file { "${hostpath}/nettraf.${interface}.graph": 
     content => template('dc_gdash/nettraf.graph.erb'), 
   }
