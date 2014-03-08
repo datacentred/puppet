96bebb0baea02a6102dd11f25db0d9a67b1063f0
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-03-08 10:07:00 +0000

    Sort out formatting and change to hash

--- modules/dc_logstash/manifests/init.pp
+++ modules/dc_logstash/manifests/init.pp
@@ -42,20 +42,21 @@ class dc_logstash {
   }
 
   # Email alerting from rsyslog
-  # Currently the logstash module only supports match, which is deprecated in favour of conditionals
+  # Currently the logstash module only supports match
+  # which is now deprecated in favour of conditionals
   logstash::output::email { 'logstash-rsyslog-email':
-    from    => "logstash@{::$fqdn}",
-    match   => [
-                "WARN","syslog_severity_code,4",
-                "ERROR","syslog_severity_code,3",
-                "CRITICAL","syslog_severity_code,2",
-                "ALERT","syslog_severity_code,1",
-                "EMERGENCY","syslog_severity_code,0",
-               ],
+    from    => "logstash@${::fqdn}",
+    match   => {
+                'WARN'      => 'syslog_severity_code,4',
+                'ERROR'     => 'syslog_severity_code,3',
+                'CRITICAL'  => 'syslog_severity_code,2',
+                'ALERT'     => 'syslog_severity_code,1',
+                'EMERGENCY' => 'syslog_severity_code,0',
+              },
     subject => '%{matchName}',
     to      => hiera(sysmailaddress),
     via     => 'sendmail',
-    body    => "Here is the event line that occured: %{@message}",
+    body    => 'Here is the event line that occured: %{@message}',
   }
 
   class { 'dc_logstash::icinga': }
