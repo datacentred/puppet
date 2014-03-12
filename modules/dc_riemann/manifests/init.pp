bc18ea9d74141a3db54ee33367153104329a6684
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-03-12 16:34:39 +0000

    Module to install riemann with config directory to allow for exported configs

---
+++ modules/dc_riemann/manifests/init.pp
@@ -0,0 +1,43 @@
+# Class: dc_riemann
+#
+# Install and configure riemann
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class dc_riemann {
+
+  $sysmailaddress = hiera(sysmailaddress)
+  $riemann_config_dir = '/etc/riemann.conf.d'
+
+  class { 'riemann':
+    config_file => '/etc/riemann.config',
+    require     => File['/etc/riemann.config'],
+  }
+
+  file { '/etc/riemann.config':
+    ensure  => file,
+    content => template('dc_riemann/riemann.config.erb'),
+  }
+
+  file {'/etc/riemann.conf.d':
+    ensure => directory,
+    owner  => 'riemann',
+    group  => 'riemann',
+  }
+
+  file {'/etc/riemann.conf.d/syslog-errors.clj':
+    ensure  => file,
+    require => File['/etc/riemann.conf.d'],
+    owner   => 'riemann',
+    group   => 'riemann',
+    content => template('dc_riemann/syslog-errors.clj.erb')
+  }
+}
+
