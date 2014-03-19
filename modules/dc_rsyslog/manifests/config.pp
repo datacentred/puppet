f65b46a71bd89dad2ddf5e3b0af7fd6a80639a5c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-03-19 15:49:34 +0000

    Make sure json stuff gets removed

--- modules/dc_rsyslog/manifests/config.pp
+++ modules/dc_rsyslog/manifests/config.pp
@@ -2,12 +2,10 @@
 # updates
 class dc_rsyslog::config {
   file { '/etc/rsyslog.d/30-jsonls.conf':
+    ensure  => absent,
     path    => '/etc/rsyslog.d/30-jsonls.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('dc_rsyslog/jsonls.conf.erb'),
   }
+
   file { '/etc/rsyslog.d/50-default.conf':
     path    => '/etc/rsyslog.d/50-default.conf',
     owner   => 'root',
