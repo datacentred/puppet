7f007b33ba4ce86c02aa2875d3c40be38d0db5a8
Simon Murray <simon.murray@datacentred.co.uk>
2014-03-20 15:02:43 +0000

    binding defines generic mariadb service setup, glance and keystone can coexist

--- modules/dc_profile/manifests/openstack/keystone_mariadb.pp
+++ modules/dc_profile/manifests/openstack/keystone_mariadb.pp
@@ -12,13 +12,9 @@
 #
 class dc_profile::openstack::keystone_mariadb {
 
-  $keystone_mariaroot_pw = hiera(keystone_mariaroot_pw)
   $keystone_db_pw        = hiera(keystone_db_pw)
 
-  class { 'dc_mariadb':
-    maria_root_pw => $keystone_mariaroot_pw,
-  }
-  contain 'dc_mariadb'
+  contain dc_mariadb
 
   dc_mariadb::db { 'keystone':
     user     => 'keystone',
