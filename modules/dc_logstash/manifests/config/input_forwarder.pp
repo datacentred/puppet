d3b4d7aa0851af998b5858ceec929934f885b90d
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-03-21 16:35:54 +0000

    Sort out coding standards

--- modules/dc_logstash/manifests/config/input_forwarder.pp
+++ modules/dc_logstash/manifests/config/input_forwarder.pp
@@ -8,29 +8,29 @@ class dc_logstash::config::input_forwarder {
   $keypath = "${forwarder_conf_path}/logstash-forwarder.key"
   $certpath = "${forwarder_conf_path}/logstash-forwarder.crt"
 
-  file { "${forwarder_conf_path}":
+  file { $forwarder_conf_path:
     ensure => directory,
     owner  => 'logstash',
     group  => 'logstash',
     mode   => '0600'
   }
 
-  file { "${keypath}":
+  file { $keypath:
     ensure  => file,
     owner   => 'logstash',
     group   => 'logstash',
     mode    => '0600',
     source  => 'puppet:///modules/dc_logstash/logstash-forwarder.key',
-    require => File["${forwarder_conf_path}"],
+    require => File[$forwarder_conf_path],
   }
 
-  file { "${certpath}":
+  file { $certpath:
     ensure  => file,
     owner   => 'logstash',
     group   => 'logstash',
     mode    => '0600',
     source  => 'puppet:///modules/dc_logstash/logstash-forwarder.crt',
-    require => File["${forwarder_conf_path}"],
+    require => File[$forwarder_conf_path],
   }
 
   logstash::configfile { 'input_forwarder':
