6ef06c8fc7fe84609a9c38adcc4303e7d25042bb
Nick Jones <nick.jones@datacentred.co.uk>
2014-03-25 15:25:36 +0000

    Neutron server role class initial commit

---
+++ modules/dc_role/manifests/neutron_server.pp
@@ -0,0 +1,17 @@
+# Class: dc_profile::openstack::neutron_server
+#
+# Openstack Neutron server components
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_role::neutron_server {
+
+  contain dc_profile::openstack::neutron_server
+
+}
