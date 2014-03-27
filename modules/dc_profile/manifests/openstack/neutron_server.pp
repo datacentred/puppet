01e7dae337d546783a1366780f33cbd7c1053975
Nick Jones <nick.jones@datacentred.co.uk>
2014-03-27 10:22:08 +0000

    Additional OVS plugin and agent configuration

--- modules/dc_profile/manifests/openstack/neutron_server.pp
+++ modules/dc_profile/manifests/openstack/neutron_server.pp
@@ -24,17 +24,21 @@ class dc_profile::openstack::neutron_server {
   $neutron_db_user    = hiera(neutron_db_user)
   $neutron_db_pass    = hiera(neutron_db_pass)
 
+  # Hard coded exported variable name
+  $nova_mq_ev = 'nova_mq_node'
+
   # enable the neutron service
   class { 'neutron':
-      enabled             => true,
-      bind_host           => '0.0.0.0',
-      rabbit_hosts        => get_exported_var('', $nova_mq_ev, []),
-      rabbit_user         => $nova_mq_username,
-      rabbit_password     => $nova_mq_password,
-      rabbit_port         => $nova_mq_port,
-      rabbit_virtual_host => $nova_mq_vhost,
-      verbose             => false,
-      debug               => false,
+      enabled               => true,
+      bind_host             => '0.0.0.0',
+      rabbit_hosts          => get_exported_var('', $nova_mq_ev, []),
+      rabbit_user           => $nova_mq_username,
+      rabbit_password       => $nova_mq_password,
+      rabbit_port           => $nova_mq_port,
+      rabbit_virtual_host   => $nova_mq_vhost,
+      allow_overlapping_ips => true,
+      verbose               => true,
+      debug                 => false,
   }
   
   # configure authentication
@@ -44,10 +48,22 @@ class dc_profile::openstack::neutron_server {
       database_connection => "mysql://${neutron_db_user}:${neutron_db_pass}@${neutron_db_host}/${neutron_db}?charset=utf8",
       mysql_module        => '2.2',
   }
+
+  # Configure Neutron for OVS
+  class { 'neutron::agents::ovs':
+    local_ip         => $::network_eth1,
+    enable_tunneling => true,
+  }
   
-  # enable the Open VSwitch plugin server
+
+  # Enable the Open VSwitch plugin server
   class { 'neutron::plugins::ovs':
       tenant_network_type => 'gre',
   } 
+
+  # Enable various agent plugins
+  class { 'neutron::agents::dhcp': }
+  class { 'neutron::agents::l3': }
+  class { 'neutron::agents::metering': }
 
 }
