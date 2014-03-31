c455fb9f9593110bbd4500382b663ead06d97d6c
Nick Jones <nick.jones@datacentred.co.uk>
2014-03-31 14:10:33 +0100

    Initial commit of the nova-compute stuff - profile and role class definitions

---
+++ modules/dc_role/manifests/compute_node.pp
@@ -0,0 +1,21 @@
+# Class: dc_role::compute_node
+#
+# OpenStack Compute role - nova-compute, neutron-agent
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_role::compute_node {
+
+  contain dc_profile::openstack::neutron_agent
+  contain dc_profile::openstack::nova_compute
+
+  Class['dc_profile::openstack::neutron_agent'] ->
+  Class['dc_profile::openstack::nova_compute']
+
+}
