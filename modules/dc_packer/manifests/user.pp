085ed7573f0ca25e99475b2bc402db72eccbca3a
Nick Jones <nick.jones@datacentred.co.uk>
2014-04-02 09:01:36 +0100

    Specify that the packer account used to build images should be a system account

--- modules/dc_packer/manifests/user.pp
+++ modules/dc_packer/manifests/user.pp
@@ -1,6 +1,7 @@
 class dc_packer::user {
   user { 'packer':
     ensure     => present,
+    system     => true,
     home       => '/home/packer',
     managehome => true,
     comment    => 'Account for building VM images via Packer',
