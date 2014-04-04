78833c2179f40735f910bb6cf8b3b5cde0bcc762
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-04 12:07:36 +0100

    Add remove of installed default

--- modules/dc_profile/manifests/log/logrotate.pp
+++ modules/dc_profile/manifests/log/logrotate.pp
@@ -27,18 +27,18 @@ class dc_profile::log::logrotate {
   # Replacement for other logrotate rules in default rsyslog rotation
   logrotate::rule { 'dc_othersyslog':
     path         => '/var/log/mail.info
-                     /var/log/mail.warn
-                     /var/log/mail.err
-                     /var/log/mail.log
-                     /var/log/daemon.log
-                     /var/log/kern.log
-                     /var/log/auth.log
-                     /var/log/user.log
-                     /var/log/lpr.log
-                     /var/log/cron.log
-                     /var/log/rsyslog.log
-                     /var/log/debug
-                     /var/log/messages',
+/var/log/mail.warn
+/var/log/mail.err
+/var/log/mail.log
+/var/log/daemon.log
+/var/log/kern.log
+/var/log/auth.log
+/var/log/user.log
+/var/log/lpr.log
+/var/log/cron.log
+/var/log/rsyslog.log
+/var/log/debug
+/var/log/messages',
     rotate        => 13,
     rotate_every  => 'week',
     missingok     => true,
@@ -49,5 +49,10 @@ class dc_profile::log::logrotate {
     postrotate    => 'reload rsyslog >/dev/null 2>&1 || true',
   }
 
+  # Remove installed logrotate config for above
+  file { '/etc/logrotate.d/rsyslog':
+    ensure  => absent,
+    require => [Logrotate::Rule['dc_othersyslog'], Logrotate::Rule['dc_rsyslog']],
+  }
 
 }
