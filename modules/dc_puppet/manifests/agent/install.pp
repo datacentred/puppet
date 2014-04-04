ac31fe0f3c1bbda7fb919272411bc8d59b54cb5c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-04 14:32:07 +0100

    Add requirement for puppet-common version

--- modules/dc_puppet/manifests/agent/install.pp
+++ modules/dc_puppet/manifests/agent/install.pp
@@ -14,8 +14,13 @@ class dc_puppet::agent::install {
 
   include dc_puppet::params
 
-  package { 'puppet':
+  package { 'puppet-common':
     ensure => $dc_puppet::params::version,
+  }
+
+  package { 'puppet':
+    ensure  => $dc_puppet::params::version,
+    require => Package['puppet-common'],
   }
 
 }
