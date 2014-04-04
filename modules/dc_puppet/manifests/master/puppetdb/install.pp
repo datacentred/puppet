7fcf169320f2c87b0f22063aad0c7eaab13286ef
Simon Murray <simon.murray@datacentred.co.uk>
2014-04-04 11:10:42 +0100

    remove puppetdb-terminus versioning

--- modules/dc_puppet/manifests/master/puppetdb/install.pp
+++ modules/dc_puppet/manifests/master/puppetdb/install.pp
@@ -15,7 +15,7 @@ class dc_puppet::master::puppetdb::install {
   include dc_puppet::params
 
   package { 'puppetdb-terminus':
-    ensure => $dc_puppet::params::version,
+    ensure => installed,
   }
 
 }
