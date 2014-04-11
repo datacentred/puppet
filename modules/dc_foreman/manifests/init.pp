cff9dc9969af5926536920fb3ca90e96e74c8c90
Sean Handley <sean.handley@gmail.com>
2014-04-11 13:52:18 +0100

    Forgot to save this :/

--- modules/dc_foreman/manifests/init.pp
+++ modules/dc_foreman/manifests/init.pp
@@ -15,7 +15,6 @@ class dc_foreman (
 ) {
 
   file { '/usr/share/foreman/app/assets/javascripts/host_edit.js':
-    require => Package['foreman'],
     owner   => 'foreman',
     group   => 'foreman',
     mode    => '0640',
@@ -23,7 +22,6 @@ class dc_foreman (
   }
 
   file { '/usr/share/foreman/app/views/hosts/_unattended.html.erb':
-    require => Package['foreman'],
     owner   => 'foreman',
     group   => 'foreman',
     mode    => '0640',
