8c03c0f6c9916d7c41b6b72765a8774ef168b26b
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-12 14:00:28 +0100

    Add forward handling

--- modules/dc_postfix/manifests/virtual.pp
+++ modules/dc_postfix/manifests/virtual.pp
@@ -2,33 +2,43 @@ class dc_postfix::virtual {
 
   $virtual_uid = hiera(postfix_virtual_uid)
   $virtual_gid = hiera(postfix_virtual_gid)
-  $sysmailsplit = split(hiera(sysmailaddress), '@')
+  $sysmailsplit = split(hiera(sal01_internal_sysmail_address), '@')
   $sysmailuser = $sysmailsplit[0]
-  $sysmailaddress = hiera(sysmailaddress)
+  $internal_sysmail_address = hiera(sal01_internal_sysmail_address)
+  $external_sysmail_address = hiera(external_sysmail_address)
 
   postfix::config { 'virtual_mailbox_domains':
-        value => $::domain,
+    value => $::domain,
   }
   postfix::config { 'virtual_mailbox_base':
-        value => hiera(postfix_vmailbox_base),
+    value => hiera(postfix_vmailbox_base),
   }
   postfix::config { 'virtual_minimum_uid':
-        value => '100',
+    value => '100',
   }
   postfix::config { 'virtual_uid_maps':
-        value => "static:${virtual_uid}",
+    value => "static:${virtual_uid}",
   }
   postfix::config { 'virtual_gid_maps':
-        value => "static:${virtual_gid}",
+    value => "static:${virtual_gid}",
   }
 
   postfix::hash { '/etc/postfix/vmailbox':
-        ensure    => present,
-        map_owner => 'postfix',
-        content   => "${sysmailaddress} ${::domain}/${sysmailuser}/",
+    ensure    => present,
+    map_owner => 'postfix',
+    content   => "${internal_sysmail_address} ${::domain}/${sysmailuser}/",
   }
   postfix::config { 'virtual_mailbox_maps':
-        value => 'hash:/etc/postfix/vmailbox',
+    value => 'hash:/etc/postfix/vmailbox',
+  }
+
+  postfix::hash { '/etc/postfix/valias':
+    ensure    => present,
+    map_owner => 'postfix',
+    content   => "${internal_sysmail_address} ${internal_sysmail_address}, ${external_sysmail_address}"
+  }
+  postfix::config { 'virtual_alias_maps':
+    value => 'hash:/etc/postfix/valias'
   }
 
 }
