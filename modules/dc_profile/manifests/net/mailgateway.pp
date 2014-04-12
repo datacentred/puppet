bc287b85b7604d677b59aa05c4d4c69a5475b904
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-12 10:04:58 +0100

    Add profile class

---
+++ modules/dc_profile/manifests/net/mailgateway.pp
@@ -0,0 +1,17 @@
+# Class: dc_profile::net::mailgateway
+#
+# Installs postfix as a mail gateway
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::net::mailgateway {
+
+  include dc_postfix
+
+}
