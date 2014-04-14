fd0f768cc39ebf2dbfb5ac4790223b3822d85d15
Nick Jones <nick.jones@datacentred.co.uk>
2014-04-14 11:03:48 +0100

    Doing The Right Thing (tm) with some appropriate comments

--- modules/dc_profile/manifests/openstack/keystone_dcadmins.pp
+++ modules/dc_profile/manifests/openstack/keystone_dcadmins.pp
@@ -1,3 +1,20 @@
+# Class: dc_profile::openstack::keystone_dcadmins
+#
+# Sets up admin accounts from Hiera in Keystone, assigns them the
+# admin role, and sets the default tenant to 'admin'.
+#
+# Parameters: $title - name of account to be created
+#             $hash - hash of values keyed on username and including
+#                     password
+#
+# Actions:
+#
+# Requires: puppet-keystone
+#
+# Sample Usage:
+# dc_profile::openstack::keystone_dcadmins { 'username':
+#   hash => hiera(admins),
+# }
 #
 define dc_profile::openstack::keystone_dcadmins (
   $hash = undef,
