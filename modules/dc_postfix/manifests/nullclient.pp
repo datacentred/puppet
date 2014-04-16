5393ec400724d4470e9ee98b0b555e89d78ef5ff
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-16 12:23:12 +0100

    Add null client config

---
+++ modules/dc_postfix/manifests/nullclient.pp
@@ -0,0 +1,24 @@
+# Class: dc_postfix::nullclient
+#
+# Installs postfix as a null client
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_postfix::nullclient {
+
+  include augeas
+
+  class { 'postfix':
+     relayhost           => hiera(primary_mail_server),
+     myorigin            => $::fqdn,
+     root_mail_recipient => hiera(sal01_internal_sysmail_address),
+     satellite           => true,
+   }
+
+}
