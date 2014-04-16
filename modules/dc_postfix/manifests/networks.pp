6ab2cc348e118c0a97cdffced43e261d3cb3e972
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-16 11:55:16 +0100

    Add fqdn for local mail handling

--- modules/dc_postfix/manifests/networks.pp
+++ modules/dc_postfix/manifests/networks.pp
@@ -3,7 +3,7 @@ class dc_postfix::networks {
   $client_networks = join(hiera(client_networks), ', ')
 
   postfix::config { 'mydestination':
-    value => "localhost.localdomain, localhost",
+    value => "localhost.localdomain, localhost, ${::fqdn}",
   }
   postfix::config { 'mynetworks':
     value => "127.0.0.0/8, ${client_networks}",
