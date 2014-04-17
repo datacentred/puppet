b6d2741e4fa6ec4eb9495e9db2f2a2c83e07908a
Sean Handley <sean.handley@gmail.com>
2014-04-17 22:53:30 +0100

    Reorganise manifest.

--- modules/dc_rails/manifests/init.pp
+++ modules/dc_rails/manifests/init.pp
@@ -12,6 +12,10 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class dc_rails {
+  include ::ruby
+  include ::bundler
+  contain 'ruby'
+  contain 'bundler'
 
   $user = 'rails'
   $password = 'secret'
@@ -20,40 +24,6 @@ class dc_rails {
   $ruby = '2.0.0-p451'
   $app_home = '/home/rails/soleman'
   $app_repo = 'https://github.com/seanhandley/helloworld.git'
-
-  package { 'git' :
-    ensure => present,
-    name   => 'git-core',
-  }
-
-  group { 'sudo' :
-    ensure => present,
-  }
-
-  user { $user :
-    ensure     => present,
-    groups     => 'sudo',
-    shell      => '/bin/bash',
-    managehome => true,
-    home       => $home,
-    password   => $password,
-    require    => Group['sudo']
-  }
-
-  class { 'sudo': }
-    sudo::conf { 'rails_sudo':
-    priority => 10,
-    content  => "${user} ALL=(ALL) NOPASSWD: ALL",
-  }
-
-  file { '/home/rails/.ssh' :
-    ensure => 'directory',
-    owner  => $user,
-    group  => $group,
-    mode   => '0700',
-  }
-
-  class { 'dc_mariadb': }
 
   class { 'nginx': manage_repo => false }
 
@@ -67,10 +37,50 @@ class dc_rails {
     proxy => 'http://rails',
   }
 
-  include ::ruby
-  include ::bundler
-  contain 'ruby'
-  contain 'bundler'
+  package { 'git' :
+    ensure => present,
+    name   => 'git-core',
+  } ->
+
+  user { $user :
+    ensure     => present,
+    groups     => 'sudo',
+    shell      => '/bin/bash',
+    managehome => true,
+    home       => $home,
+    password   => $password,
+  } ->
+
+  file { '/home/rails/.ssh' :
+    ensure => 'directory',
+    owner  => $user,
+    group  => $group,
+    mode   => '0700',
+  } ->
+
+  file { ['/var/log/rails/', '/var/run/rails/']:
+    ensure => directory,
+    owner  => $user,
+    group  => $group;
+  } ->
+
+  file { $app_home :
+    ensure => directory,
+    owner  => $user,
+    group  => $group;
+  } ->
+
+  git::repo{'soleman':
+    path   => $app_home,
+    source => $app_repo,
+    owner  => $user,
+  } ->
+
+  file { '/home/rails/soleman/log/production.log' :
+    owner  => $user,
+    group  => $group,
+    mode   => '0666',  
+  } ->
 
   rbenv::install { $user:
     group => $group,
@@ -88,17 +98,7 @@ class dc_rails {
     ruby => $ruby,
   } ->
 
-  file { $app_home :
-    ensure => directory,
-    owner  => $user,
-    group  => $group;
-  } ->
-
-  git::repo{'soleman':
-    path   => $app_home,
-    source => $app_repo,
-    owner  => $user,
-  } ->
+  class{'ruby::dev':} ->
 
   package { 'libmariadbclient-dev' :
     ensure => present,
@@ -109,6 +109,17 @@ class dc_rails {
     cwd         => $app_home,
     group       => $group,
     user        => $user,
+  } ->
+
+  exec { 'bundle binstubs unicorn':
+    command     => '/home/rails/.rbenv/shims/bundle binstubs unicorn',
+    cwd         => $app_home,
+    group       => $group,
+    user        => $user,
+  } ->
+
+  class { 'dc_mariadb': 
+    maria_root_pw => 'secret'
   } ->
 
   exec { 'rake db:create':
@@ -127,16 +138,11 @@ class dc_rails {
     environment => 'RAILS_ENV=production',
   } ->
 
-  file { ['/var/log/rails/', '/var/run/rails/']:
-    ensure => directory,
-    owner  => $user,
-    group  => $group;
-  } ->
-
-  file { '/home/rails/soleman/log/production.log' :
-    owner  => $user,
-    group  => $group,
-    mode   => '0666',  
+  exec { 'rbenv-init':
+    command     => "/bin/bash -c 'eval \"$(rbenv init -)\"'",
+    cwd         => $app_home,
+    group       => $group,
+    user        => $user,
   } ->
 
   unicorn::app { 'soleman':
@@ -151,8 +157,8 @@ class dc_rails {
     rack_env         => 'production',
     secret_key_base  => '91fe5d494f16b1f3bff432c65d1b30a39e8881c0e842ab607f78f44260ea27f5da3b7c24b5347a57c3059858435b8fc6b2f918bc8fb516c34caecd7810aea7e0',
     source           => '/home/rails/.rbenv/shims/unicorn',
-    require          => [
-      Class['ruby::dev'],
-    ],
+    # subscribe => Exec['bundle binstubs unicorn'],
   }
+
+
 }
