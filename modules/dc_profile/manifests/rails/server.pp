f5287aea8f720ba3019ab12a8bf68d1b2ffae6de
Sean Handley <sean.handley@gmail.com>
2014-04-22 14:01:58 +0100

    Profiles and roles for soleman.

---
+++ modules/dc_profile/manifests/rails/server.pp
@@ -0,0 +1,17 @@
+# Class: dc_profile::rails::server
+#
+# Provisions a node as a Rails server
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::rails::server {
+
+  contain 'dc_rails'
+
+}
--- modules/dc_role/manifests/railsserver.pp
+++ modules/dc_role/manifests/railsserver.pp
@@ -1,6 +1,6 @@
-# Class: dc_role::railsserver
+# Class: dc_role::soleman
 #
-# General-purpose Rails server
+# Management console server
 #
 # Parameters:
 #
@@ -10,6 +10,14 @@
 #
 # Sample Usage:
 #
-class dc_role::railsserver {
+class dc_role::soleman {
   contain dc_profile::rails::server
+
+  class { 'dc_rails':
+    app_name           => 'soleman',
+    app_url            => 'soleman.dev',
+    app_repo           => 'git@github.com:datacentred/soleman.git',
+    ssl_key            => 'puppet:///modules/dc_ssl/soleman/soleman.dev.key',
+    ssl_cert           => 'puppet:///modules/dc_ssl/soleman/soleman.dev.crt',
+  }
 }
