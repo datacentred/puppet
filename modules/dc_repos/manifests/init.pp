caa4126e71a24edda811ef89306d627d4cd840e2
Simon Murray <simon.murray@datacentred.co.uk>
2014-04-22 13:06:42 +0100

    Trusty Mirror Support
    * Add trusty mirrors to those repos that have them
    * Separate out precise/trusty repo lists
    * Make hiera check against the distro version for repo
      lists
    * dc_repos calls hiera_hash to merge common and distro
      specific repo lists

--- modules/dc_repos/manifests/init.pp
+++ modules/dc_repos/manifests/init.pp
@@ -15,6 +15,6 @@ class dc_repos {
   $aptsource_defaults = {
     include_src => false,
   }
-  create_resources(apt::source, hiera(repo_list), $aptsource_defaults)
+  create_resources(apt::source, hiera_hash(repo_list), $aptsource_defaults)
 
 }
