91553025bb42afdf35a6fb9039d81f60f34bf4f9
Nick Jones <nick.jones@datacentred.co.uk>
2014-04-23 16:17:59 +0100

    Install from t'Internet, slight refactoring as a result

--- modules/dc_packer/manifests/install.pp
+++ modules/dc_packer/manifests/install.pp
@@ -1,35 +1,53 @@
+# Class: dc_packer::install
+#
+# Installs the Packer binaries and the supporting DC components
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
 class dc_packer::install {
 
-  $packer_home = '/home/packer'
-  $packer_pass = hiera(packer_pass)
+  $packer_home    = '/home/packer'
+  $packer_package = '0.5.2_linux_amd64.zip'
 
   file { $packer_home:
     ensure  => directory,
+    owner   => packer,
     source  => 'puppet:///modules/dc_packer',
+  }
+
+  file {  [ "${packer_home}/bin",
+            "${packer_home}/output",
+            "${packer_home}/http",
+          ]:
+    ensure  => directory,
     recurse => true,
     owner   => 'packer',
   }
 
   file { 'preseedme.rb':
     ensure  => file,
-    path    => "$packer_home/bin/preseedme.rb",
+    path    => "${packer_home}/bin/preseedme.rb",
     content => template('dc_packer/preseedme.rb'),
     owner   => 'packer',
     mode    => '0744',
+    require => File["${packer_home}/bin"],
   }
 
-  file { [ "$packer_home/output", "$packer_home/http" ]:
-    ensure => directory,
-    owner  => 'packer',
+  package { [ 'ruby1.9.3', 'virtualbox-4.3', 'unzip']:
+    ensure  => installed,
   }
 
-  # Need this for some shady Ruby script...
-  package { 'ruby1.9.3':
-    ensure => installed,
-  }
-
-  package { 'virtualbox-4.3':
-    ensure => installed,
+  exec { 'packer_binaries':
+    command => "wget -q https://dl.bintray.com/mitchellh/packer/${packer_package} && unzip ${packer_package}",
+    cwd     => "${packer_home}/bin",
+    path    => [ '/usr/bin' ],
+    require => [ File[$packer_home], Package['unzip'] ],
   }
 
 }
