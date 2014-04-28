f21dfda1d97605cce548414bec44b1e5bc7e23f3
Rob Greenwood <bilco105@gmail.com>
2014-04-28 13:05:08 +0100

    add mount

---
+++ modules/dc_juniperbackup/manifests/exports.pp
@@ -0,0 +1,32 @@
+# Class: dc_juniperbackup::exports
+#
+# Exports the file and nfs export classes for the server to pick up
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class dc_juniperbackup::exports {
+
+  $storagedir = hiera('storagedir')
+
+  @@file { "$::hostname-juniperbackup":
+    ensure  => directory,
+    path    => "${storagedir}/backups/$::hostname-juniperbackup",
+    require => File["${storagedir}/backups"],
+    tag     => 'backups',
+  }
+
+  @@nfs::server::export { "${storagedir}/backups/$::hostname-juniperbackup":
+    ensure  => present,
+    require => File["$::hostname-juniperbackup"],
+    clients => "$::ipaddress(rw,insecure,async,no_root_squash,no_subtree_check)",
+    tag     => 'backups',
+  }
+
+}
--- modules/dc_juniperbackup/manifests/init.pp
+++ modules/dc_juniperbackup/manifests/init.pp
@@ -12,28 +12,21 @@
 #
 class dc_juniperbackup {
 
-  $storagedir = hiera('storagedir')
-  $password   = hiera('juniper_backups_password')
+  $password = hiera('juniper_backups_password')
 
   user { 'juniperbackup':
     comment    => 'Juniper Backups Storage User',
     password   => generate('/bin/sh', '-c', "mkpasswd -m sha-512 ${password} | tr -d '\n'"),
+    home       => '/var/juniperbackup', 
     managehome => true,
     system     => true,
   }
 
-  @@file { "$::hostname-juniperbackup":
-    ensure  => directory,
-    path    => "${storagedir}/backups/$::hostname-juniperbackup",
-    require => File["${storagedir}/backups"],
-    tag     => 'backups',
+  Nfs::Client::Mount <<| nfstag == "${::hostname}-juniperbackup" |>> {
+    ensure  => mounted,
+    mount   => '/var/juniperbackup-remote',
   }
 
-  @@nfs::server::export { "${storagedir}/backups/$::hostname-juniperbackup":
-    ensure  => present,
-    require => File["$::hostname-juniperbackup"],
-    clients => "$::ipaddress(rw,insecure,async,no_root_squash,no_subtree_check)",
-    tag     => 'backups',
-  }
+  contain dc_juniperbackup::exports
 
 }
