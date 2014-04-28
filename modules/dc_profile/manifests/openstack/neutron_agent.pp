accbaaa2bfc23c5ecd7ecaab1f2ca27228d5dfe3
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-28 15:29:41 +0100

    Move common code into a new class and include into roles

--- modules/dc_profile/manifests/openstack/neutron_agent.pp
+++ modules/dc_profile/manifests/openstack/neutron_agent.pp
@@ -157,25 +157,4 @@ class dc_profile::openstack::neutron_agent {
     }
   }
 
-  # Nagios checks common to both network and compute node
-  file { '/etc/nagios/nrpe.d/os_neutron_vswitch_agent.cfg':
-    ensure  => present,
-    content => 'command[check_neutron_vswitch_agent]=/usr/lib/nagios/plugins/check_procs -c 1 -u neutron -a neutron-openvswitch-agent',
-    require => Package['nagios-nrpe-server'],
-    notify  => Service['nagios-nrpe-server'],
-  }
-
-  file { '/etc/nagios/nrpe.d/os_ovswitch_proc.cfg':
-    ensure  => present,
-    content => 'command[check_ovswitch_proc]=/usr/lib/nagios/plugins/check_procs -w 2: -C ovs-vswitchd',
-    require => Package['nagios-nrpe-server'],
-    notify  => Service['nagios-nrpe-server'],
-  }
-
-  file { '/etc/nagios/nrpe.d/os_ovswitch_server_proc.cfg':
-    ensure  => present,
-    content => 'command[check_ovswitch_server_proc]=/usr/lib/nagios/plugins/check_procs -w 2: -C ovsdb-server',
-    require => Package['nagios-nrpe-server'],
-    notify  => Service['nagios-nrpe-server'],
-  }
 }
