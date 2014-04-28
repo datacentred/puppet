917eeed5af53ea2ace044722b28b3af33fe61a80
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-28 15:27:26 +0100

    Create common neutron class

---
+++ modules/dc_profile/manifests/openstack/neutron_common.pp
@@ -0,0 +1,34 @@
+# Class: dc_profile::openstack::neutron_common
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::openstack::neutron_common {
+
+  file { '/etc/nagios/nrpe.d/os_neutron_vswitch_agent.cfg':
+    ensure  => present,
+    content => 'command[check_neutron_vswitch_agent]=/usr/lib/nagios/plugins/check_procs -c 1 -u neutron -a neutron-openvswitch-agent',
+    require => Package['nagios-nrpe-server'],
+    notify  => Service['nagios-nrpe-server'],
+  }
+
+  file { '/etc/nagios/nrpe.d/os_ovswitch_proc.cfg':
+    ensure  => present,
+    content => 'command[check_ovswitch_proc]=/usr/lib/nagios/plugins/check_procs -w 2: -C ovs-vswitchd',
+    require => Package['nagios-nrpe-server'],
+    notify  => Service['nagios-nrpe-server'],
+  }
+
+  file { '/etc/nagios/nrpe.d/os_ovswitch_server_proc.cfg':
+    ensure  => present,
+    content => 'command[check_ovswitch_server_proc]=/usr/lib/nagios/plugins/check_procs -w 2: -C ovsdb-server',
+    require => Package['nagios-nrpe-server'],
+    notify  => Service['nagios-nrpe-server'],
+  }
+
+}
