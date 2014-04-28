dbaa9dbeb81e6ec2e847c7995de6843494206f8f
Rob Greenwood <bilco105@gmail.com>
2014-04-28 11:02:09 +0100

    juniper backups and NFS backup mount

---
+++ modules/dc_juniperbackup/manifests/init.pp
@@ -0,0 +1,38 @@
+# Class: dc_profile::net::juniper_backup
+#
+# Juniper switch backups
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::net::juniper_backups {
+
+  $storagedir = hiera('storagedir')
+
+  user { 'juniperbackup':
+    comment    => 'Juniper Backups Storage User',
+    shell      => '/sbin/nologin',
+    password   => sha1(hiera('juniper_backups_password')),
+    managehome => true,
+  }
+
+  @@file { "$::hostname-juniperbackup":
+    ensure  => directory,
+    path    => "${storagedir}/backups/$::hostname-juniperbackup",
+    require => File["${storagedir}/backups"],
+    tag     => 'backups',
+  }
+
+  @@nfs::server::export { "${storagedir}/backups/$::hostname-juniperbackup":
+    ensure  => present,
+    require => File["$::hostname-juniperbackup"],
+    clients => "$::ipaddress(rw,insecure,async,no_root_squash,no_subtree_check)",
+    tag     => 'backups',
+  }
+
+}
