366ee4911ba67afa55acfca07cac700a13ef7809
Rob Greenwood <bilco105@gmail.com>
2014-04-28 14:27:11 +0100

    refactor into single profile and setup tidy/nfs backups

--- modules/dc_profile/manifests/net/juniper_backups.pp
+++ modules/dc_profile/manifests/net/juniper_backups.pp
@@ -11,5 +11,49 @@
 # Sample Usage:
 #
 class dc_profile::net::juniper_backups {
-  contain dc_juniperbackup
+
+  $juniper_username  = hiera('juniper_backups_username')
+  $juniper_password  = hiera('juniper_backups_password')
+  $juniper_directory = hiera('juniper_backups_directory')
+  $storagedir        = hiera('storagedir')
+
+  user { $juniper_username:
+    comment    => 'Juniper Backups Storage User',
+    password   => generate('/bin/sh', '-c', "mkpasswd -m sha-512 ${juniper_password} | tr -d '\n'"),
+    home       => $juniper_directory,
+    managehome => true,
+    system     => true,
+  }
+
+  Nfs::Client::Mount <<| nfstag == "${::hostname}-juniperbackup" |>> {
+    ensure  => mounted,
+    mount   => "${juniper_directory}-remote",
+  }
+
+  tidy { $juniper_directory:
+    age => '12w',
+  }
+
+  cron { "juniperbackup-to-nfs":
+    command => "rsync -r --delete ${juniper_directory} ${juniper_directory}-remote",
+    hour    => 0,
+    minute  => 0,
+    require => Tidy[$juniper_directory],
+  }
+
+  @@file { "$::hostname-juniperbackup":
+    ensure  => directory,
+    path    => "${storagedir}/backups/$::hostname-juniperbackup",
+    require => File["${storagedir}/backups"],
+    tag     => 'backups',
+  }
+
+  @@nfs::server::export { "${storagedir}/backups/$::hostname-juniperbackup":
+    ensure  => present,
+    require => File["$::hostname-juniperbackup"],
+    clients => "$::ipaddress(rw,insecure,async,no_root_squash,no_subtree_check)",
+    tag     => 'backups',
+    nfstag  => "${::hostname}-juniperbackup",
+  }
+
 }
