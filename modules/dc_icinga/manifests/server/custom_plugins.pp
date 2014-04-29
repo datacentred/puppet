ed3181fddebb47dfbef13c6896a370874b04a6e6
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-04-29 11:23:06 +0100

    Add defaults

--- modules/dc_icinga/manifests/server/custom_plugins.pp
+++ modules/dc_icinga/manifests/server/custom_plugins.pp
@@ -1,91 +1,54 @@
 class dc_icinga::server::custom_plugins {
 
-# Custom nagios plugins
-  file { '/usr/lib/nagios/plugins/check_tftp':
+  File {
     ensure => file,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
+  }
+
+# Custom nagios plugins
+  file { '/usr/lib/nagios/plugins/check_tftp':
     source => 'puppet:///modules/dc_icinga/check_tftp',
   }
 
   file { '/usr/lib/nagios/plugins/check_keystone':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_keystone',
   }
 
   file { '/usr/lib/nagios/plugins/check_foreman':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_foreman',
   }
 
   file { '/usr/lib/nagios/plugins/check_rabbitmq_aliveness':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_rabbitmq_aliveness',
   }
 
   file { '/usr/lib/nagios/plugins/check_rabbitmq_objects':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_rabbitmq_objects',
   }
 
   file { '/usr/lib/nagios/plugins/check_rabbitmq_overview':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_rabbitmq_overview',
   }
 
   file { '/usr/lib/nagios/plugins/check_rabbitmq_partition':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_rabbitmq_partition',
   }
 
   file { '/usr/lib/nagios/plugins/check_rabbitmq_queue':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_rabbitmq_queue',
   }
 
   file { '/usr/lib/nagios/plugins/check_rabbitmq_server':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_rabbitmq_server',
   }
 
   file { '/usr/lib/nagios/plugins/check_rabbitmq_shovels':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_rabbitmq_shovels',
   }
 
   file { '/usr/lib/nagios/plugins/check_rabbitmq_watermark':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
     source => 'puppet:///modules/dc_icinga/check_rabbitmq_watermark',
   }
 
