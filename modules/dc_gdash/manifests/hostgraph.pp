25d32eeaa9ad00027fb951c09d2efaab8181124f
Nick Jones <nick.jones@datacentred.co.uk>
2014-05-01 16:08:41 +0100

    Ensure target directory exists before graph template is created

--- modules/dc_gdash/manifests/hostgraph.pp
+++ modules/dc_gdash/manifests/hostgraph.pp
@@ -1,3 +1,16 @@
+# Class: dc_gdash::hostgraph
+#
+# Creates a graph template in the host's graph folder
+# per interface
+#
+# Parameters: Title composed of ifname#shorthostname
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage: dc_gdash::hostgraph { 'hostname': }
+#
 define dc_gdash::hostgraph (
   $_hostname = $title,
 ) {
@@ -5,21 +18,24 @@ define dc_gdash::hostgraph (
   $tplpath = '/var/www/gdash/graph_templates'
   $hostpath = "${tplpath}/hosts/${_hostname}"
 
-  file { "${hostpath}":
+  file { $hostpath:
     ensure  => directory,
     recurse => true,
   }
 
-  file { "${hostpath}/dash.yaml": 
+  file { "${hostpath}/dash.yaml":
     content => template('dc_gdash/dash.yaml.erb'),
+    require => File[$hostpath],
   }
 
-  file { "${hostpath}/load.graph": 
+  file { "${hostpath}/load.graph":
     content => template('dc_gdash/load.graph.erb'),
+    require => File[$hostpath],
   }
 
-  file { "${hostpath}/memory.graph": 
+  file { "${hostpath}/memory.graph":
     content => template('dc_gdash/memory.graph.erb'),
+    require => File[$hostpath],
   }
 
 }
