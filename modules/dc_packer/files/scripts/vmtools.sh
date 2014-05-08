6b216e36884ee2ae9106249b673b15a3bc73108f
Nick Jones <nick.jones@datacentred.co.uk>
2014-05-08 10:41:16 +0100

    Helps if you update the right script...

--- modules/dc_packer/files/scripts/vmtools.sh
+++ modules/dc_packer/files/scripts/vmtools.sh
@@ -16,12 +16,6 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
 	# Without libdbus virtualbox would not start automatically after compile
 	apt-get -y install --no-install-recommends libdbus-1-3
 	
-	# The netboot installs the VirtualBox support (old) so we have to remove it
-	/etc/init.d/virtualbox-ose-guest-utils stop
-	rmmod vboxguest
-	aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
-	aptitude -y install dkms
-	
 	# Install the VirtualBox guest additions
 	VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 	VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
@@ -31,4 +25,8 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
 	
 	#Cleanup VirtualBox
 	rm $VBOX_ISO
+
+	# Workaround for the guest additions being broken on 4.3.10
+	# See https://www.virtualbox.org/ticket/12879
+	ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
 fi
