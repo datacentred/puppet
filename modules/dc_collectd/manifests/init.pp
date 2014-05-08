3d5252ce47e702cdd9ba187033613f425546f0b7
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-08 16:09:30 +0100

    Remove disks config so we can test seperately

--- modules/dc_collectd/manifests/init.pp
+++ modules/dc_collectd/manifests/init.pp
@@ -54,18 +54,6 @@ class dc_collectd (
   # interface
   @@dc_gdash::nettraf { $ifhashhost: }
 
-  # For disks we have 3 seperate facts which contain the devices
-  # FIXME handle only having one entry in the string )
-  # FIXME sort
-  $mdarray = split($::software_raid, ',')
-  $disksarray = split($::disks, ',')
-  $partsarray = split($::partitions, ',')
-  #$joinedarray = [ $mdarray, $disksarray, $partsarray ]
-  $joinedarray = [ split($::software_raid, ','), split($::disks, ','), split($::partitions, ',') ]
-  $diskhashhost = suffix($joinedarray, "#${::hostname}")
-
-  @@dc_gdash_disk { $diskhashhost: }
-
   # If this is defined (currently set as a top-scope variable by Foreman),
   # then configure collectd to gather statistics from SNMP-enabled devices
   if $::snmptargets {
