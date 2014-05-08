9b6dd0d74b39af82259f1975d732a1fd50720aa7
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-08 14:30:46 +0100

    Add profile class

---
+++ modules/dc_profile/manifests/util/facter.pp
@@ -0,0 +1,17 @@
+# Class: dc_profile::util::facter
+#
+# Allows the loading of custom facts on a node
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::util::facter {
+
+  contain dc_facter
+
+}
