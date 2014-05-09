f26cd52b0b5147cff243087ad462b93d8cb7cbf9
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-09 16:41:13 +0100

    Should be split not join

--- modules/dc_collectd/manifests/disks.pp
+++ modules/dc_collectd/manifests/disks.pp
@@ -3,9 +3,9 @@ class dc_collectd::disks {
   # For disks we have 3 separate facts which may contain the devices
   
   if $::software_raid {
-    $joined_array = join("$::disks,$::partitions,$::software_raid",',')
+    $joined_array = split("$::disks,$::partitions,$::software_raid",',')
   } else {
-    $joined_array = join("$::disks,$::partitions",',')
+    $joined_array = split("$::disks,$::partitions",',')
   }
 
   $diskhashhost = suffix($joined_array, "#${::hostname}")
