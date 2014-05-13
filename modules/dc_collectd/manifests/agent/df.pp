bff2808d31409696519b1a6ce7934182355d59eb
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-13 10:00:02 +0100

    I'll try this for a dollar - going to need more poking I suspect

--- modules/dc_collectd/manifests/agent/df.pp
+++ modules/dc_collectd/manifests/agent/df.pp
@@ -2,9 +2,11 @@ class dc_collectd::agent::df {
 
   # convert root
   $df_array_root = regsubst(split($::mounts, ','), '^/$', 'root')
-  # now convert all the slashes to underscores
+  # Remove leading slash altogether
+  $df_array_deslash = regsubst($df_array_root, ','), '^/', '')
+  # now convert all the remaining slashes to underscores
   # These then need converting to dashes in the gdash template
-  $df_array_convert = regsubst($df_array_root, '/', '_', 'G')
+  $df_array_convert = regsubst($df_array_deslash, '/', '_', 'G')
 
   $dfhashhost = suffix($df_array, "#${::hostname}")
 
