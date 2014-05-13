1f06b373efcc0bc2044adfa5072e518a7d576b1a
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-13 16:10:38 +0100

    Fix typo

--- modules/dc_collectd/manifests/agent/df.pp
+++ modules/dc_collectd/manifests/agent/df.pp
@@ -3,7 +3,7 @@ class dc_collectd::agent::df {
   # convert root
   $df_array_root = regsubst(split($::mounts, ','), '^/$', 'root')
   # Remove leading slash altogether
-  $df_array_deslash = regsubst($df_array_root, ','), '^/', '')
+  $df_array_deslash = regsubst($df_array_root, '^/', '')
   # now convert all the remaining slashes to underscores
   # These then need converting to dashes in the gdash template
   $df_array_convert = regsubst($df_array_deslash, '/', '_', 'G')
