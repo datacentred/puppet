4d14936a371831b6405d547ed9f9dd9847bc639b
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-13 09:20:21 +0100

    Fix characters in mounts

--- modules/dc_collectd/manifests/agent/df.pp
+++ modules/dc_collectd/manifests/agent/df.pp
@@ -1,6 +1,10 @@
 class dc_collectd::agent::df {
 
-  $df_array = split($::mounts,',')
+  # convert root
+  $df_array_root = regsubst(split($::mounts, ',', '^/$', 'root')
+  # now convert all the slashes to underscores
+  # These then need converting to dashes in the gdash template
+  $df_array_convert = regsubst($df_array_root, '/', '_', 'G')
 
   $dfhashhost = suffix($df_array, "#${::hostname}")
 
