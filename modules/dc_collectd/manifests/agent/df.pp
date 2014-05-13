bb7f67abf8ca564881774cfecee8f34435efa9e4
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-13 09:33:18 +0100

    Fix syntax error

--- modules/dc_collectd/manifests/agent/df.pp
+++ modules/dc_collectd/manifests/agent/df.pp
@@ -1,7 +1,7 @@
 class dc_collectd::agent::df {
 
   # convert root
-  $df_array_root = regsubst(split($::mounts, ',', '^/$', 'root')
+  $df_array_root = regsubst(split($::mounts, ','), '^/$', 'root')
   # now convert all the slashes to underscores
   # These then need converting to dashes in the gdash template
   $df_array_convert = regsubst($df_array_root, '/', '_', 'G')
