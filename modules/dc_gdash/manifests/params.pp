ae994021ce75f9636d7c0f308de1c10ca2883a57
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-15 11:54:06 +0100

    Add debugging

--- modules/dc_gdash/manifests/params.pp
+++ modules/dc_gdash/manifests/params.pp
@@ -2,4 +2,5 @@ class dc_gdash::params {
   $graphite_server = hiera(graphite_server)
   $whisper_root = '/var/opt/graphite/storage/whisper'
   $reversedomain = join(reverse(split($::domain, '.')),'.')
+  notify{"reversedomain is $reversedomain":}
 }
