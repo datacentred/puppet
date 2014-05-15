d27bef231cd93a0ba959a024bfe1dd29b5da6730
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-15 17:23:49 +0100

    Add switch graph creation

---
+++ modules/dc_gdash/manifests/swnettraf.pp
@@ -0,0 +1,42 @@
+# Class: dc_gdash::swnettraf
+#
+# Creates a graph template in the host's graph folder
+# per interface
+#
+# Parameters: Title composed of ifname#shorthostname
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage: dc_gdash::swnettraf { 'eth0#gdash': }
+#
+define dc_gdash::swnettraf (
+  $_hostname = regsubst($title, '.*\#', '\1'),
+  $_interface = regsubst($title, '\#.*', '\1'),
+  $reversedomain,
+) {
+
+  $tplpath = '/var/www/gdash/graph_templates/switches'
+  $hostpath = "${tplpath}/${_hostname}"
+
+  ensure_resource('file', $hostpath, { 'ensure' => 'directory', 'purge' => 'true' })
+
+  # Evaluate the template outside of the ensure_resource function
+  # as it doesn't get evaluated for some reason
+  $yaml = template('dc_gdash/nettraf.dash.yaml.erb')
+          
+  ensure_resource('file', "${hostpath}/dash.yaml", { 'ensure' => 'present', 'content' => "$yaml", 'require' => "File[$hostpath]"})
+
+  file { "${hostpath}/nettraf.${_interface}.graph": 
+    content => template('dc_gdash/nettraf.graph.erb'),
+  }
+  
+  file { "${hostpath}/netpackets.${_interface}.graph": 
+    content => template('dc_gdash/netpackets.graph.erb'),
+  }
+
+  file { "${hostpath}/neterrors.${_interface}.graph": 
+    content => template('dc_gdash/neterrors.graph.erb'),
+  }
+}
