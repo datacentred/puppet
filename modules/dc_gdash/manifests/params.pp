3bb6e6658f9dea24b7064d7751639394664cd093
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-15 11:50:14 +0100

    Remove brackets

--- modules/dc_gdash/manifests/params.pp
+++ modules/dc_gdash/manifests/params.pp
@@ -1,5 +1,5 @@
 class dc_gdash::params {
   $graphite_server = hiera(graphite_server)
   $whisper_root = '/var/opt/graphite/storage/whisper'
-  $reversedomain = join(reverse(split(${::domain}, '.')),'.')
+  $reversedomain = join(reverse(split($::domain, '.')),'.')
 }
