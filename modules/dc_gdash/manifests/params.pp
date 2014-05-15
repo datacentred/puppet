404ee4a44d1e6d352e865a84681c1182ccbd8093
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-15 12:00:03 +0100

    Fix escaping

--- modules/dc_gdash/manifests/params.pp
+++ modules/dc_gdash/manifests/params.pp
@@ -1,6 +1,5 @@
 class dc_gdash::params {
   $graphite_server = hiera(graphite_server)
   $whisper_root = '/var/opt/graphite/storage/whisper'
-  $reversedomain = join(reverse(split($::domain, '.')),'.')
-  notify{"reversedomain is $reversedomain":}
+  $reversedomain = join(reverse(split($::domain,'[.]')),'.')
 }
