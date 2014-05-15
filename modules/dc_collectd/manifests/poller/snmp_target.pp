7012c0096b397b2795bbfb409b00c34c512ef17f
Rob Greenwood <bilco105@gmail.com>
2014-05-15 10:24:24 +0100

    add logic for graphing support in dc_collectd::poller::snmp_target

--- modules/dc_collectd/manifests/poller/snmp_target.pp
+++ modules/dc_collectd/manifests/poller/snmp_target.pp
@@ -11,11 +11,20 @@ define dc_collectd::poller::snmp_target (
     $snmp_privacy_protocol,
     $snmp_privacy_password,
     $interval = 60,
+    $graphs = [],
 ) {
 
+  # Create the collectd SNMP host object
   concat::fragment { $title:
     target  => '/etc/collectd/conf.d/snmp.conf',
     content => template('dc_collectd/poller/snmpconf_main.erb'),
   }
+
+  # Ensure graphs is always an array and pass through bracket expansion
+  $_graphs = any2array($graphs)
+  $_expanded_graphs = bracket_expansion($graphs)
+
+  # Create graphs
+  notify { $_expanded_graphs: }
 
 }
