3b6eda55145f5272ba4e2769d3b63599ce3cab28
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-19 16:10:52 +0100

    Correct file_line declaration

--- modules/dc_collectd/manifests/agent/rabbitmq.pp
+++ modules/dc_collectd/manifests/agent/rabbitmq.pp
@@ -9,8 +9,9 @@ class dc_collectd::agent::rabbitmq (
   include stdlib
 
   # Add new rabbit type to types.db
-  file_line { '/usr/share/collectd/types.db':
+  file_line { "${::hostname}_rabbitmq_types.db":
     line    => 'rabbitmq                messages:GAUGE:0:U, messages_rate:GAUGE:0:U, messages_unacknolwedged:GAUGE:0:U, messages_unacknowledged_rate:GAUGE:0:U, messages_ready:GAUGE:0:U, message_ready_rate:GAUGE:0:U, memory:GAUGE:0:U, consumers:GAUGE:0:U, publish:GAUGE:0:U, publish_rate:GAUGE:0:U, deliver_no_ack:GAUGE:0:U, deliver_no_ack_rate:GAUGE:0:U, deliver_get:GAUGE:0:U, deliver_get_rate:GAUGE:0:U',
+    path    => '/usr/share/collectd/types.db',
     require => Class['collectd'],
   }
 
