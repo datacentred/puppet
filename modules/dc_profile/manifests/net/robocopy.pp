96742f445affd32d4b56dc81ceeea0629e133e22
Simon Murray <simon.murray@datacentred.co.uk>
2014-05-22 15:03:15 +0100

    Roles, profiles and class names

---
+++ modules/dc_profile/manifests/net/robocopy.pp
@@ -0,0 +1,17 @@
+# Class: dc_profile::net::robocopy
+#
+# vhost to hold robocopy resources
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::net::robocopy {
+
+  contain ::robocopy
+
+}
