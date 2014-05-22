4ca51efafc834ca6ec006abacfdbb00cb26cb6fc
Rob Greenwood <bilco105@gmail.com>
2014-05-22 15:09:57 +0100

    move to dynamic radius vhost

--- modules/dc_profile/manifests/auth/radius/server.pp
+++ modules/dc_profile/manifests/auth/radius/server.pp
@@ -14,7 +14,32 @@ class dc_profile::auth::radius::server {
   include ::radius
   include ::radius::module::ldap
 
-  radius::vhost { 'datacentred': }
+  radius::vhost { 'datacentred':
+    authorize    => ['ldap'],
+    authenticate => ['ldap'],
+    post_auth    => ['ldap'],
+  }
+
+  radius::vhost::listen {
+    'dc_auth':
+      vhost  => 'datacentred',
+      ipaddr => '0.0.0.0',
+      port   => '0',
+      type   => 'auth';
+    'dc_acct':
+      vhost  => 'datacentred',
+      ipaddr => '0.0.0.0',
+      port   => '0',
+      type   => 'acct',
+  }
+
+  radius::vhost::client {
+    'datacentred':
+      vhost   => 'datacentred',
+      ipaddr  => '0.0.0.0',
+      netmask => '0',
+      secret  => 'kJvRRR.6';
+  }
 
   @@dns_resource { "radius.${::domain}/CNAME":
     rdata => $::fqdn,
