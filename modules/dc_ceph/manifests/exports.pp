3810b152f8dd61411a58f5a57593de6e160928f9
Simon Murray <simon.murray@datacentred.co.uk>
2014-05-23 11:01:38 +0100

    Backup the ceph keys

---
+++ modules/dc_ceph/manifests/exports.pp
@@ -0,0 +1,36 @@
+# Class: dc_ceph::exports
+#
+# NFS exports for ceph key backups
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_ceph::exports {
+
+  if $::hostname == hiera(cephdeploy_primary_mon) {
+
+    $storagedir = hiera(storagedir)
+
+    @@file { 'ceph-keybackup':
+      ensure  => directory,
+      path    => "${storagedir}/backups/ceph-keybackup",
+      require => File["${storagedir}/backups"],
+      tag     => 'backups',
+    }
+
+    @@nfs::server::export { "${storagedir}/backups/ceph-keybackup":
+      ensure  => present,
+      require => File['ceph-keybackup'],
+      clients => "${::ipaddress} (rw,insecure,async,no_root_squash,no_subtree_check",
+      tag     => 'backups',
+      nfstag  => 'ceph-keybackup',
+    }
+
+  }
+
+}
---
+++ modules/dc_ceph/manifests/keybackup.pp
@@ -0,0 +1,44 @@
+# Class: dc_ceph::keybackup
+#
+# Saves the keys on the primary monitor node
+# as this is needed to seed other nodes with
+# cephdeploy
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_ceph::keybackup {
+
+  if $::hostname == hiera(cephdeploy_primary_mon) {
+
+    $sos_address      = hiera(sal01_internal_sysmail_address)
+    $ceph_deploy_user = hiera(ceph_deploy_user)
+
+    include ::nfs::client
+
+    Nfs::Client::Mount <<| nfstag == 'ceph-keybackup' |>> {
+      ensure => mounted,
+      mount  => '/var/ceph-keybackup',
+    }
+
+    file { '/usr/local/bin/ceph-keybackup':
+      ensure  => file,
+      mode    => 744,
+      content => template('dc_ceph/ceph-keybackup.erb'),
+    }
+
+    cron { 'ceph-keybackup':
+      command => '/usr/local/bin/ceph-keybackup',
+      user    => 'root',
+      hour    => 4,
+      minute  => 0,
+    }
+
+  }
+
+}
