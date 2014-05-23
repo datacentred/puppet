fab4824920487d890d6144002e45aa813578f8f9
Simon Murray <simon.murray@datacentred.co.uk>
2014-05-23 12:00:03 +0100

    Incorrect variable name for deploy user

--- modules/dc_ceph/manifests/keybackup.pp
+++ modules/dc_ceph/manifests/keybackup.pp
@@ -17,7 +17,7 @@ class dc_ceph::keybackup {
   if $::hostname == hiera(cephdeploy_primary_mon) {
 
     $sos_address      = hiera(sal01_internal_sysmail_address)
-    $ceph_deploy_user = hiera(ceph_deploy_user)
+    $ceph_deploy_user = hiera(cephdeploy_user)
     $mountpoint       = '/var/ceph-keybackup'
 
     include ::nfs::client
