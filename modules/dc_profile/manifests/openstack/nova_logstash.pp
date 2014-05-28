b2f443110699bb56a8edd8ac4d19941a842453f0
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-28 16:19:49 +0100

    Fix tabs

--- modules/dc_profile/manifests/openstack/nova_logstash.pp
+++ modules/dc_profile/manifests/openstack/nova_logstash.pp
@@ -13,32 +13,32 @@
 class dc_profile::openstack::nova_logstash {
 
   dc_logstash::client::register { 'nova_api':
-    logs     => '/var/log/nova/nova-api.log',
+    logs => '/var/log/nova/nova-api.log',
     type => 'nova-api',
   }
 
   dc_logstash::client::register { 'nova_conductor':
-    logs     => '/var/log/nova/nova-conductor.log',
+    logs => '/var/log/nova/nova-conductor.log',
     type => 'nova-conductor',
   }
 
   dc_logstash::client::register { 'nova_scheduler':
-    logs     => '/var/log/nova/nova-scheduler.log',
+    logs => '/var/log/nova/nova-scheduler.log',
     type => 'nova-scheduler',
   }
 
   dc_logstash::client::register { 'nova_cert':
-    logs     => '/var/log/nova/nova-cert.log',
+    logs => '/var/log/nova/nova-cert.log',
     type => 'nova-cert',
   }
 
   dc_logstash::client::register { 'nova_manage':
-    logs     => '/var/log/nova/nova-manage.log',
+    logs => '/var/log/nova/nova-manage.log',
     type => 'nova-manage',
   }
 
   dc_logstash::client::register { 'nova_consoleauth':
-    logs     => '/var/log/nova/nova-consoleauth.log',
+    logs => '/var/log/nova/nova-consoleauth.log',
     type => 'nova-consoleauth',
   }
 
