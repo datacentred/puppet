8c8aea060d9134bbf8071370b02d38334e7c79ed
Nick Jones <nick.jones@datacentred.co.uk>
2014-05-28 16:03:41 +0100

    Fix confusingly named variables for the Glance DB bits

--- modules/dc_profile/manifests/openstack/glance.pp
+++ modules/dc_profile/manifests/openstack/glance.pp
@@ -17,20 +17,20 @@ class dc_profile::openstack::glance {
 
   $os_region = hiera(os_region)
 
-  $glance_api_db   = hiera(glance_api_db)
-  $glance_api_user = hiera(glance_api_user)
-  $glance_api_pass = hiera(glance_api_pass)
-  $glance_api_host = hiera(glance_api_host)
+  $glance_api_db      = hiera(glance_api_db)
+  $glance_api_db_user = hiera(glance_api_db_user)
+  $glance_api_db_pass = hiera(glance_api_db_pass)
+  $glance_api_db_host = hiera(glance_api_db_host)
 
-  $glance_reg_db   = hiera(glance_reg_db)
-  $glance_reg_user = hiera(glance_reg_user)
-  $glance_reg_pass = hiera(glance_reg_pass)
-  $glance_reg_host = hiera(glance_reg_host)
+  $glance_reg_db      = hiera(glance_reg_db)
+  $glance_reg_db_user = hiera(glance_reg_db_user)
+  $glance_reg_db_pass = hiera(glance_reg_db_pass)
+  $glance_reg_db_host = hiera(glance_reg_db_host)
 
   $glance_port = '9292'
 
-  $glance_api_database = "mysql://${glance_api_user}:${glance_api_pass}@${glance_api_host}/${glance_api_db}"
-  $glance_reg_database = "mysql://${glance_reg_user}:${glance_reg_pass}@${glance_reg_host}/${glance_reg_db}"
+  $glance_api_database = "mysql://${glance_api_db_user}:${glance_api_db_pass}@${glance_api_db_host}/${glance_api_db}"
+  $glance_reg_database = "mysql://${glance_reg_db_user}:${glance_reg_db_pass}@${glance_reg_db_host}/${glance_reg_db}"
 
   class { 'glance::api':
     registry_host     => 'localhost',
