dafe7ee07ec72cc86a246d24c3408cc6cdcfe386
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-05-30 11:28:29 +0100

    Add horizon config

---
+++ modules/dc_profile/manifests/openstack/horizon_logstash.pp
@@ -0,0 +1,20 @@
+# Class: dc_profile::openstack::horizon_logstash
+#
+# Configures logstash for horizon
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::openstack::horizon_logstash {
+
+  dc_logstash::client::register { 'horizon_log':
+    logs => '/var/log/horizon/horizon.log',
+    type => 'horizon',
+  }
+
+}
