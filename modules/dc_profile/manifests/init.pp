b64a9bf73da7ab638852c4453b14754756224631
Simon Murray <simon.murray@datacentred.co.uk>
2014-06-02 16:07:39 +0100

    hack in icinga client for testing

--- modules/dc_profile/manifests/init.pp
+++ modules/dc_profile/manifests/init.pp
@@ -20,6 +20,7 @@ class dc_profile {
   contain dc_profile::util::timezone
   contain dc_profile::util::locale
   contain dc_profile::util::facter
+  contain dc_profile::mon::icinga_client
 
   if $::is_vagrant == undef {
     contain dc_profile::auth::admins
@@ -30,7 +31,6 @@ class dc_profile {
       contain dc_profile::perf::collectd::agent
       contain dc_profile::net::mail
       contain dc_profile::mon::nsca_client
-      contain dc_profile::mon::icinga_client
       contain dc_profile::log::rsyslog_client
       contain dc_profile::log::logstash_forwarder
       contain dc_profile::log::logrotate
