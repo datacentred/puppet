b401af1f58e0a3d2b9af1cc91442e357feaf481a
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-06-13 12:05:29 +0100

    Add class for hp blade stuff

---
+++ modules/dc_nrpe/manifests/hpblade.pp
@@ -0,0 +1,30 @@
+# Class: dc_nrpe::hpblade
+#
+# HP Blade specific nrpe configuration
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class dc_nrpe::hpblade {
+
+  file { '/usr/lib/nagios/plugins/check_hpasm':
+    ensure  => file,
+    require => Package['nagios-nrpe-server'],
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/dc_nrpe/check_hpasm',
+  }
+
+  sudo::conf { 'check_hpasm':
+    priority => 10,
+    content  => 'nagios ALL=NOPASSWD:/usr/lib/nagios/plugins/check_hpasm',
+  }
+
+}
