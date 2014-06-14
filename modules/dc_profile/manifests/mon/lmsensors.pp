73d2b4224c4b239ab95cefe53792c5ace0fdf2a5
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-06-14 16:22:37 +0100

    Correct path

--- modules/dc_profile/manifests/mon/lmsensors.pp
+++ modules/dc_profile/manifests/mon/lmsensors.pp
@@ -20,11 +20,11 @@ class dc_profile::mon::lmsensors {
 
       package { 'lm-sensors':
         ensure => installed,
-        notify => Exec['/usr/bin/sensors-detect'],
+        notify => Exec['/usr/sbin/sensors-detect'],
       }
 
-      exec { '/usr/bin/sensors-detect':
-        command     => "/usr/bin/yes YES | /usr/bin/sensors-detect > ${dc_profile::util::semaphores}/${title}",
+      exec { '/usr/sbin/sensors-detect':
+        command     => "/usr/bin/yes YES | /usr/sbin/sensors-detect > ${dc_profile::util::semaphores}/${title}",
         require     => Package['lm-sensors'],
         notify      => [ Service['lm-sensors'], Exec['module-init-tools'] ],
         creates     => "${dc_profile::util::semaphores}/${title}",
