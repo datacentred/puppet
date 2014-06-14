a7ff5a745b7338a9a896d785a1754a89a4b1e176
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-06-14 16:12:55 +0100

    Add lmsensors class

---
+++ modules/dc_profile/manifests/mon/lmsensors.pp
@@ -0,0 +1,48 @@
+# Class: dc_profile::mon::lmsensors
+#
+# Installs lmsensors
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::mon::lmsensors {
+
+  case $::virtual {
+
+    physical: {
+
+      include dc_profile::util::semaphores
+
+      package { 'lm-sensors':
+        ensure => installed,
+        notify => Exec['/usr/bin/sensors-detect'],
+      }
+
+      exec { '/usr/bin/sensors-detect':
+        command     => "/usr/bin/yes YES | /usr/bin/sensors-detect > ${dc_profile::util::semaphores}/${title}",
+        require     => Package['lm-sensors'],
+        notify      => [ Service['lm-sensors'], Exec['module-init-tools'] ],
+        creates     => "${dc_profile::util::semaphores}/${title}",
+        refreshonly => true,
+      }
+
+      exec { 'module-init-tools':
+        command     => 'service module-init-tools restart',
+        refreshonly => true,
+      }
+
+      service { 'lm-sensors':
+        enable  => true,
+        require => Package['lm-sensors'],
+      }
+    }
+
+    default: { }
+  }
+}
+
