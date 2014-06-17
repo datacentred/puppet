440d18ac2bb3200c079d03ff035d809e0f793198
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-06-17 10:59:19 +0100

    Add runonce defined type

---
+++ modules/dc_profile/manifests/util/runonce.pp
@@ -0,0 +1,43 @@
+# Class: dc_profile::util::runonce
+#
+# Utility define to handle flag files for exec
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+define dc_profile::util::runonce ( $command, $require = [], $refreshonly = undef ) {
+
+  $semaphore_dir = '/var/lib/puppet/semaphores'
+  $require += [ File[$semaphore_dir] ]
+
+  if ! defined( File[$semaphore_dir] ) {
+
+    file { $semaphore_dir:
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+    }
+
+  }
+
+  exec { $command:
+    unless      => "ls ${semaphore_dir}/${title}",
+    path        => '/bin:/usr/bin:/sbin:/usr/sbin',
+    require     => $require,
+    refreshonly => $refreshonly,
+  } ~>
+
+  file { "${semaphore_dir}/${title}":
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+}
