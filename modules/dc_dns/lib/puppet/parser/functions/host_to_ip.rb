c40858a2263f03d47d40fec059553f3026c548bc
Nick Jones <nick.jones@datacentred.co.uk>
2014-06-19 11:47:13 +0100

    Add in custom function to resolve a given hostname to an IP address

---
+++ modules/dc_dns/lib/puppet/parser/functions/host_to_ip.rb
@@ -0,0 +1,14 @@
+require 'resolv'
+ 
+module Puppet::Parser::Functions
+  newfunction(:get_ip_addr, :type => :rvalue) do |args|
+    ip_addr_re = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/
+    hostname = args[0].strip
+    if hostname =~ ip_addr_re then return hostname end
+    begin
+      Resolv::DNS.open { |dns| return dns.getaddress hostname }
+    rescue Resolv::ResolvError
+      return ''
+    end
+  end
+end
