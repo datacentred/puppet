461349633405022d181f840fe87be9bad3bbdee7
Nick Jones <nick.jones@datacentred.co.uk>
2014-06-24 17:00:43 +0100

    Include proxyip profile class

--- modules/dc_role/manifests/osapi.pp
+++ modules/dc_role/manifests/osapi.pp
@@ -11,9 +11,11 @@
 # Sample Usage:
 #
 class dc_role::osapi {
+  contain dc_profile::openstack::proxyip
   contain dc_profile::openstack::haproxy
   contain dc_profile::openstack::keepalived
 
+  Class['Dc_profile::Openstack::Proxyip'] ~>
   Class['Dc_profile::Openstack::Keepalived'] ~>
   Class['Dc_profile::Openstack::Haproxy']
 }
