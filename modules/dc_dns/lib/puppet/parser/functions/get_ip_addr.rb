f59bed1434f06efa3a65e1f0a4ee46072edf0cf4
Nick Jones <nick.jones@datacentred.co.uk>
2014-06-25 10:06:35 +0100

    Remove OTT regex and simplify function overall

--- modules/dc_dns/lib/puppet/parser/functions/get_ip_addr.rb
+++ modules/dc_dns/lib/puppet/parser/functions/get_ip_addr.rb
@@ -2,9 +2,7 @@ require 'resolv'
  
 module Puppet::Parser::Functions
   newfunction(:get_ip_addr, :type => :rvalue) do |args|
-    ip_addr_re = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/
     hostname = args[0].strip
-    if hostname =~ ip_addr_re then return hostname end
     begin
       Resolv.getaddress hostname
     rescue Resolv::ResolvError
