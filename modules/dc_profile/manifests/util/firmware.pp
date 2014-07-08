083e4fb8cd4d961e1613237640871bfb4b1f6d54
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-08 16:05:46 +0100

    Add profile class

---
+++ modules/dc_profile/manifests/util/firmware.pp
@@ -0,0 +1,17 @@
+# Class: dc_profile::util::firmware
+#
+# Install the firmware tools
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::util::firmware {
+
+  include dc_firmware
+
+}
