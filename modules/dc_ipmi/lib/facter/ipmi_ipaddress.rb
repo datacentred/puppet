e70cd045321ebb7d72c108a8ceb636385fcde880
Rob Greenwood <bilco105@gmail.com>
2014-07-14 11:49:17 +0100

    stupid facter exec not evaling exit codes

--- modules/dc_ipmi/lib/facter/ipmi_ipaddress.rb
+++ modules/dc_ipmi/lib/facter/ipmi_ipaddress.rb
@@ -2,7 +2,8 @@ Facter.add(:ipmi_ipaddress) do
   setcode do
     ipaddress = nil
     if output = Facter::Util::Resolution.exec('ipmitool lan print 2>&1')
-      ipaddress = /^IP Address\s*:\s*([\w\.]+)/.match(output)[1]
+      match = /^IP Address\s*:\s*([\w\.]+)/.match(output)
+      ipaddress = match[1] unless match.nil?
     end
     ipaddress
   end
