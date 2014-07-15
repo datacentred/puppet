1c767fdafc62f20744a4967406cf47e8540d249e
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-15 14:06:26 +0100

    Tidy package declarations into the same place and remove duplication

--- modules/dc_icinga/manifests/server/custom_plugins.pp
+++ modules/dc_icinga/manifests/server/custom_plugins.pp
@@ -1,27 +1,5 @@
 class dc_icinga::server::custom_plugins {
 
-  # RabbitMQ plugins need libnagios-plugin-perl
-  package { 'libnagios-plugin-perl':
-    ensure => latest,
-  }
-
-  # Python packages for Openstack checks
-  package { 'python-keystoneclient':
-    ensure => installed,
-  }
-
-  package { 'python-neutronclient':
-    ensure => installed,
-  }
-
-  package { 'python-novaclient':
-    ensure => installed,
-  }
-
-  package { 'python-cinderclient':
-    ensure => installed,
-  }
-
   # Custom nagios plugins directory
   file { '/usr/lib/nagios/plugins':
     ensure  => directory,
