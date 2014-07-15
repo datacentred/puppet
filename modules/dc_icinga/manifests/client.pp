2670989f5f18cc7d006508710f7ac752d6a1a511
Rob Greenwood <bilco105@gmail.com>
2014-07-15 12:59:23 +0100

    fix ping check

--- modules/dc_icinga/manifests/client.pp
+++ modules/dc_icinga/manifests/client.pp
@@ -21,7 +21,7 @@ class dc_icinga::client {
     ensure          => present,
     alias           => $::fqdn,
     address         => $::ipaddress,
-    check_command   => 'check_ping',
+    check_command   => 'check_ping!100.0,20%!500.0,60%',
     use             => 'dc_host_generic',
     hostgroups      => template('dc_icinga/hostgroups.erb'),
     icon_image      => 'base/ubuntu.png',
