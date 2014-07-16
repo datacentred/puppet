2f453d5993cb6ad337a4f328c29c491c92bcc339
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-16 14:16:51 +0100

    Add nova api connection check

--- modules/dc_icinga/manifests/server/config.pp
+++ modules/dc_icinga/manifests/server/config.pp
@@ -393,6 +393,11 @@ class dc_icinga::server::config {
   icinga::command { 'check_nova_instance':
     command_line => "/usr/lib/nagios/plugins/check_nova-instance.sh -H http://${keystone_host}:${keystone_port}/v2.0 -E http://${nova_host}:${nova_osapi_port}/v2 -T ${keystone_icinga_tenant} -U ${keystone_icinga_user} -P ${keystone_icinga_password} -N icinga -I CirrOS\\ 0.3.2\\ x86_64 -F m1.tiny -r"
   }
+
+  icinga::command { 'check_nova_api_connect':
+    command_line => "/usr/lib/nagios/plugins/check_nova-api.sh -H http://${keystone_host}:${keystone_port}/v2.0 -E http://${nova_host}:${nova_osapi_port}/v2 -T ${keystone_icinga_tenant} -U ${keystone_icinga_user} -P ${keystone_icinga_password}"
+  }
+  ######################################################################
   ######################################################################
   ######################################################################
   ######################################################################
