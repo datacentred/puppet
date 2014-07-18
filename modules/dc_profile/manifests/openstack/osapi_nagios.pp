a32fafe1ecde26ed5d6a992dc76522bdad629625
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-18 18:01:03 +0100

    Add class for icinga configuration

---
+++ modules/dc_profile/manifests/openstack/osapi_nagios.pp
@@ -0,0 +1,18 @@
+# Class: dc_profile::openstack::osapi_nagios
+#
+# icinga config for API endpoints
+#
+#
+# Parameters:
+#
+# Actions:
+#
+#
+#
+# Sample Usage:
+#
+class dc_profile::openstack::osapi_nagios {
+
+  include dc_icinga::hostgroup_osapiendpoint
+
+}
