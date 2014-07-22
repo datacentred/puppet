3b24ca00887556d94af2981966b5e83eecaba40d
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-22 14:55:43 +0100

    Convert to new mechanism

--- modules/dc_profile/manifests/openstack/keystone_logstash.pp
+++ modules/dc_profile/manifests/openstack/keystone_logstash.pp
@@ -13,8 +13,10 @@
 class dc_profile::openstack::keystone_logstash {
 
   dc_logstash::client::register { 'keystone_log':
-    logs => '/var/log/keystone/keystone.log',
-    type => 'keystone',
+    logs   => '/var/log/keystone/keystone.log',
+    fields => {
+      'type'   => 'keystone',
+    }
   }
 
 }
