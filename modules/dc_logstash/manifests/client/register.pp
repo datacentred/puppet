86dbcd84848eac9694fe749153cf5e6d51ab1d52
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-22 12:00:49 +0100

    Fix quoting

--- modules/dc_logstash/manifests/client/register.pp
+++ modules/dc_logstash/manifests/client/register.pp
@@ -1,10 +1,10 @@
-define dc_logstash::client::register ($logs, $order='10', $fields) {
+define dc_logstash::client::register ($order='10', $logs, $fields) {
 
   validate_hash($fields)
 
   $json_type = sorted_json($fields)
 
-  concat::fragment { "logstash_forwarder_log_$name":
+  concat::fragment { "logstash_forwarder_log_${name}":
     target  => '/etc/logstash-forwarder',
     order   => $order,
     content => template('dc_logstash/logstash-forwarder_client_log.erb'),
