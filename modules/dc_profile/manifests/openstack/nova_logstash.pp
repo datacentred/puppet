1a7cd7386473558b8acc35c64b2809f5d5eb2065
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-22 15:45:09 +0100

    Switch to new code

--- modules/dc_profile/manifests/openstack/nova_logstash.pp
+++ modules/dc_profile/manifests/openstack/nova_logstash.pp
@@ -13,33 +13,45 @@
 class dc_profile::openstack::nova_logstash {
 
   dc_logstash::client::register { 'nova_api':
-    logs => '/var/log/nova/nova-api.log',
-    type => 'nova-api',
+    logs   => '/var/log/nova/nova-api.log',
+    fields => {
+      'type' => 'nova-api',
+    }
   }
 
   dc_logstash::client::register { 'nova_conductor':
-    logs => '/var/log/nova/nova-conductor.log',
-    type => 'nova-conductor',
+    logs   => '/var/log/nova/nova-conductor.log',
+    fields => {
+      'type' => 'nova-conductor',
+    }
   }
 
   dc_logstash::client::register { 'nova_scheduler':
-    logs => '/var/log/nova/nova-scheduler.log',
-    type => 'nova-scheduler',
+    logs   => '/var/log/nova/nova-scheduler.log',
+    fields => {
+      'type' => 'nova-scheduler',
+    }
   }
 
   dc_logstash::client::register { 'nova_cert':
-    logs => '/var/log/nova/nova-cert.log',
-    type => 'nova-cert',
+    logs   => '/var/log/nova/nova-cert.log',
+    fields => {
+      'type' => 'nova-cert',
+    }
   }
 
   dc_logstash::client::register { 'nova_manage':
-    logs => '/var/log/nova/nova-manage.log',
-    type => 'nova-manage',
+    logs   => '/var/log/nova/nova-manage.log',
+    fields => {
+      'type' => 'nova-manage',
+    }
   }
 
   dc_logstash::client::register { 'nova_consoleauth':
-    logs => '/var/log/nova/nova-consoleauth.log',
-    type => 'nova-consoleauth',
+    logs   => '/var/log/nova/nova-consoleauth.log',
+    fields => {
+      'type' => 'nova-consoleauth',
+    }
   }
 
 }
