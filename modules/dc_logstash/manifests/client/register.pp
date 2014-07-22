67e7f0075ac212bb768e281821db5c2396f4ac86
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-22 15:43:42 +0100

    Add some default fields and merge the two hashes

--- modules/dc_logstash/manifests/client/register.pp
+++ modules/dc_logstash/manifests/client/register.pp
@@ -2,7 +2,13 @@ define dc_logstash::client::register ($logs, $fields, $order='10') {
 
   validate_hash($fields)
 
-  $json_fields = sorted_json($fields)
+  $default_fields = {
+    'logsource' => $::hostname,
+    'shipper'   => 'logstash-forwarder' }
+
+  $merged_fields = merge($default_fields, $fields)
+
+  $json_fields = sorted_json($merged_fields)
 
   concat::fragment { "logstash_forwarder_log_${name}":
     target  => '/etc/logstash-forwarder',
