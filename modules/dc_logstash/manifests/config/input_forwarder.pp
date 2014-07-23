1b3deba6fb7853f80a7873bebd66da52bdf63622
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-23 09:41:07 +0100

    Input always needs to be first

--- modules/dc_logstash/manifests/config/input_forwarder.pp
+++ modules/dc_logstash/manifests/config/input_forwarder.pp
@@ -6,6 +6,6 @@ class dc_logstash::config::input_forwarder {
 
   logstash::configfile { 'input_forwarder':
     content => template('dc_logstash/input_forwarder.erb'),
-    order   => '11',
+    order   => '01',
   }
 }
