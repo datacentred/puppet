00fc202832c89f2c2464ea636d39f90af1c8b220
Rob Greenwood <bilco105@gmail.com>
2014-07-23 15:07:32 +0100

    move all openstack api endpoints to ssl

--- modules/dc_profile/manifests/openstack/haproxy.pp
+++ modules/dc_profile/manifests/openstack/haproxy.pp
@@ -30,12 +30,19 @@ class dc_profile::openstack::haproxy {
 
   # Keystone
   haproxy::listen { 'keystone':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '5000',
-    options   => {
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '5000',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
       'option'  => ['tcpka', 'httpchk', 'tcplog'],
       'balance' => 'source',
+      'rspadd'  => 'Strict-Transport-Security:\ max-age=60',
     },
   }
   haproxy::balancermember { 'keystone':
@@ -46,12 +53,19 @@ class dc_profile::openstack::haproxy {
     options           => 'check inter 2000 rise 2 fall 5',
   }
   haproxy::listen { 'keystone-auth':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '35357',
-    options   => {
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '35357',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
       'option'  => ['tcpka', 'httpchk', 'tcplog'],
       'balance' => 'source',
+      'rspadd'  => 'Strict-Transport-Security:\ max-age=60',
     },
   }
   haproxy::balancermember { 'keystone-auth':
@@ -64,122 +78,9 @@ class dc_profile::openstack::haproxy {
 
   # Glance
   haproxy::listen { 'glance-api':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '9292',
-    options   => {
-      'option'  => ['tcpka', 'httpchk', 'tcplog'],
-      'balance' => 'source',
-    },
-  }
-  haproxy::balancermember { 'glance-api':
-    listening_service => 'glance-api',
-    server_names      => $glance_api_servers,
-    ipaddresses       => $glance_api_servers,
-    ports             => '9292',
-    options           => 'check inter 2000 rise 2 fall 5',
-  }
-  haproxy::listen { 'glance-reg':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '9191',
-    options   => {
-      'option'  => ['tcpka', 'tcplog'],
-      'balance' => 'source',
-    },
-  }
-  haproxy::balancermember { 'glance-reg':
-    listening_service => 'glance-reg',
-    server_names      => $glance_api_servers,
-    ipaddresses       => $glance_api_servers,
-    ports             => '9191',
-    options           => 'check inter 2000 rise 2 fall 5',
-  }
-
-  # Neutron
-  haproxy::listen { 'neutron':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '9696',
-    options   => {
-      'option'  => ['tcpka', 'httpchk', 'tcplog'],
-      'balance' => 'source',
-    },
-  }
-  haproxy::balancermember { 'neutron':
-    listening_service => 'neutron',
-    server_names      => $neutron_api_servers,
-    ipaddresses       => $neutron_api_servers,
-    ports             => '9696',
-    options           => 'check inter 2000 rise 2 fall 5',
-  }
-
-  # Nova
-  haproxy::listen { 'nova-compute':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '8774',
-    options   => {
-      'option'  => ['tcpka', 'httpchk', 'tcplog'],
-      'balance' => 'source',
-    },
-  }
-  haproxy::balancermember { 'nova-compute':
-    listening_service => 'nova-compute',
-    server_names      => $nova_api_servers,
-    ipaddresses       => $nova_api_servers,
-    ports             => '8774',
-    options           => 'check inter 2000 rise 2 fall 5',
-  }
-  haproxy::listen { 'nova-metadata':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '8775',
-    options   => {
-      'option'  => ['tcpka', 'httpchk', 'tcplog'],
-      'balance' => 'source',
-    },
-  }
-  haproxy::balancermember { 'nova-metadata':
-    listening_service => 'nova-metadata',
-    server_names      => $nova_api_servers,
-    ipaddresses       => $nova_api_servers,
-    ports             => '8775',
-    options           => 'check inter 2000 rise 2 fall 5',
-  }
-
-  # Cinder
-  haproxy::listen { 'cinder':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '8776',
-    options   => {
-      'option'  => ['tcpka', 'httpchk', 'tcplog'],
-      'balance' => 'source',
-    },
-  }
-  haproxy::balancermember { 'cinder':
-    listening_service => 'cinder',
-    server_names      => $cinder_api_servers,
-    ipaddresses       => $cinder_api_servers,
-    ports             => '8776',
-    options           => 'check inter 2000 rise 2 fall 5',
-  }
-
-  # Horizon
-  haproxy::listen { 'horizon-non-ssl-redirect':
     ipaddress    => '*',
     mode         => 'http',
-    ports        => '80',
-    options      => {
-      'redirect' => 'scheme https if !{ ssl_fc }',
-    }
-  }
-
-  haproxy::listen { 'horizon':
-    ipaddress    => '*',
-    mode         => 'http',
-    ports        => '443',
+    ports        => '9292',
     bind_options => [
       'ssl',
       'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
@@ -192,6 +93,160 @@ class dc_profile::openstack::haproxy {
       'rspadd'   => 'Strict-Transport-Security:\ max-age=60',
     },
   }
+  haproxy::balancermember { 'glance-api':
+    listening_service => 'glance-api',
+    server_names      => $glance_api_servers,
+    ipaddresses       => $glance_api_servers,
+    ports             => '9292',
+    options           => 'check inter 2000 rise 2 fall 5',
+  }
+  haproxy::listen { 'glance-reg':
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '9191',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
+      'option'  => ['tcpka', 'tcplog'],
+      'balance' => 'source',
+      'rspadd'  => 'Strict-Transport-Security:\ max-age=60',
+    },
+  }
+  haproxy::balancermember { 'glance-reg':
+    listening_service => 'glance-reg',
+    server_names      => $glance_api_servers,
+    ipaddresses       => $glance_api_servers,
+    ports             => '9191',
+    options           => 'check inter 2000 rise 2 fall 5',
+  }
+
+  # Neutron
+  haproxy::listen { 'neutron':
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '9696',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
+      'option'  => ['tcpka', 'httpchk', 'tcplog'],
+      'balance' => 'source',
+      'rspadd'  => 'Strict-Transport-Security:\ max-age=60',
+    },
+  }
+  haproxy::balancermember { 'neutron':
+    listening_service => 'neutron',
+    server_names      => $neutron_api_servers,
+    ipaddresses       => $neutron_api_servers,
+    ports             => '9696',
+    options           => 'check inter 2000 rise 2 fall 5',
+  }
+
+  # Nova
+  haproxy::listen { 'nova-compute':
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '8774',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
+      'option'  => ['tcpka', 'httpchk', 'tcplog'],
+      'balance' => 'source',
+      'rspadd'  => 'Strict-Transport-Security:\ max-age=60',
+    },
+  }
+  haproxy::balancermember { 'nova-compute':
+    listening_service => 'nova-compute',
+    server_names      => $nova_api_servers,
+    ipaddresses       => $nova_api_servers,
+    ports             => '8774',
+    options           => 'check inter 2000 rise 2 fall 5',
+  }
+  haproxy::listen { 'nova-metadata':
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '8775',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
+      'option'  => ['tcpka', 'httpchk', 'tcplog'],
+      'balance' => 'source',
+      'rspadd'  => 'Strict-Transport-Security:\ max-age=60',
+    },
+  }
+  haproxy::balancermember { 'nova-metadata':
+    listening_service => 'nova-metadata',
+    server_names      => $nova_api_servers,
+    ipaddresses       => $nova_api_servers,
+    ports             => '8775',
+    options           => 'check inter 2000 rise 2 fall 5',
+  }
+
+  # Cinder
+  haproxy::listen { 'cinder':
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '8776',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
+      'option'  => ['tcpka', 'httpchk', 'tcplog'],
+      'balance' => 'source',
+      'rspadd'  => 'Strict-Transport-Security:\ max-age=60',
+    },
+  }
+  haproxy::balancermember { 'cinder':
+    listening_service => 'cinder',
+    server_names      => $cinder_api_servers,
+    ipaddresses       => $cinder_api_servers,
+    ports             => '8776',
+    options           => 'check inter 2000 rise 2 fall 5',
+  }
+
+  # Horizon
+  haproxy::listen { 'horizon-non-ssl-redirect':
+    ipaddress => '*',
+    mode      => 'http',
+    ports     => '80',
+    options   => {
+      'redirect' => 'scheme https if !{ ssl_fc }',
+    }
+  }
+  haproxy::listen { 'horizon':
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '443',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
+      'option'  => ['tcpka', 'httpchk', 'tcplog'],
+      'balance' => 'source',
+      'rspadd'  => 'Strict-Transport-Security:\ max-age=60',
+    },
+  }
   haproxy::balancermember { 'horizon':
     listening_service => 'horizon',
     server_names      => $horizon_servers,
@@ -202,11 +257,18 @@ class dc_profile::openstack::haproxy {
 
   # HAProxy Statistics
   haproxy::listen { 'haproxy-stats':
-    ipaddress => '*',
-    mode      => 'http',
-    ports     => '1936',
-    options   => {
-      'stats' => ['enable', 'uri /'],
+    ipaddress    => '*',
+    mode         => 'http',
+    ports        => '1936',
+    bind_options => [
+      'ssl',
+      'crt /etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem',
+      'crt /etc/ssl/certs/openstack_datacentred_io.pem',
+      'ciphers HIGH:!RC4:!MD5:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM',
+    ],
+    options      => {
+      'stats'  => ['enable', 'uri /'],
+      'rspadd' => 'Strict-Transport-Security:\ max-age=60',
     },
   }
 
