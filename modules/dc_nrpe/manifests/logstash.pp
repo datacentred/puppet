9e8f8be2e3f9c54363da8457381084fa3bbbc3c4
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-23 15:42:17 +0100

    Remove conditional

--- modules/dc_nrpe/manifests/logstash.pp
+++ modules/dc_nrpe/manifests/logstash.pp
@@ -15,28 +15,25 @@ class dc_nrpe::logstash {
 
   $logstash_forwarder_port = hiera(logstash_forwarder_port)
 
-  if defined(Class['::dc_logstash::client::forwarder']) {
+  sudo::conf { 'check_logstash_forwarder_netstat':
+    priority => 10,
+    content  => 'nagios ALL=NOPASSWD:/usr/lib/nagios/plugins/check_logstash-forwarder.sh',
+  }
 
-    sudo::conf { 'check_logstash_forwarder_netstat':
-      priority => 10,
-      content  => 'nagios ALL=NOPASSWD:/usr/lib/nagios/plugins/check_logstash-forwarder.sh',
-    }
+  file { '/etc/nagios/nrpe.d/logstash_forwarder_netstat.cfg':
+    ensure  => present,
+    content => 'command[check_logstash_forwarder_netstat]=/usr/lib/nagios/plugins/check_logstash-forwarder.sh',
+    require => Package['nagios-nrpe-server'],
+    notify  => Service['nagios-nrpe-server'],
+  }
 
-    file { '/etc/nagios/nrpe.d/logstash_forwarder_netstat.cfg':
-      ensure  => present,
-      content => 'command[check_logstash_forwarder_netstat]=/usr/lib/nagios/plugins/check_logstash-forwarder.sh',
-      require => Package['nagios-nrpe-server'],
-      notify  => Service['nagios-nrpe-server'],
-    }
-
-    file { '/usr/lib/nagios/plugins/check_logstash-forwarder.sh':
-      ensure  => file,
-      content => template('dc_nrpe/check_logstash-forwarder.erb'),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      require => Package['nagios-nrpe-server'],
-      notify  => Service['nagios-nrpe-server'],
-    }
+  file { '/usr/lib/nagios/plugins/check_logstash-forwarder.sh':
+    ensure  => file,
+    content => template('dc_nrpe/check_logstash-forwarder.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['nagios-nrpe-server'],
+    notify  => Service['nagios-nrpe-server'],
   }
 }
