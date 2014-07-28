3543db43ae10f8338021e55f4b552044b23b9f87
Rob Greenwood <bilco105@gmail.com>
2014-07-28 14:56:42 +0100

    move to signed certificates

--- modules/dc_ssl/manifests/haproxy.pp
+++ modules/dc_ssl/manifests/haproxy.pp
@@ -5,11 +5,19 @@ class dc_ssl::haproxy {
   }
 
   file { '/etc/ssl/certs/osapi_sal01_datacentred_co_uk.pem':
-    source => 'puppet:///modules/dc_ssl/haproxy/osapi_sal01_datacentred_co_uk.pem',
+    ensure => absent,
   }
 
   file { '/etc/ssl/certs/openstack_datacentred_io.pem':
-    source => 'puppet:///modules/dc_ssl/haproxy/openstack_datacentred_io.pem',
+    ensure => absent,
+  }
+
+  file { '/etc/ssl/certs/STAR_sal01_datacentred_co_uk.pem':
+    source => 'puppet:///modules/dc_ssl/haproxy/STAR_sal01_datacentred_co_uk.pem',
+  }
+
+  file { '/etc/ssl/certs/STAR_datacentred_io.pem':
+    source => 'puppet:///modules/dc_ssl/haproxy/STAR_datacentred_io.pem',
   }
 
 }
