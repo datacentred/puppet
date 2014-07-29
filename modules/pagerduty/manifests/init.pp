9497bd77560e0a280bfacf8287872347d5488d56
Rob Greenwood <bilco105@gmail.com>
2014-07-29 09:57:47 +0100

    should probably fix this properly at some point...

--- modules/pagerduty/manifests/init.pp
+++ modules/pagerduty/manifests/init.pp
@@ -1,13 +1,4 @@
 class pagerduty {
-  apt::source { 'pagerduty':
-    location    => 'http://packages.pagerduty.com/pdagent deb/',
-    repos       => '',
-    release     => '',
-    include_src => false,
-    key         => '9E65C6CB',
-    key_source  => 'http://packages.pagerduty.com/GPG-KEY-pagerduty',
-  }
-
   package { 'pdagent':
     ensure  => installed,
     require => Apt::Source['pagerduty'],
