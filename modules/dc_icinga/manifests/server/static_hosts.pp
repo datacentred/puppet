e479d518277b526360e168cc599d4d6ed0d4ab4c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-07-31 14:47:44 +0100

    Move to server directory

---
+++ modules/dc_icinga/manifests/server/static_hosts.pp
@@ -0,0 +1,28 @@
+# Class:
+#
+# Creates the static host entries on the icinga server
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+#
+class dc_icinga::server::static_hosts (
+  $hostdefs = {}
+){
+
+  include icinga::client
+
+  Icinga::Host {
+    ensure  => present,
+    alias   => "${title}.${::domain}",
+    address => inline_template("<% _erbout.concat(Resolv::DNS.open.getaddress('${title}').to_s) %>"),
+  }
+
+  create_resources('icinga::host', $hostdefs)
+
+}
