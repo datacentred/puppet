002a37130fda2919e897c1fe4ad1d0b4e44d5ee8
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-08-02 19:44:23 +0100

    Server doesn't need to be in the hostgroup

--- modules/dc_icinga/manifests/server/gen_static_hosts.pp
+++ modules/dc_icinga/manifests/server/gen_static_hosts.pp
@@ -1,7 +1,6 @@
 # Class: dc_icinga::server::gen_static_hosts
 #
 # Creates the static host entries on the icinga server
-# Include any required hostgroups here also
 #
 # Parameters:
 #
@@ -17,7 +16,5 @@ class dc_icinga::server::gen_static_hosts (
 ){
 
   create_resources('dc_icinga::static_host', $hostdefs)
-
-  include dc_icinga::hostgroup_apcpdu
 
 }
