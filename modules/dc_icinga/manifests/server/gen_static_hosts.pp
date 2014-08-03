41017a5e250537106de289fcbd727c491cbac271
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-08-03 19:07:24 +0100

    Fix type naming

--- modules/dc_icinga/manifests/server/gen_static_hosts.pp
+++ modules/dc_icinga/manifests/server/gen_static_hosts.pp
@@ -15,6 +15,6 @@ class dc_icinga::server::gen_static_hosts (
   $hostdefs = {},
 ){
 
-  create_resources('dc_icinga::static_host', $hostdefs)
+  create_resources('dc_icinga::server::static_host', $hostdefs)
 
 }
