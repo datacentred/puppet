b22cec5f4f0bf0f8adcef715b5a5b97c8d37d343
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-08-09 19:01:18 +0100

    Mount and umount remote directory as needed

--- modules/dc_mariadb/files/nfscopy.sh
+++ modules/dc_mariadb/files/nfscopy.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-# Check the remote directory is mounted
-if grep -q /var/dbbackups-remote /proc/mounts
+# Check the remote directory is mounted and mount if it's not there
+if ! grep -q /var/dbbackups-remote /proc/mounts
 then
-    rsync -a --delete /var/dbbackups/ /var/dbbackups-remote/
-else
-    echo "Remote directory does not seem to be mounted"
+    if mount /var/dbbackups-remote
+    then
+        rsync -a --delete /var/dbbackups/ /var/dbbackups-remote/
+    else
+        echo "Cannot mount remote directory"
+        exit 1
+    fi
+fi
+if ! umount /var/dbbackups-remote
+then
+    echo "Cannot unmount remote directory"
     exit 1
 fi
