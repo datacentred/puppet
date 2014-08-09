690fe89c13abb59120c29b35196e323e9e1bc95c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-08-09 19:52:54 +0100

    Add grub module and profile

---
+++ modules/dc_profile/manifests/util/grub.pp
@@ -0,0 +1,17 @@
+# Class: dc_profile::util::grub
+#
+# Change the grub config to output console messages
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::util::grub {
+
+  include dc_grub
+
+}
