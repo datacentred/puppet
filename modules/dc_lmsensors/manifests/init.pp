48c8caa39169ef94453312c25c9a12d31c8bf312
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-08-12 10:27:44 +0100

    Split out lmsensors config into a new module to add files

---
+++ modules/dc_lmsensors/manifests/init.pp
@@ -0,0 +1,75 @@
+# Class: dc_lmsensors
+#
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class dc_lmsensors {
+
+  case $::virtual {
+
+    physical: {
+
+      case $::lsbdistcodename {
+        'precise': {
+          $sensor_service = 'module-init-tools'
+        }
+        default: {
+          $sensor_service = 'kmod'
+        }
+      }
+
+      package { 'lm-sensors':
+        ensure => installed,
+      } ->
+
+      runonce { 'sensors-detect':
+        command => 'yes YES | sensors-detect',
+      } ->
+
+      # Annoyingly these always get run if we use the puppet
+      # service type, so run once per reboot
+      runonce { 'sensors-module-load':
+        command    => "service ${sensor_service} start",
+        persistent => false,
+      } ->
+
+      service { 'lm-sensors':
+        ensure  => running,
+        enable  => true,
+      }
+
+      # The jc42 module is not always autodetected so force it
+      if $::boardproductname == 'X8DTT-H' {
+
+          kmod::load { 'jc42':
+            ensure => 'present',
+          }
+
+          file { '/etc/sensors.d/jc42.conf':
+            ensure  => 'file',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/dc_lmsensors/jc42.conf',
+            require => Package['lm-sensors'],
+          }
+
+      }
+
+      include dc_nrpe::sensors
+      include dc_icinga::hostgroup_lmsensors
+    }
+
+    default: { }
+  }
+
+}
+
+
