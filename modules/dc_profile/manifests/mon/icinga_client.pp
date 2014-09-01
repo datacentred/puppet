0f4c4c9001862812801def8a088725d61e3806e2
atanas <atanas.abrashev@datacentred.co.uk>
2014-09-01 13:22:03 +0100

    wtf

--- modules/dc_profile/manifests/mon/icinga_client.pp
+++ modules/dc_profile/manifests/mon/icinga_client.pp
@@ -14,12 +14,9 @@ class dc_profile::mon::icinga_client {
 
   include dc_nrpe::install
 
-  if $::environment == 'production' {
-    $icinga_ip = hiera(icinga_ip)
-    class { '::dc_nrpe':
-      allowed_hosts => "127.0.0.1 ${icinga_ip}/32",
-    } ->
-    class { '::dc_icinga::client': }
-  }
-
+  $icinga_ip = hiera(icinga_ip)
+  class { '::dc_nrpe':
+    allowed_hosts => "127.0.0.1 ${icinga_ip}/32",
+  } ->
+  class { '::dc_icinga::client': }
 }
