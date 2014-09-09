dd887c23362cc8f49f2b4662313ee4ae6c08596c
Nick Jones <nick.jones@datacentred.co.uk>
2014-09-09 11:41:57 +0100

    Move RabbitMQ queue storage to /srv, add in various SSL options, create OpenStack account

--- modules/dc_profile/manifests/openstack/rabbitmq.pp
+++ modules/dc_profile/manifests/openstack/rabbitmq.pp
@@ -12,6 +12,9 @@
 #
 class dc_profile::openstack::rabbitmq {
 
+  $osdbmq_rabbitmq_user = hiera(osdbmq_rabbitmq_user)
+  $osdbmq_rabbitmq_pw = hiera(osdbmq_rabbitmq_pw)
+
   # Return just the hostname from the FQDN stored in Hiera
   $cluster_nodes = regsubst(hiera(osdbmq_members), '\.(.*)', '', 'G')
 
@@ -25,17 +28,43 @@ class dc_profile::openstack::rabbitmq {
     $cluster_node_type = 'ram'
   }
 
+  # Sit our message queues on some SSD shiz
+  file { '/srv/rabbitmq':
+    ensure  => directory,
+    recurse => true,
+  }
+
+  file { '/var/lib/rabbitmq':
+    ensure  => link,
+    target  => '/srv/rabbitmq',
+    require => File['/srv/rabbitmq'],
+  }
+
   class { '::rabbitmq':
     wipe_db_on_cookie_change => true,
     config_cluster           => true,
     admin_enable             => true,
     cluster_nodes            => $cluster_nodes,
     cluster_node_type        => $cluster_node_type,
+    delete_guest_user        => true,
+    ssl                      => true,
+    ssl_cacert               => '/var/lib/puppet/ssl/certs/ca.pem',
+    ssl_cert                 => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
+    ssl_key                  => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
+    ssl_verify               => true,
+    ssl_fail_if_no_peer_cert => true,
+    require                  => File['/srv/rabbitmq'],
+  }
+
+  rabbitmq_user { $osdbmq_rabbitmq_user:
+    admin    => true,
+    password => $osdbmq_rabbitmq_pw,
   }
 
   exec { 'configure-ha-queue-policy':
-    command     => '/usr/sbin/rabbitmqctl set_policy HA \'^(?!amq\\.).*\' \'{"ha-mode": "all"}\'',
-    require     => Class['::rabbitmq'],
+    command => '/usr/sbin/rabbitmqctl set_policy HA \'^(?!amq\\.).*\' \'{"ha-mode": "all"}\'',
+    unless  => '/usr/sbin/rabbitmqctl list_policies -p HA',
+    require => Class['::rabbitmq'],
   }
 
 }
