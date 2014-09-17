210c7c5ae220c294939dabacc5b037f10ca5f071
Nick Jones <nick.jones@datacentred.co.uk>
2014-09-17 11:37:47 +0100

    Do dist-upgrade and insert reboot

--- modules/dc_packer/files/scripts/base.sh
+++ modules/dc_packer/files/scripts/base.sh
@@ -9,7 +9,7 @@ mv /tmp/vagrant /etc/sudoers.d/
 # Apt-install various things necessary for Ruby, guest additions,
 # etc., and remove optional things to trim down the machine.
 apt-get -y update
-apt-get -y upgrade
+apt-get -y dist-upgrade
 apt-get -y install gcc build-essential linux-headers-$(uname -r)
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install vim curl
@@ -17,3 +17,8 @@ apt-get clean
 
 # Install NFS client
 apt-get -y install nfs-common
+
+# Reboot in case of any kernel updates, prior
+# to installing guest additions
+reboot
+sleep 999999
