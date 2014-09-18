334623d632714c8ecee23548a8d3997179574187
Nick Jones <nick.jones@datacentred.co.uk>
2014-09-18 17:09:34 +0100

    Rename Keystone DB profile class, and actually add it into the role

--- modules/dc_role/manifests/osdbmq.pp
+++ modules/dc_role/manifests/osdbmq.pp
@@ -15,6 +15,7 @@ class dc_role::osdbmq {
   contain dc_profile::openstack::galera
   contain dc_profile::openstack::rabbitmq
 
+  contain dc_profile::openstack::keystone_db
   contain dc_profile::openstack::cinder_db
   contain dc_profile::openstack::glance_registry_db
   contain dc_profile::openstack::glance_api_db
@@ -22,7 +23,7 @@ class dc_role::osdbmq {
   contain dc_profile::openstack::nova_db
 
   Class['dc_profile::openstack::galera'] ->
-  Class['dc_profile::openstack::keystone_mariadb'] ->
+  Class['dc_profile::openstack::keystone_db'] ->
   Class['dc_profile::openstack::cinder_db'] ->
   Class['dc_profile::openstack::glance_registry_db'] ->
   Class['dc_profile::openstack::glance_api_db'] ->
