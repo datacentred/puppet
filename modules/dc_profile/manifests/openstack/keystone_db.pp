8dc1f86f2b555b76341d6cbbd92f7ef856e0ec12
Nick Jones <nick.jones@datacentred.co.uk>
2014-09-19 17:53:08 +0100

    Include renamed Keystone DB profile class

---
+++ modules/dc_profile/manifests/openstack/keystone_db.pp
@@ -0,0 +1,24 @@
+# Class: dc_profile::openstack::keystone_db
+#
+# Provides a backend mysql database for keystone
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::openstack::keystone_db {
+
+  $keystone_db_pw = hiera(keystone_db_pw)
+
+  dc_mariadb::db { 'keystone':
+    user     => 'keystone',
+    password => $keystone_db_pw,
+    host     => '%',
+    require  => Class['::galera']
+  }
+
+}
