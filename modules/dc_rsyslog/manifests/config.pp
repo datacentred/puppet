ef6769fe9f3e1fde5b9ded0598a3ac8a14ad071c
Simon Murray <simon.murray@datacentred.co.uk>
2014-09-22 11:22:36 +0100

    Add udp support for rsyslog

--- modules/dc_rsyslog/manifests/config.pp
+++ modules/dc_rsyslog/manifests/config.pp
@@ -7,21 +7,23 @@ class dc_rsyslog::config {
     recurse => true,
   }
 
-  file { '/etc/rsyslog.d/50-default.conf':
-    path   => '/etc/rsyslog.d/50-default.conf',
+  File {
+    ensure => file,
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
+  }
+
+  file { '/etc/rsyslog.d/10-udp.conf':
+    source => 'puppet:///modules/dc_rsyslog/10-udp.conf',
+  }
+
+  file { '/etc/rsyslog.d/50-default.conf':
     source => 'puppet:///modules/dc_rsyslog/50-default.conf',
-    notify => Service['rsyslog'],
   }
 
   file { '/etc/rsyslog.d/20-ufw.conf':
-    path   => '/etc/rsyslog.d/20-ufw.conf',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/dc_rsyslog/20-ufw.conf',
-    notify => Service['rsyslog'],
   }
+
 }
