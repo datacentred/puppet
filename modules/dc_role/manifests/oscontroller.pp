2249def4d5bfa1e22f016c0196536cba293abf80
Nick Jones <nick.jones@datacentred.co.uk>
2014-09-23 11:22:03 +0100

    Initial cloud controller role class

---
+++ modules/dc_role/manifests/oscontroller.pp
@@ -0,0 +1,28 @@
+# Class: dc_role::oscontroller
+#
+# OpenStack Cloud Controller role class
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_role::oscontroller {
+
+  contain dc_profile::openstack::keystone
+  contain dc_profile::openstack::cinder
+  contain dc_profile::openstack::glance
+  contain dc_profile::openstack::horizon
+  contain dc_profile::openstack::nova
+  contain dc_profile::openstack::neutron_server
+
+  Class['dc_profile::openstack::keystone'] ->
+  Class['dc_profile::openstack::nova'] ->
+  Class['dc_profile::openstack::cinder'] ->
+  Class['dc_profile::openstack::glance'] ->
+  Class['dc_profile::openstack::neutron_server']
+
+}
