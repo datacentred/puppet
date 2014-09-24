ba91159760a95c50372a78a4d621ec7e274798e1
Nick Jones <nick.jones@datacentred.co.uk>
2014-09-24 14:02:29 +0100

    Remove sudoers_cinder include

--- modules/dc_profile/manifests/openstack/cinder.pp
+++ modules/dc_profile/manifests/openstack/cinder.pp
@@ -32,8 +32,6 @@ class dc_profile::openstack::cinder {
 
   $cinder_port = '8776'
 
-  include dc_profile::auth::sudoers_cinder
-
   class {'::cinder':
     rpc_backend         => 'cinder.openstack.common.rpc.impl_kombu',
     database_connection => "mysql://${cinder_db_user}:${cinder_db_pass}@${cinder_db_host}/${cinder_db}?charset=utf8",
