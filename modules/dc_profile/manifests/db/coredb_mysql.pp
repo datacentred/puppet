fcdef2c188910a5be323b4c176c9089f68c93a6e
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-09-24 17:18:26 +0100

    Parameterise and use create resources

--- modules/dc_profile/manifests/db/coredb_mysql.pp
+++ modules/dc_profile/manifests/db/coredb_mysql.pp
@@ -10,24 +10,21 @@
 #
 # Sample Usage:
 #
-class dc_profile::db::coredb_mysql {
-
-  $mysqlroot_pw    = hiera(db0_mysql_pw)
-  $graphite_db_pw  = hiera(graphite_db_pw)
-  $graphite_server = hiera(graphite_server)
+class dc_profile::db::coredb_mysql (
+  $databases,
+  $mysql_pw,
+){
 
   class { '::dc_mariadb':
-    maria_root_pw => $mysqlroot_pw,
+    maria_root_pw => $mysql_pw,
   }
   contain 'dc_mariadb'
 
-  # MySQL database backend for Graphite
-  dc_mariadb::db { 'graphite':
-    user     => 'graphite',
-    host     => $graphite_server,
-    password => $graphite_db_pw,
-    grant    => ['ALL'],
-    require  => Class['::dc_mariadb'],
+  $db_defaults = {
+    grant   => ['ALL'],
+    require => Class['::dc_mariadb'],
   }
+
+  create_resources(dc_mariadb::db, $databases, $db_defaults)
 
 }
