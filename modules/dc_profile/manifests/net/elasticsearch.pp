610918c6ccc3884540991520fa4f59ad9c1c908d
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-09-30 15:24:31 +0100

    Add profile class

---
+++ modules/dc_profile/manifests/net/elasticsearch.pp
@@ -0,0 +1,27 @@
+# Class: dc_profile::net::elasticsearch
+#
+# Configures elasticsearch
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires: puppet-elasticsearch
+#
+# Sample Usage:
+#
+class dc_profile::net::elasticsearch(
+  $es_version,
+  $cluster_name
+  ) {
+
+class { '::elasticsearch':
+  config => { 'cluster.name' => $dc_profile::net::elasticsearch::cluster_name,
+              'network.host' => $::ipaddress_eth1 },
+  manage_repo  => true,
+  java_install => true,
+  repo_version => $dc_profile::net::elasticsearch::es_version,
+}
+
+  elasticsearch::instance { 'es-01': }
+}
