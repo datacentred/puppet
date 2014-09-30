4387a21acb6d53785720f79a19600a4a36f0cbad
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-09-30 11:58:04 +0100

    Change to auto-detect vagrant

--- modules/dc_profile/manifests/init.pp
+++ modules/dc_profile/manifests/init.pp
@@ -14,21 +14,21 @@ class dc_profile {
 
   contain dc_profile::editors::vim
   contain dc_profile::net::hosts
-  contain dc_profile::net::ntpgeneric
   contain dc_profile::puppet::puppet
   contain dc_profile::util::external_facts
   contain dc_profile::util::timezone
   contain dc_profile::util::locale
   contain dc_profile::util::facter
-  contain dc_profile::util::firmware
-  contain dc_profile::util::mdadm
-  contain dc_profile::perf::sysdig
-  contain dc_profile::util::grub
 
-  if $::is_vagrant == undef {
+  unless $::virtual == 'virtualbox' or $::virtual == 'vmware' {
     contain dc_profile::auth::admins
     contain dc_profile::net::ssh
     contain dc_profile::auth::sudoers
+    contain dc_profile::util::firmware
+    contain dc_profile::util::grub
+    contain dc_profile::perf::sysdig
+    contain dc_profile::util::mdadm
+    contain dc_profile::net::ntpgeneric
     if $::environment == 'production' {
       contain dc_profile::puppet::mcollective_host
       contain dc_profile::perf::collectd::agent
