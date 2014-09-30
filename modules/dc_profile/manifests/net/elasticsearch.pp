be2f4ba3fc37a6c07a45a6c69cc08df8077f2d7b
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-09-30 16:44:49 +0100

    2 spaces removed. (╯°□°）╯︵ ┻━┻)

--- modules/dc_profile/manifests/net/elasticsearch.pp
+++ modules/dc_profile/manifests/net/elasticsearch.pp
@@ -13,7 +13,7 @@
 class dc_profile::net::elasticsearch(
   $es_version,
   $cluster_name
-  ) {
+) {
 
   class { '::elasticsearch':
     config       => { 'cluster.name' => $dc_profile::net::elasticsearch::cluster_name,
