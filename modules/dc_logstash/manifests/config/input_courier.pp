a04a34b6e7cd02ebe8db3cfdcf9203bd95223a41
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-10-03 17:16:21 +0100

    Add courier input

---
+++ modules/dc_logstash/manifests/config/input_courier.pp
@@ -0,0 +1,11 @@
+# Class: dc_logstash::config::input_courier
+#
+# Server side configuration for log_courier input
+#
+class dc_logstash::config::input_courier {
+
+  logstash::configfile { 'input_courier':
+    content => template('dc_logstash/input_courier.erb'),
+    order   => '01',
+  }
+}
