a622ba02f06f601f0b05ab1f4233f7dbd4bfc7f0
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-10-03 13:57:17 +0100

    added config to make haproxy and logstash production ready

---
+++ modules/dc_profile/manifests/net/haproxy_logstash.pp
@@ -0,0 +1,21 @@
+# Class: dc_profile::net::haproxy
+#
+# Configures haproxy
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires: puppet-haproxy
+#
+# Sample Usage:
+#
+class dc_profile::net::haproxy_logstash (
+  $listeners = {}
+) {
+
+  contain haproxy
+
+  create_resources(haproxy::listen, $listeners)
+
+}
