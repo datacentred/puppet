523e156f8be3379e7f6a31b8803757752e144fe9
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-10-08 11:44:13 +0100

    Add performance tuning options for elasticsearch

--- modules/dc_profile/manifests/net/elasticsearch.pp
+++ modules/dc_profile/manifests/net/elasticsearch.pp
@@ -10,14 +10,21 @@
 #
 # Sample Usage:
 #
-class dc_profile::net::elasticsearch(
-  $cluster_name,
+class dc_profile::net::elasticsearch (
+  $es_hash
 ) {
 
+  $half_RAM = floor($::memorysize_mb/2)
+  $RAM_unit = 'M'
+
+  $config_hash = {
+    'ES_HEAP_SIZE' => "${half_RAM}${RAM_unit}",
+  }
+
   class { '::elasticsearch':
-    config       => { 'cluster.name' => $cluster_name,
-                      'network.host' => $::ipaddress },
-    java_install => true,
+    config        => $::es_hash,
+    java_install  =>  true,
+    init_defaults => $config_hash,
   }
 
   elasticsearch::instance { 'es-01': }
