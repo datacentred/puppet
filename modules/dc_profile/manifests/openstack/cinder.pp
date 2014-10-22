a126820f359bc1900c2757e747f5df0c8f03995b
Nick Jones <nick.jones@datacentred.co.uk>
2014-10-22 14:21:39 +0100

    Refactor and addition of Ceph-related configuration

--- modules/dc_profile/manifests/openstack/cinder.pp
+++ modules/dc_profile/manifests/openstack/cinder.pp
@@ -12,86 +12,21 @@
 #
 class dc_profile::openstack::cinder {
 
-  $keystone_cinder_password = hiera(keystone_cinder_password)
-
-  # OpenStack API and loadbalancer endpoint
-  $osapi_public  = 'compute.datacentred.io'
-
-  $cinder_db        = hiera(cinder_db)
-  $cinder_db_host   = $osapi_public
-  $cinder_db_user   = hiera(cinder_db_user)
-  $cinder_db_pass   = hiera(cinder_db_pass)
-
-  $rabbitmq_hosts    = hiera(osdbmq_members)
-  $rabbitmq_username = hiera(osdbmq_rabbitmq_user)
-  $rabbitmq_password = hiera(osdbmq_rabbitmq_pw)
-  $rabbitmq_port     = hiera(osdbmq_rabbitmq_port)
-  $rabbitmq_vhost    = hiera(osdbmq_rabbitmq_vhost)
-
-  $os_region = hiera(os_region)
-
-  $cinder_rbd_uuid = hiera(cinder_rbd_uuid)
-
-  $management_ip = $::ipaddress
-
-  class {'::cinder':
-    rpc_backend         => 'cinder.openstack.common.rpc.impl_kombu',
-    database_connection => "mysql://${cinder_db_user}:${cinder_db_pass}@${cinder_db_host}/${cinder_db}?charset=utf8",
-    rabbit_hosts        => $rabbitmq_hosts,
-    rabbit_userid       => $rabbitmq_username,
-    rabbit_password     => $rabbitmq_password,
-    rabbit_port         => $rabbitmq_port,
-    rabbit_virtual_host => $rabbitmq_vhost,
-    package_ensure      => present,
-  }
-
-  class {'::cinder::api':
-    keystone_enabled       => true,
-    keystone_auth_host     => $osapi_public,
-    keystone_auth_protocol => 'https',
-    keystone_user          => 'cinder',
-    keystone_password      => $keystone_cinder_password,
-    os_region_name         => $os_region,
-    package_ensure         => present,
-    enabled                => true,
-  }
-
-  class {'::cinder::scheduler':
-    scheduler_driver => 'cinder.scheduler.simple.SimpleScheduler',
-    package_ensure   => present,
-    enabled          => true,
-  }
-
-  class {'::cinder::volume':
-    package_ensure => present,
-    enabled        => true,
-  }
-
-  class { '::cinder::glance':
-    glance_api_servers => $osapi_public,
-  }
-  
-  class { '::cinder::backend::rbd':
-    rbd_pool        => 'cinder.volume',
-    rbd_user        => 'cinder',
-    rbd_secret_uuid => $cinder_rbd_uuid,
-  }
+  include ::cinder
+  include ::cinder::api
+  include ::cinder::scheduler
+  include ::cinder::glance
+  include ::cinder::quota
+  include ::cinder::volume
+  include ::cinder::volume::rbd
 
   # Add this node into our loadbalancer
   @@haproxy::balancermember { "${::fqdn}-cinder":
     listening_service => 'icehouse-cinder',
     server_names      => $::hostname,
-    ipaddresses       => $management_ip,
+    ipaddresses       => $::ipaddress,
     ports             => '8776',
     options           => 'check inter 2000 rise 2 fall 5',
-  }
-
-  # Set default quotas
-  # Set to 200GB temporarily until we have a larger backing store
-  class { '::cinder::quota':
-    quota_volumes   => '10',
-    quota_snapshots => '10',
-    quota_gigabytes => '200',
   }
 
   # Nagios config
