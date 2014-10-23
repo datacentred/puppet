f48efc590acc1c283c4d9a0b93476abf31259e4e
Rob Greenwood <bilco105@gmail.com>
2014-10-23 12:25:13 +0100

    add missing comma

--- modules/dc_motd/manifests/init.pp
+++ modules/dc_motd/manifests/init.pp
@@ -15,7 +15,7 @@ class dc_motd {
 
   file { '/etc/motd':
     ensure  => file,
-    content => template('dc_motd/motd.erb')
+    content => template('dc_motd/motd.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
