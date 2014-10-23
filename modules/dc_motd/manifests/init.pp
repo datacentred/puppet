8df6d10674822943f67ce7a0ebe7cc33ca47ce55
Rob Greenwood <bilco105@gmail.com>
2014-10-23 12:21:43 +0100

    fix motd

--- modules/dc_motd/manifests/init.pp
+++ modules/dc_motd/manifests/init.pp
@@ -1,11 +1,21 @@
 class dc_motd {
-  file { '/run/motd.dynamic':
+  file { '/etc/issue':
     ensure => absent,
+  }
+
+  file { '/etc/issue.net':
+    ensure => absent,
+  }
+
+  file { '/etc/update-motd.d':
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
   }
 
   file { '/etc/motd':
     ensure  => file,
-    content => template("dc_motd/motd.erb"),
+    content => template('dc_motd/motd.erb')
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
