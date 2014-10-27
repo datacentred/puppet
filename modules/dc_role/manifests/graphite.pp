e8c5c65035b98b66227292e34eed32b4717640b0
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-10-27 11:45:28 +0000

    Move weathermap to new graphite server

--- modules/dc_role/manifests/graphite.pp
+++ modules/dc_role/manifests/graphite.pp
@@ -12,7 +12,6 @@
 #
 class dc_role::graphite inherits dc_role {
 
-  contain dc_profile::perf::network_weathermap
   contain dc_profile::net::phpipam
   contain dc_profile::db::coredb_mysql
 
--- modules/dc_role/manifests/graphite_core.pp
+++ modules/dc_role/manifests/graphite_core.pp
@@ -15,5 +15,6 @@ class dc_role::graphite_core inherits dc_role {
   contain dc_profile::perf::graphite
   contain dc_profile::perf::gdash
   contain dc_profile::perf::collectd::poller
+  contain dc_profile::perf::network_weathermap
 
 }
