3767b7919215eee789923d29976e4f2b702ebd00
Nick Jones <nick.jones@datacentred.co.uk>
2014-10-28 16:03:20 +0000

    MongoDB profile class

---
+++ modules/dc_profile/manifests/openstack/mongodb.pp
@@ -0,0 +1,18 @@
+# Class: dc_profile::openstack::mongodb
+#
+# MongoDB installation supporting OpenStack Ceilometer
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::openstack::mongodb {
+
+  include ::mongodb::server
+  include ::mongodb::replset
+
+}
