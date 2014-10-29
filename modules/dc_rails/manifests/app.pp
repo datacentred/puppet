97613ab606b88b4e2cfad680c3d6c1155b39ade8
Sean Handley <sean.handley@gmail.com>
2014-10-29 09:35:27 +0000

    Running unicorn with bundle exec ensures correct gems loaded.

--- modules/dc_rails/manifests/app.pp
+++ modules/dc_rails/manifests/app.pp
@@ -32,7 +32,7 @@ define dc_rails::app (
   $home = "/home/${user}/"
   $app_home = "${home}${app_name}/current/"
   $bundler = "${home}.rbenv/shims/bundle"
-  $unicorn = "${home}.rbenv/shims/unicorn"
+  $unicorn = "${home}.rbenv/shims/bundle exec unicorn"
   $log_base = '/var/log/rails/'
   $logdir = "${log_base}${app_name}/"
   $run_base = '/var/run/rails/'
