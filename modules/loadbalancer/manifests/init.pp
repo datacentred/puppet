d5e187878cb9f397ed63b2c0dd815c04be593aca
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-10-29 09:53:07 +0000

    Allow loadbalancers to bind to IPs that don't exist yet

--- modules/loadbalancer/manifests/init.pp
+++ modules/loadbalancer/manifests/init.pp
@@ -1,6 +1,6 @@
 # == Class: loadbalancer
 #
-# Provides a keepalived haproxy load balancer 
+# Provides a keepalived haproxy load balancer
 #
 class loadbalancer (
   $keepalived_interface,
@@ -13,12 +13,17 @@ class loadbalancer (
   include ::keepalived
   include ::dc_ssl::haproxy
 
+  sysctl { 'net.ipv4.ip_nonlocal_bind':
+    ensure => present,
+    value  => '1',
+  }
+
   keepalived::vrrp::instance { "VI_${keepalived_virtual_router_id}":
-    interface          => $keepalived_interface,
-    state              => 'SLAVE',
-    priority           => '100',
-    virtual_router_id  => $keepalived_virtual_router_id,
-    virtual_ipaddress  => $keepalived_virtual_ipaddress,
+    interface         => $keepalived_interface,
+    state             => 'SLAVE',
+    priority          => '100',
+    virtual_router_id => $keepalived_virtual_router_id,
+    virtual_ipaddress => $keepalived_virtual_ipaddress,
   }
 
   create_resources('haproxy::listen', $haproxy_listeners)
