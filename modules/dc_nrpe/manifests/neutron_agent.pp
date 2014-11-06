25ccdea759da1f2692f4e91d11dfcd4517ff1ca0
Simon Murray <simon.murray@datacentred.co.uk>
2014-11-06 16:46:08 +0000

    missing colon

--- modules/dc_nrpe/manifests/neutron_agent.pp
+++ modules/dc_nrpe/manifests/neutron_agent.pp
@@ -2,7 +2,7 @@
 #
 class dc_nrpe::neutron_agent {
 
-  fle { '/etc/nagios/nrpe.d/neutron_agent.cfg'
+  fle { '/etc/nagios/nrpe.d/neutron_agent.cfg':
     ensure  => present,
     source  => 'file:///modules/dc_nrpe/neutron_agent.cfg',
     require => Package['nagios-nrpe-server'],
