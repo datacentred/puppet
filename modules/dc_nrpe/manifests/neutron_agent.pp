14d5d77285c9fb30454c73ce855a9acbd925c550
Simon Murray <simon.murray@datacentred.co.uk>
2014-11-06 14:40:07 +0000

    Fix up source paths

--- modules/dc_nrpe/manifests/neutron_agent.pp
+++ modules/dc_nrpe/manifests/neutron_agent.pp
@@ -4,7 +4,7 @@ class dc_nrpe::neutron_agent {
 
   fle { '/etc/nagios/nrpe.d/neutron_agent.cfg'
     ensure  => present,
-    source  => 'file:///dc_nrpe/files/neutron_agent.cfg',
+    source  => 'file:///modules/dc_nrpe/neutron_agent.cfg',
     require => Package['nagios-nrpe-server'],
     notify  => Service['nagios-nrpe-server'],
   }
