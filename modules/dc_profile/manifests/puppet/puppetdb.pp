76010f61b789e292118c79a8f324ddd6ce167cbb
Simon Murray <simon.murray@datacentred.co.uk>
2014-11-06 14:47:21 +0000

    More coding standards cleanups and puppetdb sed hack fix

--- modules/dc_profile/manifests/puppet/puppetdb.pp
+++ modules/dc_profile/manifests/puppet/puppetdb.pp
@@ -12,19 +12,18 @@
 #
 class dc_profile::puppet::puppetdb {
 
-  $puppetdb_pw = hiera(puppetdb_pass)
-
   contain puppetdb::server
 
   # Puppetdb with default settings OOM kills itself so hack
   # the defaults to increase memory to 2GB
   exec { 'sed -i "s/-Xmx[[:digit:]]\+m/-Xmx2048m/" /etc/default/puppetdb':
-    unless => 'grep Xmx2048m /etc/default/puppetdb',
-    notify => Service['puppetdb'],
+    unless  => 'grep Xmx2048m /etc/default/puppetdb',
+    require => Package['puppetdb'],
+    notify  => Service['puppetdb'],
   }
 
-  include dc_nrpe::puppetdb
-  include dc_icinga::hostgroup_puppetdb
+  include ::dc_nrpe::puppetdb
+  include ::dc_icinga::hostgroup_puppetdb
 
 }
 
