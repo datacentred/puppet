1718b94189076d11f780bf077e2c84da0fe41f79
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-11-07 09:55:37 +0000

    put haproxy in front of elasticsearch and point logstash at new location

---
+++ modules/dc_elasticsearch/manifests/lbmember.pp
@@ -0,0 +1,20 @@
+# Class: dc_elasticsearch
+#
+# Gives stats from loadbalancer members to haproxy for cluster management
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_elasticsearch::lbmember{
+  @@haproxy::balancermember { "${::hostname}_elasticsearch":
+    listening_service => 'elasticsearch',
+    ipaddresses       => $::ipaddress,
+    server_names      => $::hostname,
+    ports             => '9200',
+  }
+}
