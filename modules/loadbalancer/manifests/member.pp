3180ba7f5120bedb36614960fbe3a116f5ce38b0
Simon Murray <simon.murray@datacentred.co.uk>
2014-11-07 12:29:54 +0000

    Puppet/Hiera borkage

--- modules/loadbalancer/manifests/member.pp
+++ modules/loadbalancer/manifests/member.pp
@@ -6,11 +6,13 @@ define loadbalancer::member (
   $options,
 ) {
 
+  # NOTE: Yes it's a coding standard violation but a bug exists
+  # somewhere with implicit type conversion
   @@haproxy::balancermember { $::fqdn:
     listening_service => $name,
     server_names      => $::hostname,
     ipaddresses       => $ipaddresses,
-    ports             => $ports,
+    ports             => "${ports}",
     options           => $options,
   }
 
