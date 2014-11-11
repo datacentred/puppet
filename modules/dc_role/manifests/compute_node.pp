e21a9a18f2c5868c958943c5453a42aa0eb29968
Nick Jones <nick.jones@datacentred.co.uk>
2014-11-11 15:46:25 +0000

    Include relevant Ceilometer profile classes

--- modules/dc_role/manifests/compute_node.pp
+++ modules/dc_role/manifests/compute_node.pp
@@ -15,9 +15,11 @@ class dc_role::compute_node inherits dc_role {
   contain dc_profile::openstack::neutron_agent
   contain dc_profile::openstack::neutron_common
   contain dc_profile::openstack::nova_compute
+  contain dc_profile::openstack::ceilometer_agent
 
   Class['dc_profile::openstack::neutron_agent'] ->
-  Class['dc_profile::openstack::nova_compute']
+  Class['dc_profile::openstack::nova_compute'] ->
+  Class['dc_profile::openstack::ceilometer_agent']
 
   include dc_icinga::hostgroup_nova_compute
   include dc_nrpe::nova_compute
