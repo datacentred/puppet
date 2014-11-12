de4eae6535adcc0ed04c7d433dfa4dfb8400cd02
Simon Murray <simon.murray@datacentred.co.uk>
2014-11-12 14:44:26 +0000

    Give logstash a good going over

--- modules/dc_nrpe/manifests/logstash.pp
+++ modules/dc_nrpe/manifests/logstash.pp
@@ -13,27 +13,10 @@
 # [Remember: No empty lines between comments and class definition]
 class dc_nrpe::logstash {
 
-  $logstash_forwarder_port = hiera(logstash_forwarder_port)
-
-  sudo::conf { 'check_logstash_forwarder_netstat':
-    priority => 10,
-    content  => 'nagios ALL=NOPASSWD:/usr/lib/nagios/plugins/check_logstash-forwarder.sh',
+  dc_nrpe::check { 'check_logstash_forwarder_netstat':
+    path   => '/usr/local/bin/check_logstash-forwarder',
+    source => 'puppet:///modules/dc_nrpe/check_logstash-forwarder',
+    sudo   => true,
   }
 
-  file { '/etc/nagios/nrpe.d/logstash_forwarder_netstat.cfg':
-    ensure  => present,
-    content => 'command[check_logstash_forwarder_netstat]=sudo /usr/lib/nagios/plugins/check_logstash-forwarder.sh',
-    require => Package['nagios-nrpe-server'],
-    notify  => Service['nagios-nrpe-server'],
-  }
-
-  file { '/usr/lib/nagios/plugins/check_logstash-forwarder.sh':
-    ensure  => file,
-    content => template('dc_nrpe/check_logstash-forwarder.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    require => Package['nagios-nrpe-server'],
-    notify  => Service['nagios-nrpe-server'],
-  }
 }
