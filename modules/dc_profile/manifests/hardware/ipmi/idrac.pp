3b30bd91b7e42309afb05c10a5017029bb72c0b1
atanas <atanas.abrashev@datacentred.co.uk>
2014-11-19 12:13:59 +0000

    all the racadm stuff needs dataeng service running to work

--- modules/dc_profile/manifests/hardware/ipmi/idrac.pp
+++ modules/dc_profile/manifests/hardware/ipmi/idrac.pp
@@ -16,6 +16,12 @@ class dc_profile::hardware::ipmi::idrac {
   # Install racadm
   include dc_ipmi::dell::racadm
 
+  service { 'dataeng':
+    ensure  => running,
+    enable  => true,
+    require => Class['dc_ipmi::dell::racadm'],
+  }
+
   # Create a symlink to racadm in /usr/bin/racadm
   # because drac_setting expects it to be set
   file {'/usr/bin/racadm':
@@ -27,36 +33,43 @@ class dc_profile::hardware::ipmi::idrac {
   # set the hostname
   drac_setting { 'cfgLanNetworking/cfgDNSRacName':
     object_value => "${::hostname}-drac",
-    require => File['/usr/bin/racadm'],
+    require => Service['dataeng'],
   }
 
   # configure ldap authentication
   drac_setting { 'cfgldap/cfgLdapEnable':
-    object_value => 1
+    object_value => 1,
+    require => Service['dataeng'],
   }
 
   # this should be in hiera
   drac_setting { 'cfgldap/cfgLdapServer':
-    object_value => '10.10.192.111' 
+    object_value => '10.10.192.111',
+    require => Service['dataeng'],
   }
 
   drac_setting { 'cfgldap/cfgLdapPort':
-    object_value => 636
+    object_value => 636,
+    require => Service['dataeng'],
   }
 
   drac_setting { 'cfgldap/cfgLdapBaseDN':
-    object_value => 'dc=datacentred,dc=co,dc=uk'
+    object_value => 'dc=datacentred,dc=co,dc=uk',
+    require => Service['dataeng'],
   }
 
   drac_setting { 'cfgldap/cfgLdapCertValidationenable':
-    object_value => 0
+    object_value => 0,
+    require => Service['dataeng'],
   }
 
   drac_setting { 'cfgldaprolegroup/1/cfgLdapRoleGroupDN':
-    object_value => 'cn=iLO Admins,ou=Groups,dc=datacentred,dc=co,dc=uk'
+    object_value => 'cn=iLO Admins,ou=Groups,dc=datacentred,dc=co,dc=uk',
+    require => Service['dataeng'],
   }
 
   drac_setting { 'cfgldaprolegroup/1/cfgLdapRoleGroupPrivilege':
-    object_value => 0x01ff
+    object_value => 0x01ff,
+    require => Service['dataeng'],
   }
 }
