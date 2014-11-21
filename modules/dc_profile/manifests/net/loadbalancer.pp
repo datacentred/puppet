bf511c621983ec3170e67276c575972a374aac1f
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-11-21 10:46:25 +0000

    changed to top scope fqdn, used a cleaner command for concating

--- modules/dc_profile/manifests/net/loadbalancer.pp
+++ modules/dc_profile/manifests/net/loadbalancer.pp
@@ -8,10 +8,8 @@ class dc_profile::net::loadbalancer{
 
   if ($::loadbalancer::haproxy_stats_ssl_cert != undef) {
     exec { 'create_cert_haproxy':
-      command => "sudo cat /var/lib/puppet/ssl/certs/${::fqdn}.pem /var/lib/puppet/ssl/private_keys/${::fqdn}.pem | sudo tee /etc/ssl/certs/${::fqdn}.pem",
+      command => "sudo cat /var/lib/puppet/ssl/certs/${::fqdn}.pem /var/lib/puppet/ssl/private_keys/${::fqdn}.pem > /etc/ssl/certs/${::fqdn}.pem",
       path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
       creates => "/etc/ssl/certs/${::fqdn}.pem"
     }
   }
-
-}
