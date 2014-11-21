16938aa33cf5ee62b03a81b808239782bfed83b6
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-11-21 09:55:29 +0000

    add ssl cert location to hiera lookup

--- modules/dc_profile/manifests/net/loadbalancer.pp
+++ modules/dc_profile/manifests/net/loadbalancer.pp
@@ -7,7 +7,6 @@ class dc_profile::net::loadbalancer{
   include ::loadbalancer
 
   if ($::loadbalancer::haproxy_stats_ssl_cert != undef) {
-
     exec { 'create_cert_haproxy':
       command => "sudo cat /var/lib/puppet/ssl/certs/${::fqdn}.pem /var/lib/puppet/ssl/private_keys/${::fqdn}.pem | sudo tee /etc/ssl/certs/${::fqdn}.pem",
       path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
