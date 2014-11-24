b677f0f4ff2fcad906ec4473f53fb72404b62d07
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-11-24 13:21:06 +0000

    Stop ps_logstash boxes trying to back up non-existant data

--- modules/dc_role/manifests/ps_logstash.pp
+++ modules/dc_role/manifests/ps_logstash.pp
@@ -14,7 +14,6 @@ class dc_role::ps_logstash inherits dc_role {
 
   contain dc_profile::log::logstash
   contain dc_profile::log::kibana
-  contain dc_profile::log::logstashbackup
   contain dc_profile::log::riemann
   contain dc_profile::log::logstash_lbmember
 }
