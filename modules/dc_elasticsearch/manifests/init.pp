5dfe0c67ed3224b4ff992ce4e9c29e9855292cca
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-11-26 09:49:43 +0000

    add config to allow elasticsearch to lock unlimited memory

--- modules/dc_elasticsearch/manifests/init.pp
+++ modules/dc_elasticsearch/manifests/init.pp
@@ -14,17 +14,20 @@ class dc_elasticsearch (
   $es_hash,
 ) {
 
+  include ulimit
+
   $half_RAM = floor($::memorysize_mb/2)
   $RAM_unit = 'M'
 
   $config_hash = {
-    'ES_HEAP_SIZE' => "${half_RAM}${RAM_unit}",
+    'ES_HEAP_SIZE'      => "${half_RAM}${RAM_unit}",
+    'MAX_LOCKED_MEMORY' => 'unlimited',
   }
 
   ulimit::rule {
     'elasticsearch':
       ulimit_domain => 'elasticsearch',
-      ulimit_type   => 'hard',
+      ulimit_type   => '-',
       ulimit_item   => 'memlock',
       ulimit_value  => 'unlimited';
   }
