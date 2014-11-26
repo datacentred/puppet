220332150efef522bf905ba30a2c93e2be31327b
Max Siegieda <max.siegieda@datacentred.co.uk>
2014-11-26 16:20:01 +0000

    using a file instead of a string embedded in the puppet

--- modules/dc_logstash/manifests/server/config/output_new_es_cluster.pp
+++ modules/dc_logstash/manifests/server/config/output_new_es_cluster.pp
@@ -4,18 +4,8 @@
 #
 class dc_logstash::server::config::output_new_es_cluster inherits dc_logstash::server {
 
-$output_es_cluster = "output {
-  elasticsearch {
-    host => logstash
-    embedded => false
-    protocol => http
-  }
-}"
-
-
-
   logstash::configfile { 'output_new_es_cluster':
-    content => $output_es_cluster,
+    content => 'puppet:///modules/dc_logstash/output_new_es_cluster',
     order   => '20',
   }
 
