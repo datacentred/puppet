21e18f0f268bd0024fd28da904f12e1352a84095
Simon Murray <simon.murray@datacentred.co.uk>
2014-12-03 13:12:35 +0000

    Parameter overrides default fail

--- modules/dc_icinga/manifests/server/nagios_commands.pp
+++ modules/dc_icinga/manifests/server/nagios_commands.pp
@@ -95,7 +95,7 @@ class dc_icinga::server::nagios_commands {
   }
 
   icinga::command { 'check_nova_instance':
-    command_line => "/usr/lib/nagios/plugins/check_nova-instance.py --auth_url https://\$HOSTALIAS\$:${keystone_port}/v2.0 --endpoint_url https://\$HOSTALIAS\$:${nova_osapi_port}/v2 --tenant ${keystone_icinga_tenant} --username ${keystone_icinga_user} --password ${keystone_icinga_password} --instance_name icinga --image_name CirrOS\\ 0.3.3 --flavor_name m1.tiny --force_delete"
+    command_line => "/usr/lib/nagios/plugins/check_nova-instance.py --auth_url https://\$HOSTALIAS\$:${keystone_port}/v2.0 --endpoint_url https://\$HOSTALIAS\$:${nova_osapi_port}/v2 --tenant ${keystone_icinga_tenant} --username ${keystone_icinga_user} --password ${keystone_icinga_password} --instance_name icinga --image_name CirrOS\\ 0.3.3 --flavor_name dc1.1x0 --force_delete"
   }
 
   icinga::command { 'check_nova_api_connect':
