d8a9326f8ebd301c3571002a344612a0492deedb
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-12-04 12:47:39 +0000

    Add sync config to roles

--- modules/dc_role/manifests/dnsslave.pp
+++ modules/dc_role/manifests/dnsslave.pp
@@ -15,6 +15,6 @@ class dc_role::dnsslave inherits dc_role {
   contain dc_profile::net::dnsbackup
   contain dc_profile::net::dns_slave
   contain dc_profile::net::dhcpd_slave
-  contain dc_profile::net::tftpserver
+  contain dc_profile::net::tftp_syncslave
 
 }
