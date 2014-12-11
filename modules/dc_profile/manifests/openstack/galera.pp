0d76552b6ca248e9f5e6fe2195b422a5a77296e1
Nick Jones <nick.jones@datacentred.co.uk>
2014-12-11 12:18:39 +0000

    Ensure the target backup directory is created

--- modules/dc_profile/manifests/openstack/galera.pp
+++ modules/dc_profile/manifests/openstack/galera.pp
@@ -44,6 +44,10 @@ class dc_profile::openstack::galera {
 
   # $::backup_node is set via Foreman
   if $::backup_node {
+    file { '/var/local/backup':
+      ensure => directory,
+      mode   => '0700',
+    }
     include ::dc_backup::duplicity
     include ::mysql::server::backup
   }
