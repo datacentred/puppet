cd861be84532e013843b4253f3cb924b3d0b8495
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2014-12-13 17:32:46 +0000

    Add parameter

--- modules/dc_nrpe/manifests/postgres.pp
+++ modules/dc_nrpe/manifests/postgres.pp
@@ -1,10 +1,12 @@
 # == Class: dc_nrpe::postgres
 #
-class dc_nrpe::postgres {
+class dc_nrpe::postgres (
+  $postgres_password, 
+){
 
   dc_nrpe::check { 'check_postgres_replication':
     path    => '/usr/local/bin/check_postgres_replication',
-    content => template('dc_nrpe/check_postgres_replication.erb'),
+    content => template('dc_nrpe/check_pg_streaming_replication.erb'),
     sudo    => true,
   }
 
