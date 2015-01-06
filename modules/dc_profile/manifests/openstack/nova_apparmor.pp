9b61ef9d74a2235f1661487d91509390e1f0ff9d
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-01-06 20:35:21 +0000

    Correct path

--- modules/dc_profile/manifests/openstack/nova_apparmor.pp
+++ modules/dc_profile/manifests/openstack/nova_apparmor.pp
@@ -13,7 +13,7 @@ class dc_profile::openstack::nova_apparmor {
 
   # Install modified libvirt-qemu configuration for apparmor
   file { 'libvirt-qemu-apparmor':
-    path   => '/etc/apparmor.d/abstractions/libvirt-qemu-apparmor',
+    path   => '/etc/apparmor.d/abstractions/libvirt-qemu',
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
