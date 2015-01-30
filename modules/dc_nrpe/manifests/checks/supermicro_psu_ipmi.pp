0f73d39540b1dd2f19c91d7c29d156773e37c301
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-01-30 15:25:02 +0000

    Helps if you actually install the check

--- modules/dc_nrpe/manifests/checks/supermicro_psu_ipmi.pp
+++ modules/dc_nrpe/manifests/checks/supermicro_psu_ipmi.pp
@@ -3,8 +3,9 @@
 class dc_nrpe::checks::supermicro_psu_ipmi {
 
   dc_nrpe::check { 'check_supermicro_psu_ipmi':
-    path => '/usr/lib/nagios/plugins/check_supermicro_psu_ipmi',
-    sudo => true,
+    path   => '/usr/local/bin/check_supermicro_psu_ipmi.sh',
+    source => 'puppet:///modules/dc_nrpe/check_supermicro_psu_ipmi.sh',
+    sudo   => true,
   }
 
 }
