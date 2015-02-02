9b83fdd985f06ae4726964e732265c29d3039b50
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-02-02 20:38:37 +0000

    Add handling for codec config

--- modules/dc_logstash/manifests/client/register.pp
+++ modules/dc_logstash/manifests/client/register.pp
@@ -1,19 +1,23 @@
 # == Define: dc_logstash::client::register
 #
-define dc_logstash::client::register ($logs, $fields, $order='10') {
+define dc_logstash::client::register ($logs, $fields, $order='10', $codec_hash=undef ) {
 
-  validate_hash($fields)
+    validate_hash($fields)
 
-  $default_fields = {
-    'shipper'   => 'log-courier' }
+    if $codec_hash {
+      validate_hash($codec_hash)
+    }
 
-  $merged_fields = merge($default_fields, $fields)
+    $default_fields = {
+      'shipper'   => 'log-courier' }
 
-  $json_fields = sorted_json($merged_fields)
+    $merged_fields = merge($default_fields, $fields)
 
-  concat::fragment { "log_courier_log_${name}":
-    target  => '/etc/log-courier/log-courier.conf',
-    order   => $order,
-    content => template('dc_logstash/client/log-courier_client_log.erb'),
-  }
+    $json_fields = sorted_json($merged_fields)
+
+    concat::fragment { "log_courier_log_${name}":
+      target  => '/etc/log-courier/log-courier.conf',
+      order   => $order,
+      content => template('dc_logstash/client/log-courier_client_log.erb'),
+    }
 }
