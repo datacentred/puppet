4521fbcc2b02368d1e92bdeeb6825754f3837107
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-02-02 21:15:56 +0000

    Fix indentation

--- modules/dc_logstash/manifests/client/register.pp
+++ modules/dc_logstash/manifests/client/register.pp
@@ -2,22 +2,23 @@
 #
 define dc_logstash::client::register ($logs, $fields, $order='10', $codec_hash=undef ) {
 
-    validate_hash($fields)
+  validate_hash($fields)
 
-    if $codec_hash {
-      validate_hash($codec_hash)
-    }
+  if $codec_hash {
+    validate_hash($codec_hash)
+  }
 
-    $default_fields = {
-      'shipper'   => 'log-courier' }
+  $default_fields = {
+    'shipper'   => 'log-courier'
+  }
 
-    $merged_fields = merge($default_fields, $fields)
+  $merged_fields = merge($default_fields, $fields)
 
-    $json_fields = sorted_json($merged_fields)
+  $json_fields = sorted_json($merged_fields)
 
-    concat::fragment { "log_courier_log_${name}":
-      target  => '/etc/log-courier/log-courier.conf',
-      order   => $order,
-      content => template('dc_logstash/client/log-courier_client_log.erb'),
-    }
+  concat::fragment { "log_courier_log_${name}":
+    target  => '/etc/log-courier/log-courier.conf',
+    order   => $order,
+    content => template('dc_logstash/client/log-courier_client_log.erb'),
+  }
 }
