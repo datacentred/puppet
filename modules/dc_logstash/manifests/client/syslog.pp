ba9bd536aecadedbc87d5405dcd24b228d8a4b47
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-02-03 16:57:42 +0000

    Add codec hash option to syslog

--- modules/dc_logstash/manifests/client/syslog.pp
+++ modules/dc_logstash/manifests/client/syslog.pp
@@ -2,12 +2,15 @@
 #
 # Forwards syslog
 #
-class dc_logstash::client::syslog {
+class dc_logstash::client::syslog (
+  $syslog_codec_hash=undef,
+){
 
   dc_logstash::client::register { 'syslog':
     logs          => '/var/log/syslog',
     fields        => {
       'type'      => 'syslog',
-    }
+    },
+    codec_hash    => $syslog_codec_hash,
   }
 }
