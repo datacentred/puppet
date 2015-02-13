8f9a65f912af8907b182bb5ab105b0ba77564220
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-02-13 15:19:53 +0000

    Add NRPE checks for BMC

--- modules/dc_bmc/manifests/params.pp
+++ modules/dc_bmc/manifests/params.pp
@@ -10,7 +10,11 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class dc_bmc::params {
+class dc_bmc::params (
+  $supermicro_fw_version,
+  $hp_fw_version,
+  $dell_fw_version,
+){
 
   if $::productname == 'X8DTT-H' {
     $ipmi_smnew_admin_user_slot = '9'
@@ -22,21 +26,25 @@ class dc_bmc::params {
     /ProLiant BL/: {
       $ipmi_user_channel = '2'
       $ipmi_admin_user_slot = '1'
+      $std_fw_version = $hp_fw_version
     }
 
     'X8DTT-H': {
       $ipmi_user_channel = '1'
       $ipmi_admin_user_slot = '2'
+      $std_fw_version = $supermicro_fw_version
     }
 
     /PowerEdge/: {
       $ipmi_user_channel = '1'
       $ipmi_admin_user_slot = '2'
+      $std_fw_version = $dell_fw_version
     }
 
     default: {
       $ipmi_user_channel = '1'
       $ipmi_admin_user_slot = '1'
+      $std_fw_version = undef
     }
   }
 
