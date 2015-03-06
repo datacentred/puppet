72cec328cdecb61e6baa50d5dd4e97b8fa7d4401
Nick Jones <nick.jones@datacentred.co.uk>
2015-03-06 18:07:08 +0000

    Add Augeas configuration to manage /etc/network/interfaces for p1p1

--- modules/dc_profile/manifests/ceph/mon.pp
+++ modules/dc_profile/manifests/ceph/mon.pp
@@ -21,4 +21,18 @@ class dc_profile::ceph::mon {
   Class['dc_ceph::exports'] ->
   Class['dc_ceph::keybackup']
 
+  # Ensure our 10GbE interfaces are properly configured
+  $ceph_public = 'p1p1'
+
+  augeas { $ceph_public:
+    context => '/files/etc/network/interfaces',
+    changes => [
+        "set auto[child::1 = '${ceph_public}']/1 ${ceph_public}",
+        "set iface[. = '${ceph_public}'] ${ceph_public}",
+        "set iface[. = '${ceph_public}']/family inet",
+        "set iface[. = '${ceph_public}']/method dhcp",
+        "set iface[. = '${ceph_public}']/pre-up '/sbin/ip link set ${ceph_public} mtu 9000'",
+    ],
+  }
+
 }
