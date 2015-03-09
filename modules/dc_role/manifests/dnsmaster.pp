227c4740a711572c8d024c1796a570b5e32abc86
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-03-09 10:01:19 +0000

    Move production specific includes up to this level

--- modules/dc_role/manifests/dnsmaster.pp
+++ modules/dc_role/manifests/dnsmaster.pp
@@ -15,7 +15,9 @@ class dc_role::dnsmaster inherits dc_role {
   contain dc_profile::net::dnsbackup
   contain dc_profile::net::dns_master
   contain dc_profile::net::dhcpd_master
+  contain dc_apparmor::dhcpd
   contain dc_profile::net::tftp_syncmaster
   contain dc_profile::net::foreman_proxy
+  contain dc_icinga::hostgroup_dhcp
 
 }
