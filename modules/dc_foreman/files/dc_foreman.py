33715c64685c3a3ca7008792086b51c4c66fa8a4
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-04-02 18:02:06 +0100

    Add a server side check against Foreman for net services

---
+++ modules/dc_foreman/files/dc_foreman.py
@@ -0,0 +1,120 @@
+"""
+DataCentred Foreman module
+Expects a config file in ini format eg.
+      
+[foreman]
+foreman_admin_user = some_user
+foreman_admin_pw = some_password
+foreman_api_baseurl = https://foreman-server/api/v2/
+cert_path = a_cert
+cacert_path = a_cacert
+key_path = a_key
+  
+Usage:
+import dc_foreman
+foreman_server = dc_foreman.Foreman(configfile)
+foreman_server.check_api_endpoint()
+
+"""
+import requests
+import json
+import sys
+from base64 import b64encode
+import os
+import ConfigParser
+
+class Foreman(object):
+    """
+    Foreman class
+    """
+    def __init__(self, configfile):
+        try:
+            parser = ConfigParser.SafeConfigParser()
+            parser.read(configfile)
+        except ConfigParser.ParsingError, err:
+            print 'Could not parse config file:', err
+            sys.exit(1)
+        self.foreman_api_baseurl = parser.get('foreman', 'foreman_api_baseurl')
+        foreman_admin_user = parser.get('foreman', 'foreman_admin_user')
+        foreman_admin_pw = parser.get('foreman', 'foreman_admin_pw')
+        cert_path = parser.get('foreman', 'cert_path')
+        key_path = parser.get('foreman', 'key_path')
+        cacert_path = parser.get('foreman', 'cacert_path')
+        auth_encode = b64encode('%s:%s' % (foreman_admin_user,
+                                    foreman_admin_pw)).decode("ascii")
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Basic %s'
+            % auth_encode}
+        self.cacert = cacert_path
+        self.certs = (cert_path, key_path)
+
+    	# Disable warnings on latest version of requests
+    	if requests.__version__ >= 2:
+        	requests.packages.urllib3.disable_warnings()
+
+
+    def get_from_api(self, api_url):
+        """
+        Get stuff from Foreman
+        """
+        url = self.foreman_api_baseurl + api_url + '?per_page=10000'
+        try:
+            req = requests.get(
+                 url, verify=self.cacert,
+                    headers=self.headers, cert=self.certs)
+        except requests.exceptions.RequestException as err:
+            print "Error - got %s" % err
+            sys.exit(1)
+        if req.status_code == 200:
+            data = json.loads(req.text)
+            return data['results']
+        else:
+            print "Unexpected return code %s" % req.status_code
+            print req.text
+            sys.exit(1)
+
+   # Check Foreman API endpoint
+    def check_api_endpoint(self):
+        """
+        Check the Foreman API endpoint is working
+        """
+        try:
+            req = requests.get(self.foreman_api_baseurl, verify=self.cacert,
+                    headers=self.headers, cert=self.certs)
+        except requests.exceptions.RequestException as err:
+            print "Connection Error - got %s" % err
+            sys.exit(1)
+        if req.status_code != 200:
+            print "Connection Error - got %s" % req.status_code
+            sys.exit(1)
+        else:
+            return True
+
+    def load_interfaces(self):
+        """
+        Return a dictionary with all Foreman defined interfaces
+        """
+        ints = []
+        for host in self.get_from_api('hosts'):
+            # Load the main interface
+            ints.append({
+                'name':host['name'].encode("ascii"),
+                'ip':host['ip'].encode("ascii"),
+                'mac':host['mac'].encode("ascii"),
+                'type':'main'
+            })
+          # Check for any additional interfaces
+            for foreman_int in self.get_from_api(
+                        'hosts/' + str(host['id']) + '/interfaces'):
+                if foreman_int['managed'] == True:
+                    int_fqdn = foreman_int['name'] + '.' \
+                            + foreman_int['domain_name']
+                    ints.append({
+                        'name':int_fqdn.encode("ascii"),
+                        'ip':foreman_int['ip'].encode("ascii"),
+                        'mac':foreman_int['mac'].encode("ascii"),
+                        'type':foreman_int['type'].encode("ascii")
+                    })
+        return ints
---
+++ modules/dc_foreman/files/foreman_check.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+"""
+Script to check Foreman data against network services
+Requires the dc_foreman and dc_omapi python modules
+"""
+import sys
+import socket
+import os
+import ConfigParser
+import dc_foreman
+import dc_omapi
+from pypureomapi import OmapiErrorNotFound
+
+# Look up hostname in DNS
+def dns_lookup(name):
+    """
+    Look up hostname in DNS
+    """
+    try:
+        ip_addr = socket.gethostbyname(name)
+        return ip_addr
+    except socket.gaierror:
+        raise
+
+############################################
+
+def main():
+    """
+    Check proxy config against Foreman
+    Expects config file in ini format eg.
+
+    [foreman]
+    foreman_admin_user = some_user
+    foreman_admin_pw = some_password
+    foreman_api_baseurl = https://foreman-server/api/v2/
+    cert_path = a_cert
+    cacert_path = a_cacert
+    key_path = a_key
+
+    [omapi]
+    omapi_port = 7911
+    omapi_key = some_key
+    omapi_secret = some_secret
+    dhcp_server = 127.0.0.1
+
+    [tftp]
+    pxe_root = /var/tftpboot/pxelinux.cfg
+
+    """
+    configfile = '/usr/local/etc/foreman_check.config'
+
+    try:
+        parser = ConfigParser.SafeConfigParser()
+        parser.read(configfile)
+    except ConfigParser.ParsingError, err:
+        print 'Could not parse config file:', err
+        sys.exit(1)
+    pxe_root = parser.get('pxe', 'pxe_root')
+
+    # Create a Foreman object
+    foreman_server = dc_foreman.Foreman(configfile)
+
+    # Check we can reach the Foreman API
+    foreman_server.check_api_endpoint()
+
+    failures = []
+    errors = 0
+
+    # Load a dictionary with all of the defined interfaces in Foreman
+    ints = foreman_server.load_interfaces()
+
+    # Create an OMAPI Wrapper object
+    omapi_wrapper = dc_omapi.OmapiWrapper(configfile)
+
+    for interface in ints:
+        # Check if the IP for the host entry is correct
+        try:
+            ip_addr = omapi_wrapper.omapi_lookup(interface['mac'], 'host')
+            if ip_addr != interface['ip']:
+                errors += 1
+                failures.append(
+		    "Host entry in DHCP does not match Foreman %s %s, expected %s received %s"
+                    % (interface['name'], interface['mac'],
+                        interface['ip'], ip_addr))
+        except OmapiErrorNotFound:
+            errors += 1
+            failures.append("No host entry in DHCP for %s %s"
+                    % (interface['name'], interface['ip']))
+
+        # Test for any dynamic leases associated with Foreman MAC addresses
+        try:
+            lease = omapi_wrapper.omapi_lookup(interface['mac'], 'lease')
+            if getattr(lease, 'state') == 'active':
+                failures.append("Active dynamic lease found for %s %s"
+               	    % (interface['name'], interface['mac']))
+                errors += 1
+        except OmapiErrorNotFound:
+            next
+
+        # Check DNS is correct
+        try:
+            dns_ip = dns_lookup(interface['name'])
+            if dns_ip != interface['ip']:
+                errors += 1
+                failures.append("DNS address for %s does not match Foreman, expected %s got %s"
+                     % (interface['name'], interface['ip'], dns_ip))
+        except socket.gaierror:
+            errors += 1
+            failures.append("No DNS entry found for %s" % interface['name'])
+
+        # Check for each MAC there is a correct tftp entry
+        # Munge the mac into the expected format for PXE
+        if interface['type'] in ['bond', 'main']:
+            if not os.path.exists(
+                    pxe_root + '/01-' + interface['mac'].replace(':', '-')):
+                errors += 1
+                failures.append("No PXE config found for %s %s"
+                        % (interface['name'], interface['mac']))
+
+    if errors != 0:
+        print "----------------------------------------------------------------"
+        print "The following errors were found during automated Foreman checks:"
+        print "----------------------------------------------------------------"
+        for fail in failures:
+            print fail
+
+if __name__ == "__main__":
+    main()
