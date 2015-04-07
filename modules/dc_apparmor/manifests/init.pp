0cc680f4794c0901354c64d88301948a364a7f74
Simon Murray <simon.murray@datacentred.co.uk>
2015-04-07 16:16:55 +0100

    Update apparmor status check
    * From 14.10 onwards init wont work when checking the status
    * This updates the service to use the apparmor_status command
    * Backwards compatible with 12.04 and 14.04

--- modules/dc_apparmor/manifests/init.pp
+++ modules/dc_apparmor/manifests/init.pp
@@ -13,12 +13,19 @@
 # [Remember: No empty lines between comments and class definition]
 class dc_apparmor {
 
-  service { 'apparmor':
-    ensure => running
-  }
-
   package { 'apparmor':
     ensure => installed,
+  } ~>
+
+  # Note: there is a bug in the dpkg puppet provider whereby you need
+  #       to use another provider type to override the default status
+  #       command as we do here to be forward compatible with 14.10+
+  service { 'apparmor':
+    ensure   => running,
+    provider => 'base',
+    start    => 'service apparmor start',
+    stop     => 'service apparmor stop',
+    status   => 'apparmor_status',
   }
 
 }
