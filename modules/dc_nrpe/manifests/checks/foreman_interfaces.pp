2e53ff6e223a33f5e61aa0b576d100de9e37f98f
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-04-08 10:17:14 +0100

    Add NRPE check for interface config against Foreman

---
+++ modules/dc_nrpe/manifests/checks/foreman_interfaces.pp
@@ -0,0 +1,12 @@
+# == Class: dc_nrpe::checks::foreman_interfaces
+#
+class dc_nrpe::checks::foreman_interfaces {
+
+  dc_nrpe::check { 'check_foreman_interfaces':
+    path    => '/usr/local/bin/check_foreman_interfaces.py',
+    content => template('dc_nrpe/check_foreman_interfaces.py.erb'),
+    sudo    => true,
+  }
+
+
+}
