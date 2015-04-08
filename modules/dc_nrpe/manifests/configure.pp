f728f12c857a981251f9bbe3f575b1448101eef0
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-04-08 14:39:53 +0100

    Fix indentation

--- modules/dc_nrpe/manifests/configure.pp
+++ modules/dc_nrpe/manifests/configure.pp
@@ -3,12 +3,12 @@
 class dc_nrpe::configure {
 
   file { '/etc/nagios/nrpe.cfg':
-    ensure  => file,
-    path    => '/etc/nagios/nrpe.cfg',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/dc_nrpe/nrpe.cfg',
+    ensure => file,
+    path   => '/etc/nagios/nrpe.cfg',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/dc_nrpe/nrpe.cfg',
   }
 
   file { '/etc/nagios/nrpe.d':
@@ -18,9 +18,9 @@ class dc_nrpe::configure {
   }
 
   concat { '/etc/nagios/nrpe.d/dc_nrpe_check.cfg':
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
   }
 
   include ::dc_nrpe::checks::ceilometer_compute
