d33d41f10b8af54b2b86423fbb8c7a8014f0aea2
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-04-22 17:36:22 +0100

    Clean up rebase typo

--- modules/dc_role/manifests/dnsslave.pp
+++ modules/dc_role/manifests/dnsslave.pp
@@ -17,7 +17,6 @@ class dc_role::dnsslave inherits dc_role {
   contain dc_profile::net::dhcpd_slave
   contain dc_profile::net::tftp_syncslave
   contain dc_profile::net::foreman_proxy
-  contain dc_backup::duplicity_dnsbackups
   contain dc_icinga::hostgroup_ntp
   contain dc_backup::gpg_keys
   contain dc_profile::net::duplicity_dns
