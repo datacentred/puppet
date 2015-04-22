f88dcdd2cea4abd03b01234077587fc2669dbd18
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-04-22 17:37:01 +0100

    Remove old backup class

--- modules/dc_role/manifests/dnsslave.pp
+++ modules/dc_role/manifests/dnsslave.pp
@@ -12,7 +12,6 @@
 #
 class dc_role::dnsslave inherits dc_role {
 
-  contain dc_profile::net::dnsbackup
   contain dc_profile::net::dns_slave
   contain dc_profile::net::dhcpd_slave
   contain dc_profile::net::tftp_syncslave
