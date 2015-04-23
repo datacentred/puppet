de6e1708cbd723ab2538cd224b6ef82e3eb4c657
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-04-23 10:28:48 +0100

    This cannot possibly have ever worked

--- modules/dc_dhcp/files/dhcp_sync_hosts.sh
+++ modules/dc_dhcp/files/dhcp_sync_hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! diff /tmp/dhcp.hosts /etc/dhcp/dhcpd.hosts; then
+if ! diff /tmp/dhcpd.hosts /etc/dhcp/dhcpd.hosts; then
   TIMESTAMP=`date | tr '\n' ']'`
   echo "[${TIMESTAMP} Synchronizing /etc/dhcp/dhcpd.hosts" > /var/log/dhcp_sync_hosts.log
   cp /tmp/dhcp.hosts /etc/dhcp/dhcpd.hosts
