8dc0a54702babf2a270fb33b63e433be2b169a01
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-04-23 10:34:08 +0100

    Make script exit 1 on a failure

--- modules/dc_dhcp/files/dhcp_sync_hosts.sh
+++ modules/dc_dhcp/files/dhcp_sync_hosts.sh
@@ -3,7 +3,15 @@
 if ! diff /tmp/dhcpd.hosts /etc/dhcp/dhcpd.hosts; then
   TIMESTAMP=`date | tr '\n' ']'`
   echo "[${TIMESTAMP} Synchronizing /etc/dhcp/dhcpd.hosts" > /var/log/dhcp_sync_hosts.log
-  cp /tmp/dhcp.hosts /etc/dhcp/dhcpd.hosts
-  sudo /etc/init.d/isc-dhcp-server restart
+  if ! cp /tmp/dhcp.hosts /etc/dhcp/dhcpd.hosts
+  then
+       echo "Could not copy file"
+       exit 1
+  fi
+  if ! sudo /etc/init.d/isc-dhcp-server restart
+  then
+       echo "Could not restart server"
+       exit 1
+  fi
 fi
 
