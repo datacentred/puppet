385cf9af3462bb17f9f12eea7ca64d0f244616f6
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-04-30 15:16:34 +0100

    Add collectd iostat plugin

---
+++ modules/dc_collectd/manifests/agent/iostat.pp
@@ -0,0 +1,34 @@
+# == Class: dc_collectd::agent::iostat
+class dc_collectd::agent::iostat {
+
+  # Install pre-reqs
+  $prereqs = [ 'sysstat' ]
+  package { $prereqs :
+    ensure => present,
+  }
+
+  # Make plugins directory structure
+  $pythondirs = [
+    '/usr/lib/collectd/python'
+  ]
+  file { $pythondirs:
+    ensure  => directory,
+    require => Package['collectd'],
+  }
+
+  # Copy in new plugin
+  file { '/usr/lib/collectd/python/collectd_iostat_python.py':
+    ensure  => file,
+    require => File[$pythondirs],
+    source  => 'puppet:///modules/dc_collectd/collectd_iostat_python.py',
+  }
+
+  # Add config
+  file { '/etc/collectd/conf.d/10-iostat.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/dc_collectd/10-iostat.conf',
+    require => File['/usr/lib/collectd/python/collectd_iostat_python.py'],
+    notify  => Service['collectd'],
+  }
+
+}
