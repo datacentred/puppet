c12538f56f7e2dae893d2c0936fca96636c67776
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-05-14 07:16:36 +0100

    Add beaver logging on aarch64

--- modules/dc_logstash/manifests/params.pp
+++ modules/dc_logstash/manifests/params.pp
@@ -12,13 +12,16 @@
 class dc_logstash::params (
   $logstash_server,
   $logstash_syslog_port,
+  $logstash_beavertcp_port,
   $logcourier_port,
   $logcourier_version,
   $logstash_grok_patterns_dir,
   $logstash_cert_alias,
-  $logstash_beavertcp_port,
   $riemann_dev_host,
   $elasticsearch_host,
   $elasticsearch_embedded,
   $elasticsearch_protocol,
+  $logstash_api_version,
+  $log_shipper,
+  $beaver_timeout,
 ){}
