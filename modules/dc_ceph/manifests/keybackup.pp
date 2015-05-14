32108f3cd0dad058081dd82ef84e79f511ccf9eb
Max Siegieda <max.siegieda@datacentred.co.uk>
2015-05-14 16:50:11 +0100

    Oops, add dates to the ceph key lists produced

--- modules/dc_ceph/manifests/keybackup.pp
+++ modules/dc_ceph/manifests/keybackup.pp
@@ -18,8 +18,10 @@ class dc_ceph::keybackup (
 
   if $::hostname == $primary_mon {
 
+
+
   dc_backup::dc_duplicity_job { "${::hostname}_ceph_keys" :
-    pre_command    => 'ceph auth list > /var/ceph-keybackup/keys.txt',
+    pre_command    => 'ceph auth list > /var/ceph-keybackup/keys_`date +"%m_%d_%Y"`.txt',
     source_dir     => '/var/ceph-keybackup',
     backup_content => 'ceph_keys',
   }
