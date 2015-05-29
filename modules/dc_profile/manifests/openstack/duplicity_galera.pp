b0e9e0783359610f74890d1f71fbc3c7cce73619
Max Siegieda <max.siegieda@datacentred.co.uk>
2015-05-29 13:06:26 +0100

    stop mongodb from emailing passwords at us by wrapping dumps in a script file. Also whitespace and commas

--- modules/dc_profile/manifests/openstack/duplicity_galera.pp
+++ modules/dc_profile/manifests/openstack/duplicity_galera.pp
@@ -24,13 +24,12 @@ class dc_profile::openstack::duplicity_galera (
   file { '/usr/local/sbin/galera_dump_for_duplicity.sh':
     ensure  => file,
     content => "#!/bin/bash\n/usr/bin/mysqldump -u ${osdbmq_galera_backup_user} -p${osdbmq_galera_backup_pw} --opt --all-databases | bzcat -zc > /var/local/backup/galera-`date +%Y%m%d-%H%M%S`.sql.bz2",
-    mode    => '0700'
+    mode    => '0700',
   }
 
   file { '/var/local/backup':
     ensure => directory,
   }
-
 
   tidy { 'galera_dumps':
     path    => '/var/local/backup',
