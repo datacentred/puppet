9a6c831332cc4821d9f36ea65d0267b6321e430a
Sean Handley <sean.handley@gmail.com>
2015-06-08 13:45:32 +0100

    Move hubot stuff into yaml.

--- modules/dc_profile/manifests/hubot/marvin.pp
+++ modules/dc_profile/manifests/hubot/marvin.pp
@@ -14,32 +14,17 @@ class dc_profile::hubot::marvin {
 
   include ::redis
 
-  package {'npm':} ->
+  apt::source{'nodesource_0.12':
+    location => 'https://deb.nodesource.com/node_0.12',
+    release  => 'trusty',
+    repos => 'main',
+    key => {
+      source => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+    }
+  }
 
-  file { '/usr/local/bin/node':
-    ensure => 'link',
-    target => '/usr/bin/nodejs',
-  } ->
-
-  class { 'hubot':
-    env_export          => {
-      'HUBOT_HIPCHAT_JID'               => hiera(hubot::marvin::hipchat_jid),
-      'HUBOT_HIPCHAT_PASSWORD'          => hiera(hubot::marvin::hipchat_password),
-      'HUBOT_HIPCHAT_ROOMS'             => hiera(hubot::marvin::hipchat_rooms),
-      'HUBOT_PAGERDUTY_API_KEY'         => hiera(hubot::marvin::pagerduty::api_key),
-      'HUBOT_PAGERDUTY_SCHEDULE_ID'     => hiera(hubot::marvin::pagerduty::schedule_id),
-      'HUBOT_PAGERDUTY_SERVICE_API_KEY' => hiera(hubot::marvin::pagerduty::service_api_key),
-      'HUBOT_PAGERDUTY_SUBDOMAIN'       => hiera(hubot::marvin::pagerduty::subdomain),
-      'HUBOT_ICINGA_HOST'               => hiera(nagios_api_host),
-      'HUBOT_ICINGA_USER'               => hiera(nagios_api_username),
-      'HUBOT_ICINGA_PASSWORD'           => hiera(nagios_api_password),
-    },
-    git_source          => 'git@github.com:datacentred/marvin.git',
-    ssh_privatekey_file => 'puppet:///modules/dc_puppet/keys/marvin/id_rsa',
-    nodejs_manage_repo  => false,
-    bot_name            => 'marvin',
-    display_name        => 'Marvin',
-    adapter             => 'hipchat',
+  class { 'hubot': 
+    nodejs_manage_repo => false
   }
 
 }
