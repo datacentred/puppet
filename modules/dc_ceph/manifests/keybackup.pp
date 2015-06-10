ad1171c5ad8c22c6ead8c3877bec13842eefa42b
Simon Murray <simon.murray@datacentred.co.uk>
2015-06-10 10:32:19 +0100

    Add ceph backup wrapper
    * Strips stderr output (aka spam emails) from successful output

--- modules/dc_ceph/manifests/keybackup.pp
+++ modules/dc_ceph/manifests/keybackup.pp
@@ -22,9 +22,17 @@ class dc_ceph::keybackup (
       ensure => directory,
     }
 
+    file { '/usr/local/bin/cephkeybackup':
+      ensure => file,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      source => 'puppet:///modules/dc_ceph/cephkeybackup',
+    }
+
     #percentage signs in cron job are escaped because cron subs them for newlines otherwise
     dc_backup::dc_duplicity_job { "${::hostname}_ceph_keys" :
-      pre_command    => 'ceph auth list > /var/ceph-keybackup/keys_`date +"\%m_\%d_\%Y"`.txt',
+      pre_command    => '/usr/local/bin/cephkeybackup -o /var/ceph-keybackup/keys_`date +"\%m_\%d_\%Y"`.txt',
       source_dir     => '/var/ceph-keybackup',
       backup_content => 'ceph_keys',
     }
