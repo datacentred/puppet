774e180579d7b7c0c83f5a9bc28ac2f3be7fa49e
Simon Murray <simon.murray@datacentred.co.uk>
2015-06-24 13:45:02 +0100

    Fix file permissions in iostat
    * Fix the file ownership and permissions which were showing up as false
      positives on a --noop run
    * On one host I looked at these files were owned by git:git...

--- modules/dc_collectd/manifests/agent/iostat.pp
+++ modules/dc_collectd/manifests/agent/iostat.pp
@@ -4,24 +4,30 @@ class dc_collectd::agent::iostat {
   # Install pre-reqs
   ensure_packages('sysstat')
 
+  File {
+    owner => 'root',
+    group => 'root',
+  }
+
   # Make plugins directory structure
   file { '/usr/lib/collectd/python':
     ensure  => directory,
+    mode    => '0755',
     require => Package['collectd'],
-  }
+  } ->
 
   # Copy in new plugin
   file { '/usr/lib/collectd/python/collectd_iostat_python.py':
     ensure  => file,
-    require => File['/usr/lib/collectd/python'],
+    mode    => '0644',
     source  => 'puppet:///modules/dc_collectd/collectd_iostat_python.py',
-  }
+  } ->
 
   # Add config
   file { '/etc/collectd/conf.d/10-iostat.conf':
     ensure  => file,
+    mode    => '0644',
     source  => 'puppet:///modules/dc_collectd/10-iostat.conf',
-    require => File['/usr/lib/collectd/python/collectd_iostat_python.py'],
     notify  => Service['collectd'],
   }
 
