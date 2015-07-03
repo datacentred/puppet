503eb2cea6aee480f4037d646d1fcb20a1c8837f
Nick Jones <nick.jones@datacentred.co.uk>
2015-07-03 16:16:04 +0100

    Remove profile class and declaration in relevant role

--- modules/dc_role/manifests/graphite_core.pp
+++ modules/dc_role/manifests/graphite_core.pp
@@ -13,7 +13,6 @@
 class dc_role::graphite_core {
 
   contain dc_profile::perf::graphite
-  contain dc_profile::perf::gdash
   contain dc_profile::perf::collectd::poller
   contain dc_profile::perf::network_weathermap
   contain dc_profile::perf::grafana
