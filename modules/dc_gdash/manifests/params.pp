901a2fd6deda52abd1b5c8cc804d998eeb1b5484
Nick Jones <nick.jones@datacentred.co.uk>
2015-07-03 16:16:32 +0100

    Remove dc_gdash module

--- modules/dc_gdash/manifests/params.pp
+++
@@ -1,7 +0,0 @@
-# == Class: dc_gdash::params
-#
-class dc_gdash::params {
-  $graphite_server = hiera(graphite_server)
-  $whisper_root = '/var/opt/graphite/storage/whisper'
-  $reversedomain = join(reverse(split($::domain,'[.]')),'.')
-}
