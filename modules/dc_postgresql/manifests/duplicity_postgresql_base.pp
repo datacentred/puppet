3e7a146863b66ee0192eab96b8bf80a4e2bc50bb
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-07-24 11:55:09 +0100

    Make sure we actually get more than one backup a week

--- modules/dc_postgresql/manifests/duplicity_postgresql_base.pp
+++ modules/dc_postgresql/manifests/duplicity_postgresql_base.pp
@@ -23,7 +23,7 @@ class dc_postgresql::duplicity_postgresql_base {
   }
 
   dc_backup::dc_duplicity_job { "${::hostname}_postgresql_base" :
-    pre_command    => 'pg_basebackup --pgdata=/var/postgres_base_dump/ --format=tar --gzip --progress --username=postgres --no-password',
+    pre_command    => 'pg_basebackup --pgdata=/var/postgres_base_dump/`date +%F`/ --format=tar --gzip --progress --username=postgres --no-password',
     source_dir     => '/var/postgres_base_dump',
     backup_content => 'postgresql_base',
   }
