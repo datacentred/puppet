1d5903358cfc10480e8bb56f49558aa28cb4321e
Max Siegieda <max.siegieda@datacentred.co.uk>
2015-08-17 13:02:23 +0100

    change pg_basebackup command to output to dev null, quietening postgres emails

--- modules/dc_postgresql/manifests/duplicity_postgresql_base.pp
+++ modules/dc_postgresql/manifests/duplicity_postgresql_base.pp
@@ -23,16 +23,9 @@ class dc_postgresql::duplicity_postgresql_base {
   }
 
   dc_backup::dc_duplicity_job { "${::fqdn}_postgresql_base" :
-    pre_command    => 'pg_basebackup --pgdata=/var/postgres_base_dump/$(date +\%F)/ --format=tar --gzip --progress --username=postgres --no-password',
+    pre_command    => 'pg_basebackup --pgdata=/var/postgres_base_dump/$(date +\%F)/ --format=tar --gzip --username=postgres --no-password > /dev/null',
     source_dir     => '/var/postgres_base_dump',
     backup_content => 'postgresql_base',
-  }
-  #remove me once old hostname based scripts have been blatted
-  dc_backup::dc_duplicity_job { "${::hostname}_postgresql_base" :
-    pre_command    => 'pg_basebackup --pgdata=/var/postgres_base_dump/$(date +\%F)/ --format=tar --gzip --progress --username=postgres --no-password',
-    source_dir     => '/var/postgres_base_dump',
-    backup_content => 'postgresql_base',
-    cloud          => 'none',
   }
 
   tidy { 'postgres_base_dump':
