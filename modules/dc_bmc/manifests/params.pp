868e87bcb3ff8c34d8edc2f53f4093ecf4209dcb
Simon Murray <simon.murray@datacentred.co.uk>
2015-08-20 09:33:32 +0100

    Move BMC status checks into dc_nrpe
    Removes implicit requirement that to use BMC provisioning you
    need to install NRPE.  As an added bonus, this gets rid of
    scope.lookupvar() calls which silently return '' if you move the
    source.  Having the variables defined locally in the class that
    consumes them solves this by raising a compilation error.  Also
    updates R220 firmware to 2.0f across the board.

--- modules/dc_bmc/manifests/params.pp
+++ modules/dc_bmc/manifests/params.pp
@@ -10,11 +10,7 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class dc_bmc::params (
-  $supermicro_fw_version,
-  $hp_fw_version,
-  $dell_fw_version,
-){
+class dc_bmc::params {
 
   if $::productname == 'X8DTT-H' {
     $ipmi_smnew_admin_user_slot = '9'
@@ -26,25 +22,21 @@ class dc_bmc::params (
     /ProLiant BL/: {
       $ipmi_user_channel = '2'
       $ipmi_admin_user_slot = '1'
-      $std_fw_version = $hp_fw_version
     }
 
     'X8DTT-H': {
       $ipmi_user_channel = '1'
       $ipmi_admin_user_slot = '2'
-      $std_fw_version = $supermicro_fw_version
     }
 
     /PowerEdge/: {
       $ipmi_user_channel = '1'
       $ipmi_admin_user_slot = '2'
-      $std_fw_version = $dell_fw_version
     }
 
     default: {
       $ipmi_user_channel = '1'
       $ipmi_admin_user_slot = '1'
-      $std_fw_version = undef
     }
   }
 
