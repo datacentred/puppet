946c669228786737a1db7cda30f2669e53157be4
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-09-04 12:40:37 +0100

    Postfix mail queue checks should be different on MX's

--- modules/dc_nrpe/manifests/checks/postfix.pp
+++ modules/dc_nrpe/manifests/checks/postfix.pp
@@ -1,10 +1,13 @@
 # == Class: dc_nrpe::checks::postfix
 #
-class dc_nrpe::checks::postfix {
+class dc_nrpe::checks::postfix (
+  $warn,
+  $crit,
+){
 
   dc_nrpe::check { 'check_mailq_postfix':
     path => '/usr/lib/nagios/plugins/check_mailq',
-    args => '-M postfix -w 10 -c 30',
+    args => "-M postfix -w ${warn} -c ${crit}",
   }
 
 }
