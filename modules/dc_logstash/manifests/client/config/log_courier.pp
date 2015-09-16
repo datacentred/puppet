35d338811cbd779a4a3cc00cbefaeadc50bdae4c
Simon Murray <simon.murray@datacentred.co.uk>
2015-09-16 15:06:54 +0100

    Switch to PPA log-courier
    Uses the PPA log-courier package and init scripts.  Configuration is now
    derived from YAML and just spat out buy the JSON formatter.  The reason
    for this is that upstream log-courier will complain if we terminate the
    list of files with an empty hash (e.g. the only way concat based config
    can work).

--- modules/dc_logstash/manifests/client/config/log_courier.pp
+++ modules/dc_logstash/manifests/client/config/log_courier.pp
@@ -6,77 +6,22 @@ class dc_logstash::client::config::log_courier {
 
   include ::dc_logstash::client
 
-  $version = $::dc_logstash::client::version
-  $server = $::dc_logstash::client::server
-  $port = $::dc_logstash::client::port
-  $key = $::dc_logstash::client::key
-  $cert = $::dc_logstash::client::cert
-  $cacert = $::dc_logstash::client::cacert
-  $timeout = $::dc_logstash::client::timeout
+  $log_courier_config = $dc_logstash::client::log_courier_config
 
-  file { '/usr/sbin/log-courier':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => "puppet:///modules/dc_logstash/log_courier/log-courier-${version}",
-  }
+  package { 'log-courier':
+    ensure => installed,
+  } ->
 
-  file { '/usr/sbin/lc-admin':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/dc_logstash/log_courier/lc-admin',
-  }
-
-  file { '/etc/init.d/log-courier':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/dc_logstash/log_courier/log_courier.init',
-  }
-
-  file { '/etc/log-courier':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
-
-  file { '/var/lib/log-courier':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
-
-  concat { '/etc/log-courier/log-courier.conf':
+  file { '/etc/log-courier/log-courier.conf':
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    require => File['/etc/log-courier'],
-  }
-
-  concat::fragment { 'log_courier_header':
-    target  => '/etc/log-courier/log-courier.conf',
-    content => template('dc_logstash/client/log-courier_client_header.erb'),
-    order   => '01',
-  }
-
-  concat::fragment { 'log_courier_footer':
-    target  => '/etc/log-courier/log-courier.conf',
-    content => template('dc_logstash/client/log-courier_client_footer.erb'),
-    order   => '99',
-  }
+    content => inline_template('<%= JSON.pretty_generate(@log_courier_config) %>')
+  } ~>
 
   service { 'log-courier':
-    ensure    => running,
-    enable    => true,
-    require   => [ File['/usr/sbin/log-courier'], File['/etc/init.d/log-courier'] ],
-    subscribe => [ Concat['/etc/log-courier/log-courier.conf'], File['/usr/sbin/log-courier'] ],
+    ensure => 'running',
   }
 
   logrotate::rule { 'log-courier':
