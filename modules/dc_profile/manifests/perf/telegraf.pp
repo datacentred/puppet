185257e5f09feda46bec5f004920224ada330cb2
Nick Jones <nick.jones@datacentred.co.uk>
2015-09-24 12:34:30 +0100

    Replace collectd with telegraf in Core Services
    This change swaps out collectd for an InfluxDB-native metrics collector
    called telegraf.  Aside from performance benefits (as we'll be able to
    disable the Graphite-compatibility plugin in InfluxDB), it offers
    greater flexibility for how measurements are sent and allows additional
    tags to be specified for each metric.  In our case this is three Facts -
    domain, role, and hostgroup.
    
    Stunnel is still required as telegraf doesn't (yet) support SSL
    natively.

---
+++ modules/dc_profile/manifests/perf/telegraf.pp
@@ -0,0 +1,22 @@
+#
+# == Class dc_profile::perf::telegraf
+#
+class dc_profile::perf::telegraf {
+
+  include ::stunnel
+  include ::telegraf
+
+  $influxdb_server  = hiera(influxdb_server)
+  $influxdb_port    = hiera(influxdb_port)
+
+  stunnel::tun { 'influxdb':
+    accept      => '18086',
+    connect     => "${influxdb_server}:${influxdb_port}",
+    cert        => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
+    cafile      => '/var/lib/puppet/ssl/certs/ca.pem',
+    global_opts => {
+      'key' => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem" },
+    client      => true,
+  }
+
+}
