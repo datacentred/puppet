fc2a4bc38cdd6a434696e200c4da8eb313372ab0
Simon Murray <simon.murray@datacentred.co.uk>
2015-11-11 16:26:21 +0000

    Roll Out Icinga 2
    Updates the packages on arm to point at backports for utopic (vivid was
    put in there for logcourier support - may make a utopic build at some
    point to rectify).  Installs the generic set of stuff across all the
    things with will allow me to engineer solutions for memory and load
    type stuff that will occur on ceph and compute nodes

--- vagrant/default.pp
+++ vagrant/default.pp
@@ -8,7 +8,10 @@ Exec {
   ],
 }
 
-hiera_include('classes')
+$classes = hiera_array('classes')
+$excludes = hiera_array('excludes', [])
+$includes = delete($classes, $excludes)
+include $includes
 
 # If we're running in Vagrant and a role is defined, include it.
 if $::is_vagrant and $::vagrant_role {
