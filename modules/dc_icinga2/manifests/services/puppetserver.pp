0e19e2152b11b3187a662d56fadd1318fe9d9e27
Simon Murray <simon.murray@datacentred.co.uk>
2015-11-18 16:21:13 +0000

    Update Icinga2 Base Checks
    Jenkins returns a 403 and fails when checking / so check against /login instead.
    Puppet at the moment doesn't allow access to the /status API so revert to a plain
    TCP check for now until this is enabled, or we switch to puppet server. Finally
    make load average checks relative to the number of CPUs installed in the
    machine

--- modules/dc_icinga2/manifests/services/puppetserver.pp
+++ modules/dc_icinga2/manifests/services/puppetserver.pp
@@ -6,16 +6,22 @@ class dc_icinga2::services::puppetserver {
 
   icinga2::object::apply_service { 'puppetserver':
     import        => 'generic-service',
-    check_command => 'http',
+    check_command => 'tcp',
     vars          => {
-      'http_uri'         => '/environment/status/name',
-      'http_port'        => 8140,
-      'http_ssl'         => true,
-      'http_clientcert'  => '/var/lib/puppet/ssl/certs/$host.name$.pem',
-      'http_privatekey'  => '/var/lib/puppet/ssl/private_keys/$host.name$.pem',
-      'http_header'      => 'Accept: pson',
+      'tcp_port'         => 8140,
       'enable_pagerduty' => true,
     },
+    # TODO: Alter auth.conf to allow access to /status
+    #check_command => 'http',
+    #vars          => {
+    #  'http_uri'         => '/environment/status/name',
+    #  'http_port'        => 8140,
+    #  'http_ssl'         => true,
+    #  'http_clientcert'  => '/var/lib/puppet/ssl/certs/$host.name$.pem',
+    #  'http_privatekey'  => '/var/lib/puppet/ssl/private_keys/$host.name$.pem',
+    #  'http_header'      => 'Accept: pson',
+    #  'enable_pagerduty' => true,
+    #},
     zone          => 'host.name',
     assign_where  => 'match("puppet_*", host.vars.role)',
     target        => '/etc/icinga2/zones.d/global-templates/services.conf',
