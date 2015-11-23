d950b8e6b0bddbdc5c8d9c5f1a623ea756a4f5c4
Simon Murray <simon.murray@datacentred.co.uk>
2015-11-23 15:56:02 +0000

    [Icinga2] Enable BMC Checks
    Creates a new dc_icinga2_plugins module where we can define nagios checks in
    isolation away from the existing NRPE cruft.  The BMC check has been simplified
    and modified to accept commandline parameters in keeping with other checks.
    A bmc command has been added and a set of services which are applied only on
    the products affected.  Few other cleanups in there too defining default file
    targets and removing files that were committed erroneously.

---
+++ modules/dc_icinga2/manifests/services/bmc.pp
@@ -0,0 +1,100 @@
+# == Class: dc_icinga2::services::bmc
+#
+# Perform BMC configration checks
+#
+# === Parameters
+#
+# [*username*]
+#   User name to perform the check as
+#
+# [*password*]
+#   User password to perform the check with
+#
+class dc_icinga2::services::bmc (
+  $username,
+  $password,
+) {
+
+  Icinga2::Object::Apply_service {
+    target => '/etc/icinga2/zones.d/global-templates/services.conf',
+  }
+
+  icinga2::object::apply_service { 'bmc R220':
+    import        => 'generic_service',
+    check_command => 'bmc',
+    vars          => {
+      'bmc_host'     => 'host.vars.address_bmc',
+      'bmc_username' => $username,
+      'bmc_password' => $password,
+      'bmc_revision' => '2.0f',
+    },
+    zone          => 'host.name',
+    assign_where  => 'host.vars.productname == "PowerEdge R220"',
+  }
+
+  icinga2::object::apply_service { 'bmc R420':
+    import        => 'generic_service',
+    check_command => 'bmc',
+    vars          => {
+      'bmc_host'     => 'host.vars.address_bmc',
+      'bmc_username' => $username,
+      'bmc_password' => $password,
+      'bmc_revision' => '2.20',
+    },
+    zone          => 'host.name',
+    assign_where  => 'host.vars.productname == "PowerEdge R420"',
+  }
+
+  icinga2::object::apply_service { 'bmc BL460c G1':
+    import        => 'generic_service',
+    check_command => 'bmc',
+    vars          => {
+      'bmc_host'     => 'host.vars.address_bmc',
+      'bmc_username' => $username,
+      'bmc_password' => $password,
+      'bmc_revision' => '2.25',
+    },
+    zone          => 'host.name',
+    assign_where  => 'host.vars.productname == "ProLiant BL460c G1"',
+  }
+
+  icinga2::object::apply_service { 'bmc BL465c G1':
+    import        => 'generic_service',
+    check_command => 'bmc',
+    vars          => {
+      'bmc_host'     => 'host.vars.address_bmc',
+      'bmc_username' => $username,
+      'bmc_password' => $password,
+      'bmc_revision' => '2.25',
+    },
+    zone          => 'host.name',
+    assign_where  => 'host.vars.productname == "ProLiant BL465c G1"',
+  }
+
+  icinga2::object::apply_service { 'bmc X8DTT-H':
+    import        => 'generic_service',
+    check_command => 'bmc',
+    vars          => {
+      'bmc_host'     => 'host.vars.address_bmc',
+      'bmc_username' => $username,
+      'bmc_password' => $password,
+      'bmc_revision' => '2.20',
+    },
+    zone          => 'host.name',
+    assign_where  => 'host.vars.productname == "X8DTT-H"',
+  }
+
+  icinga2::object::apply_service { 'bmc X9DRT':
+    import        => 'generic_service',
+    check_command => 'bmc',
+    vars          => {
+      'bmc_host'     => 'host.vars.address_bmc',
+      'bmc_username' => $username,
+      'bmc_password' => $password,
+      'bmc_revision' => '3.40',
+    },
+    zone          => 'host.name',
+    assign_where  => 'host.vars.productname == "X9DRT"',
+  }
+
+}
