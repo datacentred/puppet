9f2d15e9a7148284c65ab19af30dbf67bc77e465
Simon Murray <simon.murray@datacentred.co.uk>
2015-11-25 14:43:26 +0000

    [Ceph] Make Disk Tuning Persistent
    Adds in a udev rule to set the read ahead and queue depth for OSDs nodes
    which will get applied as soon as the disks are detected during boot

--- modules/dc_profile/manifests/ceph/osd.pp
+++ modules/dc_profile/manifests/ceph/osd.pp
@@ -51,4 +51,16 @@ class dc_profile::ceph::osd {
     ],
   }
 
+  # ***README***
+  # This only caters for machines post reboot, OSD nodes need to generate the same
+  # file during pre-seed so they come up initially with the correct rules in place
+  # so if you modify this please do the same in Foreman
+  file { '/etc/udev/rules.d/99-osd.rules':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => 'SUBSYSTEM=="block", ATTR{queue/rotational}=="1", ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/read_ahead_kb}="1024", ATTR{queue/nr_requests}="1024"',
+  }
+
 }
