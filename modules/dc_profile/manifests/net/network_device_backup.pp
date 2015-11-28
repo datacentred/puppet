51520f52cab94cb45998ae7b46d403c89da7fda1
millingworth <matt.illingworth@gmail.com>
2015-11-28 00:08:04 +0000

    [net_device_backup] Changed SSH key and locked down iptables
    As this backup host is offsite I have locked down the iptables to only allow connections from DC subnets.
    The SSH key has also been updated.

--- modules/dc_profile/manifests/net/network_device_backup.pp
+++ modules/dc_profile/manifests/net/network_device_backup.pp
@@ -12,6 +12,46 @@
 # Sample Usage:
 class dc_profile::net::network_device_backup {
 
+    include ::firewall
+    
+    firewall { '001 accept all to lo interface':
+        proto   => 'all',
+        iniface => 'lo',
+        action  => 'accept',
+    }
+
+    firewall { '002 Allow inbound SSH':
+        source => '185.98.148.0/22',
+        dport  => 22,
+        proto  => tcp,
+        action => accept,
+    }
+
+    firewall { '003 Allow inbound SSH':
+        source => '85.199.236.48/29',
+        dport  => 22,
+        proto  => tcp,
+        action => accept,
+    }
+
+    firewall { '004 Allow inbound SSH':
+        source => '185.43.216.0/22',
+        dport  => 22,
+        proto  => tcp,
+        action => accept,
+    }
+
+    firewall { '005 accept related established rules':
+        proto  => 'all',
+        state  => ['RELATED', 'ESTABLISHED'],
+        action => 'accept',
+    }
+
+    firewallchain { 'INPUT:filter:IPv4':
+        purge  => true,
+        policy => drop,
+    }
+
     user { 'Junos Backup User':
         ensure     => present,
         name       => 'junos',
@@ -33,6 +73,6 @@ class dc_profile::net::network_device_backup {
     ssh_authorized_key { 'junos@datacentred.co.uk':
         user => 'junos',
         type => 'ssh-rsa',
-        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDZIV98YFCbCTwB5VKahUsT0pmWuhp/posTRWaX98DNXRgdeLb0LoaINmZK1EtVWZFj9I7TRif3KOiZ2yvLmCiHaGYT5bQTfbaUbZ0fHEyNWymLHJ2tyjVyS9iTFqUBNieOs0WPmI+IIdEYpGIgUOF6xTfzh7fHGgSjfol5uwfWpG1VoWK11MtWQeYkQj4dSpnGJkeFrRZlIWrWZtOKxusr87Khxi7Vs8O4C+lOuFhjG8cTwpQ4OlWtVTPKnQ3zysrFYJhidqWskNrl7jnFr0LByhS2pI3Q6pVzLgVWHqqlVP+fJf+PTkR62jYnrwSeKMBZR1CaVodtokKk82ir8+Ot'
+        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0aA1ffZhxKHL90e+fuk+3iOM+kdxpOwGVM6g7giyDHJAc82vGOPyL97trw4izIg6a6neDIFtJBJmlWi42qZEImj5pO0s9/cr34HBoux8ZaIj2rAFZpyW/FqayshDePsLa75OSX92JJr4aTMCyjpRn8hIm0jGlRyj8yQqrTnVfj3I88MYfMmjZ7HHpQqWIu4omBpQ9jCnD+1Yl/xbcvtFMfXiuPJAKNqYDMikOhDbDGaAGaqa3ymbRNfen7yfSz8OgmKl6hRX80GL71Q5oGDWGTa2cRrXa9pyjknvB8LOh8JWr1KH9+fSQ2O4uop+r5GLjAYEwJKd30m85oF6p9MUd',
     }
 }
