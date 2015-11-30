7def6e1714fa7ad4a5b5ef8744fc5e82343fef4f
millingworth <matt.illingworth@gmail.com>
2015-11-30 15:50:07 +0000

    [net_device_backup] Changed dependency and order of iptable rules
    Without the strict ordering the default table policy change was cutting off the puppet run before it's completion.

--- modules/dc_profile/manifests/net/network_device_backup.pp
+++ modules/dc_profile/manifests/net/network_device_backup.pp
@@ -14,39 +14,35 @@ class dc_profile::net::network_device_backup {
 
     include ::firewall
     
-    firewall { '001 accept all to lo interface':
+    firewall { '000 accept all to lo interface':
         proto   => 'all',
         iniface => 'lo',
         action  => 'accept',
-    }
-
-    firewall { '002 Allow inbound SSH':
+    } ->
+    firewall { '005 Allow inbound SSH':
         source => '185.98.148.0/22',
         dport  => 22,
         proto  => tcp,
         action => accept,
-    }
-
-    firewall { '003 Allow inbound SSH':
+    } ->
+    firewall { '010 Allow inbound SSH':
         source => '85.199.236.48/29',
         dport  => 22,
         proto  => tcp,
         action => accept,
-    }
-
-    firewall { '004 Allow inbound SSH':
+    } ->
+    firewall { '015 Allow inbound SSH':
         source => '185.43.216.0/22',
         dport  => 22,
         proto  => tcp,
         action => accept,
     }
 
-    firewall { '005 accept related established rules':
+    firewall { '020 accept related established rules':
         proto  => 'all',
         state  => ['RELATED', 'ESTABLISHED'],
         action => 'accept',
-    }
-
+    } ->
     firewallchain { 'INPUT:filter:IPv4':
         purge  => true,
         policy => drop,
@@ -57,7 +53,7 @@ class dc_profile::net::network_device_backup {
         name       => 'junos',
         home       => '/home/junos',
         managehome => true,
-        password   => '$6$OmomDUedirt2Sz$J95Pp0pjXRhxLjLeT6Hj3du1jS4OYNrJIzMX98cQijyXrIrvDHbvXJ7Gi/VeuSyTpU5NrZyNFQD.u5id34F1s.',
+        password   => '$6$63391E4yISne2q$en8R8nWymvepuggBPjT4e8DFYufJ1LVNooEC5bpWCvjPkrhV.1qKeE708hnIM5/JD8P1rYK8vQi6ddU7.iIIs/',
         shell      => '/bin/bash',
         system     => true,
     }
@@ -71,8 +67,10 @@ class dc_profile::net::network_device_backup {
     }
 
     ssh_authorized_key { 'junos@datacentred.co.uk':
-        user => 'junos',
-        type => 'ssh-rsa',
-        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0aA1ffZhxKHL90e+fuk+3iOM+kdxpOwGVM6g7giyDHJAc82vGOPyL97trw4izIg6a6neDIFtJBJmlWi42qZEImj5pO0s9/cr34HBoux8ZaIj2rAFZpyW/FqayshDePsLa75OSX92JJr4aTMCyjpRn8hIm0jGlRyj8yQqrTnVfj3I88MYfMmjZ7HHpQqWIu4omBpQ9jCnD+1Yl/xbcvtFMfXiuPJAKNqYDMikOhDbDGaAGaqa3ymbRNfen7yfSz8OgmKl6hRX80GL71Q5oGDWGTa2cRrXa9pyjknvB8LOh8JWr1KH9+fSQ2O4uop+r5GLjAYEwJKd30m85oF6p9MUd',
+        user    => 'junos',
+        type    => 'ssh-rsa',
+        key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0aA1ffZhxKHL90e+fuk+3iOM+kdxpOwGVM6g7giyDHJAc82vGOPyL97trw4izIg6a6neDIFtJBJmlWi42qZEImj5pO0s9/cr34HBoux8ZaIj2rAFZpyW/FqayshDePsLa75OSX92JJr4aTMCyjpRn8hIm0jGlRyj8yQqrTnVfj3I88MYfMmjZ7HHpQqWIu4omBpQ9jCnD+1Yl/xbcvtFMfXiuPJAKNqYDMikOhDbDGaAGaqa3ymbRNfen7yfSz8OgmKl6hRX80GL71Q5oGDWGTa2cRrXa9pyjknvB8LOh8JWr1KH9+fSQ2O4uop+r5GLjAYEwJKd30m85oF6p9MUd',
+        require => User['Junos Backup User'],
     }
+
 }
