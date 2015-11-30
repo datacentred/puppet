32470982ea909761d8a07d82ccd210042857f17b
Nick Jones <nick.jones@datacentred.co.uk>
2015-11-30 13:02:20 +0000

    Add in a couple of file resources in order to work around packaging oversights

--- modules/dc_profile/manifests/openstack/neutron/server.pp
+++ modules/dc_profile/manifests/openstack/neutron/server.pp
@@ -18,7 +18,23 @@ class dc_profile::openstack::neutron::server {
   include ::neutron::quota
   include ::dc_icinga::hostgroup_neutron_server
 
+  # Workaround for upstream packaging bugs, such as:
+  # https://bugs.launchpad.net/ubuntu/+source/neutron-lbaas/+bug/1460228
   ensure_packages( ['python-neutron-vpnaas', 'python-neutron-lbaas'] )
+
+  file { '/etc/neutron/neutron_lbaas.conf':
+    mode    => '0644',
+    owner   => 'neutron',
+    content => "[service_providers]\nservice_provider=LOADBALANCER:Haproxy:neutron_lbaas.services.loadbalancer.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver:default",
+    require => Package['python-neutron-lbaas'],
+  }
+
+  file { '/etc/neutron/neutron_vpnaas.conf':
+    mode    => '0644',
+    owner   => 'neutron',
+    content => "[service_providers]\nservice_provider=VPN:openswan:neutron_vpnaas.services.vpn.service_drivers.ipsec.IPsecVPNDriver:default",
+    require => Package['python-neutron-vpnaas'],
+  }
 
   # Add this node's API services into our loadbalancer
   @@haproxy::balancermember { "${::fqdn}-neutron":
