a5647f16746af2f6c5a98e14ae8b39075e37378c
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2015-12-01 11:49:10 +0000

    Change config for discovery and templates

--- modules/dc_foreman_proxy/manifests/init.pp
+++ modules/dc_foreman_proxy/manifests/init.pp
@@ -10,16 +10,21 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class dc_foreman_proxy (
-  $use_tftp      = $dc_foreman_proxy::params::use_tftp,
-  $use_dns       = $dc_foreman_proxy::params::use_dns,
-  $use_dhcp      = $dc_foreman_proxy::params::use_dhcp,
-  $use_bmc       = $dc_foreman_proxy::params::use_bmc,
-  $use_puppetca  = $dc_foreman_proxy::params::use_puppetca,
-  $use_puppet    = $dc_foreman_proxy::params::use_puppet,
-  $dns_key       = $dc_foreman_proxy::params::dns_key,
-  $omapi_key     = $dc_foreman_proxy::params::omapi_key,
-  $omapi_secret  = $dc_foreman_proxy::params::omapi_secret,
-  $trusted_hosts = $dc_foreman_proxy::params::trusted_hosts,
+  $use_tftp         = $dc_foreman_proxy::params::use_tftp,
+  $use_dns          = $dc_foreman_proxy::params::use_dns,
+  $use_dhcp         = $dc_foreman_proxy::params::use_dhcp,
+  $use_bmc          = $dc_foreman_proxy::params::use_bmc,
+  $use_puppetca     = $dc_foreman_proxy::params::use_puppetca,
+  $use_puppet       = $dc_foreman_proxy::params::use_puppet,
+  $use_templates    = $dc_foreman_proxy::params::use_templates,
+  $use_discovery    = $dc_foreman_proxy::params::use_discovery,
+  $dns_key          = $dc_foreman_proxy::params::dns_key,
+  $omapi_key        = $dc_foreman_proxy::params::omapi_key,
+  $omapi_secret     = $dc_foreman_proxy::params::omapi_secret,
+  $trusted_hosts    = $dc_foreman_proxy::params::trusted_hosts,
+  $proxy_https_port = $dc_foreman_proxy::params::proxy_https_port,
+  $proxy_http_port  = $dc_foreman_proxy::params::proxy_http_port,
+  $foreman_url      = $dc_foreman_proxy::params::foreman_url,
 ) inherits dc_foreman_proxy::params {
 
   contain dc_foreman_proxy::install
