a13acb5beeb0beb36604385954abedcd60fe6a1f
Simon Murray <simon.murray@datacentred.co.uk>
2016-01-14 12:47:18 +0000

    [Icinga2] Add Sensors Check
    Checks any voltage, current or temperature sensors against thresholds
    set by the hardware vendor

--- modules/dc_icinga2/manifests/services.pp
+++ modules/dc_icinga2/manifests/services.pp
@@ -16,6 +16,7 @@ class dc_icinga2::services {
   include ::dc_icinga2::services::psu
   include ::dc_icinga2::services::puppetdb
   include ::dc_icinga2::services::puppetserver
+  include ::dc_icinga2::services::sensors
   include ::dc_icinga2::services::smart
   include ::dc_icinga2::services::ssh
   include ::dc_icinga2::services::users
