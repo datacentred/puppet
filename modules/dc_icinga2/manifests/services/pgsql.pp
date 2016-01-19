d5dcc30576d606d3a777ea0fd97486f5bc3f64b3
Simon Murray <simon.murray@datacentred.co.uk>
2016-01-19 16:09:59 +0000

    [Icinga2] Fix Postgres Manifest
    Alter the include parameter (wrong) to be import (right)

--- modules/dc_icinga2/manifests/services/pgsql.pp
+++ modules/dc_icinga2/manifests/services/pgsql.pp
@@ -9,7 +9,7 @@ class dc_icinga2::services::pgsql (
 ) {
 
   icinga2::object::apply_service { 'pgsql':
-    include       => 'generic-service',
+    import        => 'generic-service',
     check_command => 'pgsql',
     vars          => {
       'pgsql_database' => $database,
