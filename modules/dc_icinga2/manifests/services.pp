700ace113673df5ccd99af0b159407668e19ccda
Simon Murray <simon.murray@datacentred.co.uk>
2016-01-26 11:18:53 +0000

    [Icinga2] Enable SAS Phy Checks
    Include the SAS class so that checks are propagated through the
    cluster.  Checks manually installed on the OS compute nodes which
    have puppet disabled

--- modules/dc_icinga2/manifests/services.pp
+++ modules/dc_icinga2/manifests/services.pp
@@ -21,6 +21,7 @@ class dc_icinga2::services {
   include ::dc_icinga2::services::psu
   include ::dc_icinga2::services::puppetdb
   include ::dc_icinga2::services::puppetserver
+  include ::dc_icinga2::services::sas
   include ::dc_icinga2::services::sensors
   include ::dc_icinga2::services::smart
   include ::dc_icinga2::services::ssh
