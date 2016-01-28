f5584340f31165c984e1662bd330c27b488f8047
Tristan Watson <tristan.watson@protonmail.ch>
2016-01-28 15:37:20 +0000

    Adding the kernal dump module everywhere, which will hopefully give us back some information as to why machines crash in the future if they are stopped dead by some error such as is happening now.
    1. Installs linux-crashdump module
    2. Changes line in /etc/default/kdump-tools to enable kdump
    3. Changes boot daemon parameters, so that kdump is built with enough RAM to initate a dump

---
+++ modules/dc_profile/manifests/util/kdump.pp
@@ -0,0 +1,33 @@
+#Class: dc_profile::util::kdump
+#
+# Enable linux-crashdump (k-dump) on a node.
+# Ensure that there is enough RAM allocated to kdump as 128MB is not enough
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+# Grub2.0 Installed (dc_grub)
+# Stdin
+#
+# Sample Usage:
+#
+class dc_profile::util::kdump {
+
+  ensure_packages('linux-crashdump')
+
+  file_line { 'kdump':
+    path    => '/etc/default/kdump-tools',
+    line    => 'USE_KDUMP=1',
+    match   => 'USE_KDUMP=0',
+    require => Package['linux-crashdump'],
+  }
+
+  file_line { 'grub update':
+    path    => '/etc/default/grub.d/kexec-tools.cfg',
+    line    => 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT crashkernel=384M-:256M"',
+    match   => '^GRUB',
+    require => Package['linux-crashdump'],
+  }
+}
