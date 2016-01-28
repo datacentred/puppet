c3e1922bb1d137d978df01e04f96866e68b5a6b8
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2016-01-28 16:32:42 +0000

    [PD1943] Blacklist infiniband modules on compute nodes

---
+++ modules/dc_profile/manifests/util/blacklist_modules.pp
@@ -0,0 +1,19 @@
+# Class: dc_profile::util::blacklist_modules
+#
+# Manage kernel modules
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::util::blacklist_modules (
+  $modules = [],
+){
+
+  ensure_resource( 'kmod::blacklist', $modules, { ensure => present } )
+
+}
