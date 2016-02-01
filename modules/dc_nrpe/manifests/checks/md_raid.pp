c62a2aedd1d0d4d8aa68ba16ad950a56996908f2
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2016-02-01 15:43:23 +0000

    Install the check

--- modules/dc_nrpe/manifests/checks/md_raid.pp
+++ modules/dc_nrpe/manifests/checks/md_raid.pp
@@ -3,7 +3,8 @@
 class dc_nrpe::checks::md_raid {
 
   dc_nrpe::check { 'check_md_raid':
-    path => '/usr/lib/nagios/plugins/check_md_raid.sh',
+    source => 'puppet:///modules/dc_nrpe/check_md_raid.sh',
+    path   => '/usr/local/bin/check_md_raid.sh',
   }
 
 }
