fff85683ca390367c414eaf460945ca17c3e7d80
matt.illingworth <matt.illingworth@datacentred.co.uk>
2016-02-18 17:25:45 +0000

    [nfsen] Add hiera roles/data and submodules
    - Adds in the submodules nfdump / nfsen
    - Adds new hiera role for netflow monitoring
    - Protects the apache endpoints with client certs.

---
+++ modules/dc_profile/manifests/net/nfsen.pp
@@ -0,0 +1,55 @@
+# Class: dc_profile::net::nfsen
+#
+# Deploys the nfsen web interface to a server for netflow interrogation. 
+# This will install apache2 with default vhosts protect with client certs. 
+# It pulls in the nfdump module with the use_with_nfsen parameter. 
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+
+# Sample Usage:
+class dc_profile::net::nfsen {
+
+  include ::nfsen
+  include ::apache
+  include ::apache::mod::php
+  include ::apache::mod::ssl
+    
+  apache::vhost { "${::fqdn} non-ssl":
+    servername      => $::fqdn,
+    port            => 80,
+    docroot         => '/var/www/html/',
+    redirect_status => 'permanent',
+    redirect_dest   => "https://${::fqdn}",
+    before          => Class['::nfsen'],
+  }
+
+  apache::vhost { "${::fqdn} ssl":
+    servername        => $::fqdn,
+    port              => 443,
+    docroot           => '/var/www/html/',
+    ssl               => true,
+    ssl_cert          => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
+    ssl_key           => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
+    ssl_ca            => '/var/lib/puppet/ssl/certs/ca.pem',
+    ssl_crl           => '/var/lib/puppet/ssl/crl.pem',
+    ssl_verify_client => 'require',
+    ssl_verify_depth  => 1,
+    before            => Class['::nfsen'],
+  }
+
+  $str = '<!DOCTYPE html><html><head><title>Holding page</title>
+    </head><body>You are probably looking for
+    <a href="/nfsen/"> nfsen</a></body></html>'
+
+  file { 'default index.html':
+    path    => '/var/www/html/index.html',
+    content => $str,
+  }
+
+  Class['::nfdump'] -> Class['::nfsen']
+
+}
