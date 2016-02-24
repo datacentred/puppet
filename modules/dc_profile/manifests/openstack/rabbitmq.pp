1be5da73dc9bec6bd276457043c8be4a9a7151af
Nick Jones <nick.jones@datacentred.co.uk>
2016-02-24 15:16:41 +0000

    Refactor RabbitMQ profile slightly, introduce net.core sysctls
    This change:
    * Lightly refactors the RabbitMQ profile class;
    * Ensures that the configuration is in place to make each node a
    'disc' node (it already is, this was done during the upgrade);
    *  Makes sure that the `net.core.r{w}mem_max` sysctl tunables are
    persistent.

--- modules/dc_profile/manifests/openstack/rabbitmq.pp
+++ modules/dc_profile/manifests/openstack/rabbitmq.pp
@@ -12,55 +12,38 @@
 #
 class dc_profile::openstack::rabbitmq {
 
+  include ::rabbitmq
+
   $osdbmq_rabbitmq_user = hiera(osdbmq_rabbitmq_user)
   $osdbmq_rabbitmq_pw = hiera(osdbmq_rabbitmq_pw)
 
-  $rabbitmq_monuser = hiera(rabbitmq_monuser)
-  $rabbitmq_monuser_password = hiera(rabbitmq_monuser_password)
-
-  $management_ip = $::ipaddress
-
-  # Return just the hostname from the FQDN stored in Hiera
-  $cluster_nodes = regsubst(hiera(osdbmq_members), '\.(.*)', '', 'G')
-
-  # We need one of our nodes to be a 'disc' cluster node,
-  # the rest should be 'ram'
-  $disc_cluster_node = $cluster_nodes[0]
-
-  if $::hostname == $disc_cluster_node {
-    $cluster_node_type = 'disc'
-  } else {
-    $cluster_node_type = 'ram'
-  }
-
-  # rabbitmq application account needs puppet group
-  # membership in order to use the latter's SSL keys
+  # The 'rabbitmq' application account needs puppet group membership in order
+  # to use the latter's SSL keys
   user { 'rabbitmq':
     ensure     => present,
     groups     => 'puppet',
     home       => '/srv/rabbitmq',
     managehome => true,
     system     => true,
-  } ->
-  file { '/var/lib/rabbitmq':
-    ensure => link,
-    target => '/srv/rabbitmq',
-  } ->
-  file { '/srv/rabbitmq/mnesia':
-    ensure => directory,
-    owner  => 'rabbitmq',
-    group  => 'rabbitmq',
-  } ->
-  class { '::rabbitmq':
-    cluster_nodes     => $cluster_nodes,
-    cluster_node_type => $cluster_node_type,
   }
-  contain ::rabbitmq
+
+  file { ['/srv/rabbitmq', '/srv/rabbitmq/mnesia']:
+    ensure  => directory,
+    owner   => 'rabbitmq',
+    group   => 'rabbitmq',
+    require => User['rabbitmq'],
+  }
+
+  file { '/var/lib/rabbitmq':
+    ensure  => link,
+    target  => '/srv/rabbitmq',
+    require => File['/srv/rabbitmq'],
+  }
 
   exec { 'configure-ha-queue-policy':
     command => '/usr/sbin/rabbitmqctl set_policy HA \'^(?!amq\\.).*\' \'{"ha-mode": "all"}\'',
-    require => Class['::rabbitmq'],
     unless  => '/usr/sbin/rabbitmqctl list_policies | grep -q HA',
+    require => Class['::rabbitmq'],
   }
 
   rabbitmq_user { $osdbmq_rabbitmq_user:
@@ -73,15 +56,14 @@ class dc_profile::openstack::rabbitmq {
     configure_permission => '.*',
     read_permission      => '.*',
     write_permission     => '.*',
+    require              => Rabbitmq_User[$osdbmq_rabbitmq_user],
   }
 
-  # Icinga checks
   unless $::is_vagrant {
     include ::dc_icinga::hostgroup_rabbitmq
-    # Required for Icinga monitoring
-    rabbitmq_user { $rabbitmq_monuser:
+    rabbitmq_user { 'monitor':
       admin    => true,
-      password => $rabbitmq_monuser_password,
+      password => hiera(rabbitmq_monuser_password),
       require  => Class['::rabbitmq'],
     }
   }
