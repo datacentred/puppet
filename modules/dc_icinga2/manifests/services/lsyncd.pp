68c57b6fc41c12530d2675db199881eaa41e0779
Simon Murray <simon.murray@datacentred.co.uk>
2016-03-14 14:00:27 +0000

    [Icinga2] Enable Lsyncd Checks
    Apllies a simple check on the puppet CA to ensure lsyncd is running and
    synchronising configuration to the puppet masters (probably...)

---
+++ modules/dc_icinga2/manifests/services/lsyncd.pp
@@ -0,0 +1,20 @@
+# == Class: dc_icinga2::services::lsyncd
+#
+# Checks an lsyncd process is running
+#
+class dc_icinga2::services::lsyncd {
+
+  icinga2::object::apply_service { 'lsyncd':
+    import        => 'generic-service',
+    check_command => 'procs',
+    vars          => {
+      'procs_critical' => '1:',
+      'procs_argument' => 'lsyncd',
+    },
+    zone          => 'host.name',
+    assign_where  => 'host.vars.role == "puppet_ca"',
+    target        => '/etc/icinga2/zones.d/global-templates/services.conf',
+  }
+
+
+}
