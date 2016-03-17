898fca19e791b04563ebebdae603a9275940a47b
Simon Murray <simon.murray@datacentred.co.uk>
2016-03-17 14:37:53 +0000

    [Icinga2] S3/OS HAProxy Checks
    Updates the check script to accept a port if necessary and also perform some
    basic error checking on the HTTP status code.  Icinga2 module updated to accept
    a display name for 'apply Service' objects, so that regardless of the configuration
    haproxy reports as haproxy.  Added in a new service check for port 1936 applied
    locally to all S3 and OS public API endpoints.

--- modules/dc_icinga2/manifests/services/haproxy.pp
+++ modules/dc_icinga2/manifests/services/haproxy.pp
@@ -4,9 +4,14 @@
 #
 class dc_icinga2::services::haproxy {
 
-  icinga2::object::apply_service { 'core services':
+  Icinga2::Object::Apply_service {
+    target => '/etc/icinga2/zones.d/global-templates/services.conf',
+  }
+
+  icinga2::object::apply_service { 'haproxy port 443':
     import        => 'generic-service',
     check_command => 'haproxy',
+    display_name  => 'haproxy',
     vars          => {
       'haproxy_host'       => 'stats.datacentred.services',
       'haproxy_privatekey' => '/var/lib/puppet/ssl/private_keys/$host.name$.pem',
@@ -15,7 +20,21 @@ class dc_icinga2::services::haproxy {
     },
     zone          => 'host.name',
     assign_where  => 'host.vars.role == "monitoring_master"',
-    target        => '/etc/icinga2/zones.d/global-templates/services.conf',
+  }
+
+  icinga2::object::apply_service { 'haproxy port 1936':
+    import        => 'generic-service',
+    check_command => 'haproxy',
+    display_name  => 'haproxy',
+    vars          => {
+      'haproxy_host'       => 'host.name',
+      'haproxy_port'       => 1936,
+      'haproxy_privatekey' => '/var/lib/puppet/ssl/private_keys/$host.name$.pem',
+      'haproxy_clientcert' => '/var/lib/puppet/ssl/certs/$host.name$.pem',
+      'haproxy_perfdata'   => 'scur',
+    },
+    zone          => 'host.name',
+    assign_where  => 'host.vars.role in ["ceph_radosgatewaylb", "openstack_proxies"]',
   }
 
 }
