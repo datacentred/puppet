71555ff2afef00e786722de501a2232d80ba5c77
Nick Jones <nick.jones@datacentred.co.uk>
2016-03-17 17:02:22 +0000

    Remove stunnel related cleanup code

--- modules/dc_profile/manifests/perf/telegraf.pp
+++ modules/dc_profile/manifests/perf/telegraf.pp
@@ -5,14 +5,6 @@ class dc_profile::perf::telegraf {
 
   include ::telegraf
 
-  # TODO: Remove once this stuff has been nuked
-  service { ['stunnel-graphite', 'stunnel-influxdb']:
-    ensure => stopped,
-  } ->
-  package { 'stunnel4':
-    ensure => purged,
-  }
-
   user { 'telegraf':
     ensure  => present,
     groups  => 'puppet',
