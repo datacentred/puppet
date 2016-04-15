357fb59b556061995ec702fbf78ba070f4fb02c4
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2016-04-15 11:49:51 +0100

    Add new collectd proxy class
    Add new proxy class, move defaults out into their own class, remove
    un-needed profile class

---
+++ modules/dc_collectd/manifests/agent/proxy.pp
@@ -0,0 +1,36 @@
+# Class: dc_collectd::agent::proxy
+#
+# Installs network plugin as a proxy
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#  $listeners and $servers need to be hashes
+#
+class dc_collectd::agent::proxy (
+  $user,
+  $password,
+  $servers,
+  $listeners,
+){
+
+  class { '::collectd::plugin::network':
+    require   => File['/etc/collectd_auth'],
+    forward   => true,
+    servers   => $servers,
+    listeners => $listeners,
+  }
+
+  file { '/etc/collectd_auth':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => "${user}: ${password}",
+  }
+
+}
