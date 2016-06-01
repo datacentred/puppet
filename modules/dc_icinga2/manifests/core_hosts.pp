305b6121bd00511625cb1fa1b0d3d46421cc58bd
Simon <spjmurray@yahoo.co.uk>
2016-06-01 15:28:21 +0100

    [Icinga2] Cleanup Core Virtual Hosts (#2749)
    Rename 'global' hosts to be core specific, and only created on the master in the
    core domain.  The actual definitions are created with the virtual_host defined
    type to keep in sync with how PDUs are defined.

---
+++ modules/dc_icinga2/manifests/core_hosts.pp
@@ -0,0 +1,40 @@
+# == Class: dc_icinga2::core_hosts
+#
+# Defines virtual hosts for S3/OpenStack API checks
+#
+class dc_icinga2::core_hosts {
+
+  dc_icinga2::virtual_host { 'compute.datacentred.io':
+    address    => '185.43.218.26',
+    vars       => {
+      'role'                             => 'openstack-endpoint',
+      'certificates["*.datacentred.io"]' => {
+        'port' => 443,
+      },
+    },
+    icon_image => 'http://incubator.storage.datacentred.io/openstack-logo-16x16.png',
+  }
+
+  icinga2::virtual_host { 'storage.datacentred.io':
+    address    => '185.43.218.55',
+    vars       => {
+      'role'                                     => 'rados-endpoint',
+      'certificates["*.storage.datacentred.io"]' => {
+        'port' => 443,
+      },
+    },
+    icon_image => 'http://incubator.storage.datacentred.io/ceph-logo-16x16.png',
+  }
+
+  icinga2::virtual_host { 'jenkins.datacentred.services':
+    address    => '185.43.217.42',
+    vars       => {
+      'role'                                   => 'jenkins-endpoint',
+      'certificates["*.datacentred.services"]' => {
+        'port' => 8080,
+      },
+    },
+    icon_image => 'http://incubator.storage.datacentred.io/jenkins-logo-16x16.png',
+  }
+
+}
