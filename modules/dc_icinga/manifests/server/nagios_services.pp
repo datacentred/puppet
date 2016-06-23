bfffcb36eeb510ac0fe6df5bb73ae75e14a77c28
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2016-06-23 17:08:56 +0100

    Slow down checking for cluster health to reduce alerting (#2774)

--- modules/dc_icinga/manifests/server/nagios_services.pp
+++ modules/dc_icinga/manifests/server/nagios_services.pp
@@ -509,7 +509,7 @@ class dc_icinga::server::nagios_services {
   }
 
   icinga::service { 'check_elasticsearch_cluster_health':
-    use                 => 'dc_service_generic',
+    use                 => 'dc_service_slowcheck',
     hostgroup_name      => 'dc_hostgroup_elasticsearch',
     check_command       => 'check_es_cluster_health',
     service_description => 'Elasticsearch cluster health',
