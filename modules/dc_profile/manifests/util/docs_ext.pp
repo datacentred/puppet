b40bc1eb8028f3b3c6a83d8bb1d6d2e59214d5e4
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2016-07-05 12:43:33 +0100

    The first copy-paste version of manifest for docs.

---
+++ modules/dc_profile/manifests/util/docs_ext.pp
@@ -0,0 +1,49 @@
+# Class: dc_profile::util::docs_ext
+#
+# Manage Confluence installation for external docs host
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+class dc_profile::util::docs_ext {
+
+  include ::java
+  include ::confluence
+  include ::nginx
+  include ::postgresql::server
+  include ::dc_backup
+
+  $database_password = hiera('confluence_database_password')
+
+  nginx::resource::upstream { 'confluence':
+    ensure  => present,
+    members => [ 'localhost:8080' ],
+  }
+
+  nginx::resource::vhost { $::fqdn:
+    proxy       => 'http://confluence',
+    server_name => [ $::fqdn, 'docs.datacentred.io', 'http://confluence' ],
+  }
+
+  postgresql::server::role { 'confluenceadmin':
+    login    => true,
+    createdb => true,
+  }
+
+  postgresql::server::db { 'confluencedb':
+    owner    => 'confluenceuser',
+    user     => 'confluenceuser',
+    password => $database_password,
+  }
+
+  dc_backup::dc_duplicity_job { "${::fqdn}_confluencexml" :
+    cloud          => 's3',
+    backup_content => 'homedir',
+    source_dir     => '/home/confluence/backups/',
+  }
+
+}
