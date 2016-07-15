ba2c8facf120d519ad61cdda09a8922342de3cbe
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2016-07-15 11:22:11 +0100

    Actually install the check

--- modules/dc_nrpe/manifests/checks/conntrack.pp
+++ modules/dc_nrpe/manifests/checks/conntrack.pp
@@ -3,8 +3,9 @@
 class dc_nrpe::checks::conntrack {
 
   dc_nrpe::check { 'check_conntrack':
-    path => '/usr/lib/nagios/plugins/check_conntrack',
-    args => '80 90',
+    path   => '/usr/lib/nagios/plugins/check_conntrack',
+    source => 'puppet:///modules/dc_nrpe/check_conntrack',
+    args   => '80 90',
   }
 
 }
