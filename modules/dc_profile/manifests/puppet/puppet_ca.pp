524fd20e6036a7bf9454844a1a708fbc9b2d6b39
Matt Jarvis <matt.jarvis@datacentred.co.uk>
2016-07-21 15:40:24 +0100

    Enable new module on puppet ca (#2855)

--- modules/dc_profile/manifests/puppet/puppet_ca.pp
+++ modules/dc_profile/manifests/puppet/puppet_ca.pp
@@ -4,7 +4,7 @@
 #
 class dc_profile::puppet::puppet_ca {
 
-  include ::dc_foreman_proxy
+  include ::foreman_proxy
   include ::dc_icinga::hostgroup_foreman_proxy
 
   # On trusty the number of inotify nodes is terribly low
@@ -16,6 +16,6 @@ class dc_profile::puppet::puppet_ca {
   # NOTE: Implies a CA is a master
   # NOTE: Foreman interferes with CA generation if a master is
   #       not installed first, so knee cap it
-  Class['::puppet::master'] -> Class['::dc_foreman_proxy']
+  Class['::puppet::master'] -> Class['::foreman_proxy']
 
 }
