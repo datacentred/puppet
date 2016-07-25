8b705da24bd31a4598d37934379024183d2fada5
Nick Jones <nick.jones@datacentred.co.uk>
2016-07-25 15:51:16 +0100

    [Docker] Configure Docker Registry
    This commit introduces basic configuration for Docker Registry, which will use
    the Swift API in order to store the actual images itself.  For now it'll be
    deployed on `graph` which currently hosts Grafana and nothing else (and so has
    the `telemetry` role).
    
    A new project, `devops`, and a new storage container, `registry`, has been
    created for this use.
    
    There'll be a subsequent commit to introduce the necessary haproxy
    configuration which will enable SSL cert-based auth for Docker clients.
    
    Part of PD-1962.

---
+++ modules/dc_docker/manifests/init.pp
@@ -0,0 +1,2 @@
+# Class: ::dc_docker
+#
---
+++ modules/dc_profile/manifests/docker/registry.pp
@@ -0,0 +1,9 @@
+# == Class: ::dc_profile::docker::registry
+#
+# Deploys a Docker Registry
+#
+class dc_profile::docker::registry {
+
+  include ::dc_docker::registry
+
+}
