9695e71b851d19a59f96ddd1feb5dd51d9c8703a
Joel Merrick <joelio@users.noreply.github.com>
2016-07-27 15:10:08 +0100

    PD-2461 - Kibana Upgrade (#2882)
    * Updates the submodule kibana4 to the latest upstream
    * Removes the tarball installation in favour of proper debian packages
    * Adds a plethora of wonderful plugins for eye-candy
    * Sets Kibana version to 4.5

--- modules/dc_kibana/manifests/install.pp
+++ modules/dc_kibana/manifests/install.pp
@@ -7,14 +7,5 @@ class dc_kibana::install {
         recurse => true,
   }
 
-  exec { 'install_kibana':
-    command => 'wget http://download.elasticsearch.org/kibana/kibana/kibana-latest.tar.gz \
-                && tar xzf kibana-latest.tar.gz -C /var/www/ \
-                && mv kibana-latest kibana',
-    cwd     => '/var/www',
-    path    => ['/bin', '/usr/bin'],
-    creates => '/var/www/kibana-latest.tar.gz',
-    require => File['/var/www'],
-  }
 
 }
--- modules/dc_profile/manifests/log/kibana.pp
+++ modules/dc_profile/manifests/log/kibana.pp
@@ -14,6 +14,34 @@ class dc_profile::log::kibana {
   include apache
   include kibana4
 
+  class { 'kibana4':
+    plugins => {
+      'elastic/sense'                    => {
+        'ensure'          => 'present',
+        'plugin_dest_dir' => 'sense',
+      },
+      'elastic/timelion'                 => {
+        'ensure'          => 'present',
+        'plugin_dest_dir' => 'timelion',
+      },
+      'elasticsearch/marvel'             => {
+        'ensure'          => 'present',
+        'plugin_dest_dir' => 'marvel',
+      },
+      'oxalide/kibana_metric_vis_colors' => {
+        'plugin_dest_dir' => 'marvel',
+        'ensure'          => 'present',
+        'url'             => 'https://github.com/Oxalide/kibana_metric_vis_colors/archive/master.zip'
+      },
+      'stormpython/vectormap'            => {
+        'plugin_dest_dir' => 'vectormap',
+        'ensure'          => 'present',
+        'url'             => 'https://github.com/stormpython/vectormap/archive/master.zip'
+      }
+    }
+  }
+
+
   #Apache vhost config, proxies http requests to kibana server instance
   apache::vhost{ 'kibana':
     servername => "kibana.${::domain}",
@@ -29,9 +57,9 @@ class dc_profile::log::kibana {
     ],
     rewrites   => [
       {
-        comment       => 'Kibana rewrite',
-        rewrite_cond  => ['%{DOCUMENT_ROOT}/%{REQUEST_FILENAME} !-f'],
-        rewrite_rule  => ['.* http://127.0.0.1:5601%{REQUEST_URI} [P,QSA]']
+        comment      => 'Kibana rewrite',
+        rewrite_cond => ['%{DOCUMENT_ROOT}/%{REQUEST_FILENAME} !-f'],
+        rewrite_rule => ['.* http://127.0.0.1:5601%{REQUEST_URI} [P,QSA]']
       }
     ]
   }
