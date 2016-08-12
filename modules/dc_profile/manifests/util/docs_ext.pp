2a5d3c46e5ed56d313bbda69552eb7cf31ebd0ec
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2016-08-12 16:36:33 +0100

    The first attempt to add nginx proxy.

--- modules/dc_profile/manifests/util/docs_ext.pp
+++ modules/dc_profile/manifests/util/docs_ext.pp
@@ -22,11 +22,19 @@ class dc_profile::util::docs_ext {
   nginx::resource::upstream { 'confluence':
     ensure  => present,
     members => [ 'localhost:8080' ],
+    #members => [ 'localhost:8443' ],
   }
 
   nginx::resource::vhost { $::fqdn:
+    ssl => true,
+    ssl_port => 443,
+    listen_port => 443,
     proxy       => 'http://confluence',
+    #proxy       => 'https://confluence',
     server_name => [ $::fqdn, 'docs.datacentred.io', 'http://confluence' ],
+    #server_name => [ $::fqdn, 'docs.datacentred.io', 'https://confluence' ],
+    ssl_cert             => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
+    ssl_key              => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
   }
 
   postgresql::server::role { 'confluenceuser':
