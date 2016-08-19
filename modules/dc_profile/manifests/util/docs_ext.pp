d91173f288d4d67d0f1e0a28f1924ff5248a14e9
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2016-08-19 11:13:36 +0100

    Move nginx vhost definition to hiera.

--- modules/dc_profile/manifests/util/docs_ext.pp
+++ modules/dc_profile/manifests/util/docs_ext.pp
@@ -19,16 +19,6 @@ class dc_profile::util::docs_ext {
 
   $database_password = hiera('confluence_database_password')
 
-  nginx::resource::vhost { $::fqdn:
-    ssl => true,
-    ssl_port => 443,
-    listen_port => 443,
-    proxy       => 'http://localhost:8080',
-    server_name => [ $::fqdn, 'docs.datacentred.io' ],
-    ssl_cert             => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
-    ssl_key              => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
-  }
-
   postgresql::server::role { 'confluenceuser':
     login    => true,
     createdb => true,
