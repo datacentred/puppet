2b41a538961a7d3483c692eff41de17ae724f4ae
Simon Murray <simon.murray@datacentred.co.uk>
2016-08-25 13:22:09 +0100

    [Foreman] Reinstate Deleted File
    One that fell through the cracks... Profile referenced by the name servers got
    accidentally deleted during the spring clean.

---
+++ modules/dc_profile/manifests/net/foreman_checks.pp
@@ -0,0 +1,17 @@
+# Class: dc_profile::net::foreman_checks
+#
+# Foreman check for nodes running DHCP
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::net::foreman_checks {
+
+  include dc_foreman::service_checks
+
+}
