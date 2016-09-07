e904c845980fa6bc224a230ee34f18333530bda4
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2016-09-07 13:28:44 +0100

    Puppetised version of the docs host (test env): (#2942)
    This is adding Puppet manifest for provisioning a test
    instance of the docs host.
    The heart of the setup is Confluence with postgres as
    a database backend and an nginx SSL-capable reverse proxy.
    
    There are some manual steps involved in the process, and
    all are outlined on the below Confluence page:
    https://datacentred.atlassian.net/wiki/pages/viewpage.action?pageId=67305476

---
+++ modules/dc_profile/manifests/util/docs_ext.pp
@@ -0,0 +1,23 @@
+# Class: dc_profile::util::docs_ext
+#
+# Manage Confluence installation for external docs host
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+class dc_profile::util::docs_ext {
+
+  include ::java
+  include ::confluence
+  include ::nginx
+  include ::postgresql::server
+  include ::dc_backup
+
+  create_resources('postgresql::server::db', hiera_hash('postgresql::server_dbs'))
+  create_resources('dc_backup::dc_duplicity_job', hiera_hash('dc_backup::confluence_db_backup'))
+
+}
