1b4f17583da7eb41c624b34e426ea3f5027edc2b
Joel Merrick <joel.merrick@datacentred.co.uk>
2016-09-16 13:00:49 +0100

    Use postgresql instead of mysql and add some chaining

--- modules/dc_profile/manifests/net/zabbix_proxy.pp
+++ modules/dc_profile/manifests/net/zabbix_proxy.pp
@@ -1,9 +1,11 @@
 class dc_profile::net::zabbix_proxy {
 
-    include ::mysql::server
+    include ::postgresql::server
 
     class { 'zabbix::proxy':
       zabbix_server_host => $_zabbix_url,
       database_type      => $_database_type,
     }
+
+    Class['postgresql::server'] -> Class['zabbix::proxy']
 }
--- modules/dc_profile/manifests/net/zabbix_server.pp
+++ modules/dc_profile/manifests/net/zabbix_server.pp
@@ -2,10 +2,12 @@ class dc_profile::net::zabbix_server {
 
     include ::apache 
     include ::apache::mod::php
-    include ::mysql::server
+    include ::postgresql::server
 
     class { 'zabbix':
       zabbix_url    => $_zabbix_url,
       database_type => $_database_type,
     }
+
+    Class['apache'] -> Class['postgresql::server'] -> Class['zabbix']
 }
