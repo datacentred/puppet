af83e0650a7b27e10a8862bedb04548fd4507dca
Joel Merrick <joelio@users.noreply.github.com>
2016-09-22 13:32:52 +0100

    Use augeaus privers (#2957)

--- modules/dc_grub/manifests/init.pp
+++ modules/dc_grub/manifests/init.pp
@@ -12,23 +12,14 @@
 # [Remember: No empty lines between comments and class definition]
 class dc_grub {
 
-  file { '/etc/default/grub':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/dc_grub/grub',
+  kernel_parameter { 'noplymouth':
+    ensure => present,
   }
 
   # Since we don't use plymouth we don't need this
   # if it exists it will complain during boot
   file { '/etc/init/plymouth-upstart-bridge.conf':
     ensure => absent,
-  }
-
-  exec { '/usr/sbin/update-grub':
-    refreshonly => true,
-    subscribe   => File['/etc/default/grub'],
   }
 
 }
