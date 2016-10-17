788816da26cdbcfb9db9359148e46bbf6808b2be
Simon <spjmurray@yahoo.co.uk>
2016-10-17 11:46:30 +0100

    [Icinga] Fix Xenial and RedHat (#3023)
    Xenial is constantly applying as libsys-syslog-perl is aliased to libperl5.22.
    RedHat doesn't have any of the packages defined in the module hiera, so move
    this to Debian variants only.

--- modules/dc_icinga2/manifests/params.pp
+++ modules/dc_icinga2/manifests/params.pp
@@ -6,10 +6,17 @@ class dc_icinga2::params {
 
   case $::osfamily {
     'Debian': {
-      $pagerduty_deps = [
-        'libsys-syslog-perl',
-        'libwww-perl',
-      ]
+      if versioncmp($::operatingsystemrelease, '16.04') >= 0 {
+        $pagerduty_deps = [
+          'libperl5.22',
+          'libwww-perl',
+        ]
+      } else {
+        $pagerduty_deps = [
+          'libsys-syslog-perl',
+          'libwww-perl',
+        ]
+      }
       $user = 'nagios'
       $sudo_paths = '/usr/lib/nagios/plugins/*, /usr/local/lib/nagios/plugins/*, /usr/bin/cciss_vol_status'
     }
