0c80734c716aeb7099ee3a838e341e18421ddcfc
Simon <spjmurray@yahoo.co.uk>
2016-11-10 13:37:18 +0000

    [Graphite] Purge References (#3070)
    Removes references to collectd and graphite from hiera and profiles.  Also kills off
    dashing as it'll not work without graphite, it's not used and things have moved on
    to such an extent that dashing as we knew it isn't maintained

--- modules/dc_dashing/files/jobs/openstack.rb
+++
@@ -1,9 +0,0 @@
-data_source = DataProvider.new
-SCHEDULER.every '30s', :first_in => 0 do |job|
-  numberOfVCPUsUsed = data_source.get_total_vcpus_used
-  runningVMs     = data_source.get_total_number_of_running_vms
-  memoryUsage    = data_source.get_total_memory_usage
-  send_event('number_vcpus_used', {current: numberOfVCPUsUsed})
-  send_event('ram-hypervisor', {current: convert_num(memoryUsage)})
-  send_event('running_vms-hypervisor', {current: runningVMs})
-end
--- modules/dc_facter/lib/facter/collectd_hostname.rb
+++
@@ -1,5 +0,0 @@
-Facter.add(:collectd_hostname) do
-    setcode do
-        collectd_hostname = %x{hostname -f}.strip.split('.').reverse.join('.')
-    end
-end
