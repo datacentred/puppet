0021bccf88d89654a1366396db3cb654fd314505
Nick Jones <nick.jones@datacentred.co.uk>
2016-11-29 11:12:04 +0000

    [Neutron] Remove L3-HA hacks (#3115)
    These are hacks that were monkey-patched into place in order to
    facillitate migration of standard routers to HA via the Neutron API.
    
    These hacks are no longer required as all routers have been transitioned
    to HA.
    
    Implements PD-2748.

--- modules/dc_profile/manifests/openstack/neutron/server.pp
+++ modules/dc_profile/manifests/openstack/neutron/server.pp
@@ -18,17 +18,9 @@ class dc_profile::openstack::neutron::server {
   include ::neutron::quota
   include ::dc_icinga::hostgroup_neutron_server
 
-  case $::operatingsystem {
-    'Ubuntu': {
-      $packages = ['python-neutron-vpnaas', 'python-neutron-lbaas']
-      $pydir = 'dist-packages'
-    }
-    'RedHat', 'CentOS': {
-      $packages = ['openstack-neutron-fwaas', 'openstack-neutron-lbaas', 'openstack-neutron-vpnaas']
-      $pydir = 'site-packages'
-    }
-    default: {}
-  }
+  # Workaround for upstream packaging bugs and missing dependancies, such as:
+  # https://bugs.launchpad.net/ubuntu/+source/neutron-lbaas/+bug/1460228
+  ensure_packages(['python-neutron-vpnaas', 'python-neutron-lbaas'])
 
   # TODO: Remove post-upgrade
   file_line { 'neutron_auth_version':
@@ -36,48 +28,21 @@ class dc_profile::openstack::neutron::server {
     path    => '/etc/neutron/neutron.conf',
     line    => 'auth_version=V2.0',
     notify  => Service['neutron-server'],
-    require => Package[$packages],
+    require => Package['python-neutron-vpnaas'],
   }
-
-  # Workaround for upstream packaging bugs and missing dependancies, such as:
-  # https://bugs.launchpad.net/ubuntu/+source/neutron-lbaas/+bug/1460228
-  ensure_packages($packages)
 
   file { '/etc/neutron/neutron_lbaas.conf':
     mode    => '0644',
     owner   => 'neutron',
     content => "[service_providers]\nservice_provider=LOADBALANCER:Haproxy:neutron_lbaas.services.loadbalancer.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver:default",
-    require => Package[$packages],
+    require => Package['python-neutron-vpnaas'],
   }
 
   file { '/etc/neutron/neutron_vpnaas.conf':
     mode    => '0644',
     owner   => 'neutron',
     content => "[service_providers]\nservice_provider=VPN:openswan:neutron_vpnaas.services.vpn.service_drivers.ipsec.IPsecVPNDriver:default",
-    require => Package[$packages],
-  }
-
-  # TODO: Remove once we've upgraded to Liberty
-  # This patch enables migration of legacy routers to L3-HA via router-update
-  # See https://bugs.launchpad.net/neutron/+bug/1365426
-  file {"/usr/lib/python2.7/${pydir}/neutron/db/l3_hamode_db.py":
-    mode    => '0644',
-    owner   => 'neutron',
-    group   => 'neutron',
-    source  => 'puppet:///modules/dc_openstack/l3_hamode_db.py',
-    notify  => Service['neutron-server'],
-    require => Package[$packages],
-  }
-
-  # TODO: Remove once we've upgraded to Liberty
-  # As above
-  file {"/usr/lib/python2.7/${pydir}/neutron/extensions/l3_ext_ha_mode.py":
-    mode    => '0644',
-    owner   => 'neutron',
-    group   => 'neutron',
-    source  => 'puppet:///modules/dc_openstack/l3_ext_ha_mode.py',
-    notify  => Service['neutron-server'],
-    require => Package[$packages],
+    require => Package['python-neutron-vpnaas'],
   }
 
 }
