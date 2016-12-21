a8338e4437c8a7fcf762ff8b4f640d5101ad1b92
Joel Merrick <joel.merrick@datacentred.co.uk>
2016-12-21 16:16:08 +0000

    [All] Enable Performance Mode This change sets the CPU frequency scaling governor to 'performance' due to systems with intel_pstate defaulting to 'powersave'
    This change also raises the technodrome and implements PD-2818

---
+++ modules/dc_profile/manifests/util/cpu_performance.pp
@@ -0,0 +1,32 @@
+# Class: dc_profile::util::cpu_performance
+#
+# Sets the cpu governor to 'performance' 
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::util::cpu_performance {
+
+  ensure_packages('cpufrequtils')
+
+  file { '/etc/default/cpufrequtils':
+    content => 'GOVERNOR="performance"',
+  }
+
+  service { 'cpufrequtils':
+    ensure  => running,
+    require => [File['/etc/default/cpufrequtils'],Package['cpufrequtils']],
+  }
+
+  service { 'ondemand':
+    ensure => stopped,
+  }
+
+  File['/etc/default/cpufrequtils'] ~> Service['cpufrequtils']
+
+}
