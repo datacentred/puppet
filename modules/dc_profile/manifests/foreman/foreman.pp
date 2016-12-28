3ff8a0e2b840931eb20c6ce8a81e8d14f85383e0
Simon <spjmurray@yahoo.co.uk>
2016-12-28 11:20:27 +0000

    [Foreman] Initialize The Database (#3174)
    When we spin up a virgin environment there is no code to migrate and seed the database
    because we aren't doing it in a way that RedHat care for.  Instead add in some code to
    initialise the database which slips in between foreman being configured and apache being
    restarted.

--- modules/dc_profile/manifests/foreman/foreman.pp
+++ modules/dc_profile/manifests/foreman/foreman.pp
@@ -25,4 +25,28 @@ class dc_profile::foreman::foreman {
   include ::dc_icinga::hostgroup_https
   include ::dc_icinga::hostgroup_foreman
 
+  # SM: bit of a hack
+  # The foreman class only populates the database if it is locally managed
+  # by the foreman class, e.g. our HA solution is unsupported.  We instead
+  # do this by hand.  Sadly it'll get refreshed if the configuration updates
+  # but should be non-destructive
+  Class['::foreman::config'] ~>
+
+  exec { '/usr/sbin/foreman-rake db:migrate':
+    user        => 'foreman',
+    refreshonly => true,
+  } ~>
+
+  exec { '/usr/sbin/foreman-rake db:seed':
+    user        => 'foreman',
+    refreshonly => true,
+  } ~>
+
+  exec { '/usr/sbin/foreman-rake apipie:cache:index':
+    user        => 'foreman',
+    refreshonly => true,
+  } ~>
+
+  Class['::foreman::service']
+
 }
