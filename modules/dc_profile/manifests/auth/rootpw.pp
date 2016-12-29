fc1db8197d69d3a8d5cf12b277ae32610a383070
Simon <spjmurray@yahoo.co.uk>
2016-12-29 15:11:26 +0000

    [SSH] Allow Root Login For Staging Bootstrap (#3187)
    We need to log in as root to poke about when provisioning.  However as soon as
    our code gets run you get locked out.  This adds in the ability to allow root
    logins during this process.  Standard security will be applied once the ENC is
    fully in control.

--- modules/dc_profile/manifests/auth/rootpw.pp
+++ modules/dc_profile/manifests/auth/rootpw.pp
@@ -12,8 +12,21 @@
 #
 class dc_profile::auth::rootpw {
 
-  user { 'root':
-    password => '!',
+  # ********************************************************************
+  # IMPORTANT SECURITY INFORMATION
+  # ********************************************************************
+  # Bootstrapping staging requires password root login via SSH
+  # if this fact is set then allow a relaxed configuration.  Once
+  # bootstrapping is complete hosts configured like this will
+  # converge to the default security settings.  Root login MUST
+  # be via an in-memory one time password to minimise the risks
+  # to an absolute minimum.
+  # ********************************************************************
+
+  if ! $::staging_bootstrap {
+    user { 'root':
+      password => '!',
+    }
   }
 
 }
--- modules/dc_ssh/manifests/init.pp
+++ modules/dc_ssh/manifests/init.pp
@@ -21,7 +21,42 @@ class dc_ssh {
 
   include dc_ssh::params
 
-  $merged_config = hiera_hash(dc_ssh::config)
+  # ********************************************************************
+  # IMPORTANT SECURITY INFORMATION
+  # ********************************************************************
+  # Bootstrapping staging requires password root login via SSH
+  # if this fact is set then allow a relaxed configuration.  Once
+  # bootstrapping is complete hosts configured like this will
+  # converge to the default security settings.  Root login MUST
+  # be via an in-memory one time password to minimise the risks
+  # to an absolute minimum.
+  # ********************************************************************
+
+  if $::staging_bootstrap {
+    $_bootstrap_configuration = {
+      'PermitRootLogin'        => {
+        'value' => 'yes',
+      },
+      'PasswordAuthentication' => {
+        'value' => 'yes',
+      },
+      # Public key will be tried first, so we need at least 2 tries when
+      # logging in manually for debugging purposes
+      'MaxAuthTries'           => {
+        'value' => '2',
+      },
+      # stdlib's deep_merge doesn't cater for array merging so just disable
+      # this option for now
+      'AllowGroups'            => {
+        'ensure' => 'absent',
+      },
+    }
+  } else {
+    $_bootstrap_configuration = {}
+  }
+
+  $_merged_config = hiera_hash(dc_ssh::config)
+  $merged_config = merge($_merged_config, $_bootstrap_configuration)
 
   create_resources('sshd_config', $merged_config)
 
