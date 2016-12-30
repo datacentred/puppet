477dff9de8f38ac2645f23a07386aad29802322f
Nick Jones <nick.jones@datacentred.co.uk>
2016-12-30 09:58:40 +0000

    [Dell] Fix for missing permissions on racadm (#3191)
    Dell ship a package without permissions correctly set for the 'racadm'
    executable, which in turn results in a broken Puppet provider.
    
    Fix this, and remove superfluous / patronising comments.

--- modules/dc_bmc/manifests/dell/idrac.pp
+++ modules/dc_bmc/manifests/dell/idrac.pp
@@ -1,27 +1,22 @@
-# Class: dc_bmc::dell::idrac
+# == Class: dc_bmc::dell::idrac
 #
-# Configure LDAP authentication and hostname on DELL iDRAC
-#
-# Parameters:
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
-# [Remember: No empty lines between comments and class definition]
+# Configure LDAP authentication and hostname on Dell iDRAC
 class dc_bmc::dell::idrac {
 
-  # Install racadm and start dataeng service
   include ::dc_bmc::dell::racadm
-  # Configure racadm network
   include ::dc_bmc::dell::network
-  # Configure LDAP auth
   include ::dc_bmc::dell::ldap
 
   Class['::dc_bmc::dell::racadm'] ->
   Class['::dc_bmc::dell::network'] ->
   Class['::dc_bmc::dell::ldap']
+
+  # Workaround for Dell's broken package
+  file { '/opt/dell/srvadmin/sbin/racadm-wrapper-idrac7':
+    owner => 'root',
+    group => 'root',
+    mode  => '0755',
+  } ->
+  Drac_setting <||>
 
 }
