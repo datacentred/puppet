1385b50b5827bb32c5d4652ee3568e1abdb63a9d
Peter Tar <peter.tar@datacentred.co.uk>
2017-02-22 14:51:18 +0000

    Custom filter
    Adds a custom filter to the nova scheduler to use instead of the
    currently not working AggregateImagePropertiesIsolation filter

--- modules/dc_profile/manifests/openstack/glance_meta.pp
+++ modules/dc_profile/manifests/openstack/glance_meta.pp
@@ -1,10 +1,8 @@
 # == Class: dc_profile::glance_meta
 #
 # Limits the editability of a certain image property only to users with administrator privileges
-#
+# and adds a custom filter to nova scheduler
 class dc_profile::openstack::glance_meta {
-
-  #include glance::config
 
   file { '/usr/local/property_pr_rules':
     ensure  => present,
@@ -13,6 +11,11 @@ class dc_profile::openstack::glance_meta {
 
   glance_api_config { 'DEFAULT/property_protection_file' :
   value => '/usr/local/property_pr_rules',
+  }
+
+  file { '/usr/lib/python2.7/dist-packages/nova/scheduler/filters/aggregate_property_fixed.py':
+    ensure  => present,
+    content => file('dc_profile/aggregate_property_fixed.py'),
   }
 
 }
