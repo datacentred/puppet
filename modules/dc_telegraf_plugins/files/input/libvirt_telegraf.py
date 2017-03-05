7fb1726d11ed745fff9c6284edddc0b7b24a6710
Joel Merrick <joelio@users.noreply.github.com>
2017-03-05 17:57:38 +0000

    [Monitoring] Add block device statistics (#3265)
    Refactored some of the functions in order to support other types of metric
    Added an enumerate block device function and return block device metrics for each dev
    Dropped the integer type casting
    
    Addresses (in part) PD-2915

--- modules/dc_telegraf_plugins/files/input/libvirt_telegraf.py
+++ modules/dc_telegraf_plugins/files/input/libvirt_telegraf.py
@@ -20,29 +20,56 @@ def setup_connection():
         get_stats(conn)
 
 
-def write_telegraf_line(data, virt_machine):
+def write_telegraf_line(data, virtm, series_type, device=None):
     """ Takes a dict and formats and prints the output in telegraf format """
-    line = ','.join("{}={}i".format(key, val) for key, val in data.items())
-    print 'libvirt,instance={} {}'.format(virt_machine.name(), line)
+    line = ','.join("{}={}".format(key, val) for key, val in data.items())
+    if series_type == 'block':
+        print 'libvirt_{},instance={},device={} {}'.format(series_type, virtm.name(), device, line)
+    else:
+        print 'libvirt_{},instance={} {}'.format(series_type, virtm.name(), line)
 
 
-def get_network_stats(virt_machine):
+def enumerate_block_devices(virtm):
+    """ Enumerates the block devices present on an instance and returns an array """
+    tree = ElementTree.fromstring(virtm.XMLDesc(0))
+    #The list of block device names.
+    devices = []
+    #Iterate through all disk target elements of the domain.
+    for target in tree.findall("devices/disk/target"):
+        #Get the device name.
+        dev = target.get("dev")
+        #Check if we have already found the device name for this domain.
+        if not dev in devices:
+            devices.append(dev)
+    #Completed device name list.
+    return devices
+
+
+def get_block_stats(virtm, device):
+    """ Collect block device statistics """
+    labels = ['read_requests', 'read_bytes', 'write_requests', 'write_bytes', 'errors']
+    return dict(zip(labels, virtm.blockStats(device)))
+
+
+def get_network_stats(virtm):
     """ Collect network statistics """
-    tree = ElementTree.fromstring(virt_machine.XMLDesc())
+    tree = ElementTree.fromstring(virtm.XMLDesc())
     iface = tree.find('devices/interface/target').get('dev')
     labels = [
         'read_bytes', 'read_packets', 'read_errors', 'read_drops',
         'write_bytes', 'write_packets', 'write_errors', 'write_drops'
     ]
-    return dict(zip(labels, virt_machine.interfaceStats(iface)))
+    return dict(zip(labels, virtm.interfaceStats(iface)))
 
 
 def get_stats(conn):
     """ Grabs list of domains and iterates over each one to gather stats """
     domains = conn.listDomainsID()
     for dom in domains:
-        virt_machine = conn.lookupByID(dom)
-        write_telegraf_line(get_network_stats(virt_machine), virt_machine)
+        virtm = conn.lookupByID(dom)
+        write_telegraf_line(get_network_stats(virtm), virtm, 'network')
+        for device in enumerate_block_devices(virtm):
+            write_telegraf_line(get_block_stats(virtm, device), virtm, 'block', device)
     conn.close()
     exit(0)
 
