316a02427a7705226c6957b6e5c26a81047f6795
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-03-13 12:08:30 +0000

    Heat in a custom domain + trusts + cfn: (#3263)
    * remove Keystone v2 related stuff;
     * let Puppet to create and manage the custom heat domain with
       an admin user;
     * disable Heat CFN api.
    
    These changes only affect Heat and have been tested in Vagrant but
    DO NOT MERGE - I'll stage them on one of the controllers.

--- modules/dc_profile/manifests/openstack/heat.pp
+++ modules/dc_profile/manifests/openstack/heat.pp
@@ -13,45 +13,19 @@
 class dc_profile::openstack::heat {
 
   include ::heat
+  include ::heat::keystone::auth
+  include ::heat::keystone::auth_cfn
   include ::heat::engine
   include ::heat::api
   include ::heat::api_cfn
-
-  # Install Keystone V2 plugin
-  # Sourced from: https://github.com/openstack/heat/tree/master/contrib/rackspace/heat_keystoneclient_v2
-  # TODO: Remove once we're on Keystone V3
-  file { [ '/usr/lib/heat', '/usr/lib/heat/heat_keystoneclient_v2' ]:
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
-
-  file { 'heat_keystone_v2_client.py':
-    source  => 'puppet:///modules/dc_openstack/heat_client.py',
-    path    => '/usr/lib/heat/heat_keystoneclient_v2/client.py',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => File['/usr/lib/heat/heat_keystoneclient_v2'],
-  }
-
-  file { 'heat_keystone_v2_client_init.py':
-    source  => 'puppet:///modules/dc_openstack/heat_init.py',
-    path    => '/usr/lib/heat/heat_keystoneclient_v2/__init__.py',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => File['/usr/lib/heat/heat_keystoneclient_v2'],
-    notify  => Service['heat-engine'],
-  }
+  include ::heat::keystone::domain
 
   # Enable 'preview' Stack Adopt and Abandon features
   heat_config {
     'DEFAULT/stack_abandon'          : value => true;
     'DEFAULT/stack_adopt'            : value => true;
-    'DEFAULT/keystone_backend'       : value => 'heat.engine.plugins.heat_keystoneclient_v2.client.KeystoneClientV2';
-    'keystone_authtoken/auth_version': value => '2.0';
+    'DEFAULT/keystone_backend'       : value => 'heat.common.heat_keystoneclient.KeystoneClientV3';
+    'keystone_authtoken/auth_version': value => '3';
   }
 
 }
