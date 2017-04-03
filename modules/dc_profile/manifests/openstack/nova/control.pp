275bf2b401b0534e6d4fe31a93b405078dfed0b6
Nick Jones <nick.jones@datacentred.co.uk>
2017-04-03 13:15:44 +0100

    [Nova] Deploy Liberty release in a Docker container (#3333)
    This commit upgrades the version of Nova's control components to the
    Liberty edition and switches their running to within a Docker container.
    
    Part of PD-2797.

--- modules/dc_profile/manifests/openstack/nova/control.pp
+++ modules/dc_profile/manifests/openstack/nova/control.pp
@@ -12,36 +12,12 @@
 #
 class dc_profile::openstack::nova::control {
 
-  include ::nova
-  include ::nova::api
-  include ::nova::network::neutron
-  include ::nova::cert
-  include ::nova::conductor
-  include ::nova::consoleauth
-  include ::nova::scheduler
-  include ::nova::scheduler::filter
-  include ::nova::vncproxy
   include ::dc_icinga::hostgroup_nova_server
 
-  nova_config { 'DEFAULT/restrict_isolated_hosts_to_isolated_images':
-    value => true,
-  }
+  $containers = hiera('containers')
 
-  # TODO: Use the actual ::nova::api parameter once we've upgraded puppet-nova
-  nova_config { 'DEFAULT/secure_proxy_ssl_header':
-    value => 'X-Forwarded-Proto',
-  }
-
-  nova_config { 'DEFAULT/osapi_compute_link_prefix':
-    value => 'https://compute.datacentred.io:8774/',
-  }
-
-  file { '/usr/lib/python2.7/dist-packages/nova/scheduler/filters/aggregate_image_properties_isolation_dc.py':
-    ensure  => present,
-    content => file('dc_openstack/aggregate_image_properties_isolation_dc.py'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+  dc_docker::run { 'nova':
+    * => $containers['nova'],
   }
 
 }
