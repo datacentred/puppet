dd54b636cd662d28096fffed57e221dd1bddd85d
Simon <spjmurray@yahoo.co.uk>
2017-04-27 21:57:57 +0100

    [Nova] Non Dick Apparmor Configuration (#3392)
    Leaves the professionally created and maintained file contents in place and
    just adds in the line we require, which supports poking the qemu socket to
    notify VMs of things like impending sleeps and migrations so it may quiesce
    file systems safely.

--- modules/dc_profile/manifests/openstack/nova/apparmor.pp
+++ modules/dc_profile/manifests/openstack/nova/apparmor.pp
@@ -2,23 +2,19 @@
 #
 # OpenStack apparmor configuration for libvirt
 #
-# Parameters:
-#
-# Actions:
-#
-#
-# Sample Usage:
-#
 class dc_profile::openstack::nova::apparmor {
 
-  # Install modified libvirt-qemu configuration for apparmor
-  file { 'libvirt-qemu-apparmor':
-    path   => '/etc/apparmor.d/abstractions/libvirt-qemu',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/dc_openstack/libvirt-qemu',
-    notify => [ Service['libvirt'], Service['nova-compute'], Service['apparmor'] ],
-  }
+  $interested_parties = [
+    'libvirt',
+    'nova-compute',
+    'apparmor',
+  ]
+
+  file_line { 'libvirt-qemu-apparmor':
+    path => '/etc/apparmor.d/abstractions/libvirt-qemu',
+    line => '/var/lib/libvirt/qemu/*.sock rw,',
+  } ~>
+
+  Service[$interested_parties]
 
 }
