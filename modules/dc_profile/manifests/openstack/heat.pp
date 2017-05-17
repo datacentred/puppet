4a3648677aa1c9f84d732df1e21a97057a9386e9
Nick Jones <nick.jones@datacentred.co.uk>
2017-05-17 15:10:06 +0100

    [Heat] Temporarily disable Heat-related services (#3425)
    This will be re-enabled once an upgrade path has been established.

--- modules/dc_profile/manifests/openstack/heat.pp
+++ modules/dc_profile/manifests/openstack/heat.pp
@@ -1,31 +1,9 @@
-# Class: dc_profile::openstack::heat_db
-#
-# Openstack image API database definitions
-#
-# Parameters:
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
+# == Class: dc_profile::openstack::heat_db
 #
 class dc_profile::openstack::heat {
 
-  include ::heat
-  include ::heat::keystone::auth
-  include ::heat::keystone::auth_cfn
-  include ::heat::engine
-  include ::heat::api
-  include ::heat::api_cfn
-  include ::heat::keystone::domain
-
-  # Enable 'preview' Stack Adopt and Abandon features
-  heat_config {
-    'DEFAULT/stack_abandon'          : value => true;
-    'DEFAULT/stack_adopt'            : value => true;
-    'DEFAULT/keystone_backend'       : value => 'heat.common.heat_keystoneclient.KeystoneClientV3';
-    'keystone_authtoken/auth_version': value => '3';
+  service { [ 'heat-api', 'heat-api-cfn', 'heat-engine' ]:
+    ensure =>  stopped,
   }
 
 }
