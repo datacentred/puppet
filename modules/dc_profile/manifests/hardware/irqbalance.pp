dc13e38d0c2411da8fca55cb5f6a29d0a230a3af
Nick Jones <nick.jones@datacentred.co.uk>
2017-05-31 11:18:51 +0100

    Manage 'irqbalance' service (#3453)
    This profile class is missing the irqbalance service resource
    definition, thus causing Puppet catalogue compilation errors.

--- modules/dc_profile/manifests/hardware/irqbalance.pp
+++ modules/dc_profile/manifests/hardware/irqbalance.pp
@@ -1,9 +1,18 @@
 #
+# == Class: dc_profile::hardware::irqbalance
+#
+# Fix for bug present in irqbalance that only manifests itself in X9
+# and X10-based SuperMicro nodes
+# See https://bugs.launchpad.net/ubuntu/+source/irqbalance/+bug/1321425
+#
 class dc_profile::hardware::irqbalance {
 
-  # Fix for bug present in irqbalance that only manifests itself in X9
-  # and X10-based SuperMicro nodes
-  # See https://bugs.launchpad.net/ubuntu/+source/irqbalance/+bug/1321425
+  service { 'irqbalance':
+    ensure    => running,
+    hasstatus => true,
+    enable    => true,
+  }
+
   file_line { 'irqbalance':
     path   => '/etc/default/irqbalance',
     line   => 'OPTIONS="--hintpolicy=ignore"',
