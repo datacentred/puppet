353f673c934e1281ec2232c2140b2f1562fcde0e
Mariusz Karpiarz <mkarpiarz@users.noreply.github.com>
2017-06-15 14:55:46 +0100

    [Heat] Deploy containerised Heat: (#3478)
    Deploys the Mitaka Heat container.
    Leaves the old services stopped for now, in case we need to revert.
    
    PD-2659

--- modules/dc_profile/manifests/openstack/heat.pp
+++ modules/dc_profile/manifests/openstack/heat.pp
@@ -1,6 +1,11 @@
-# == Class: dc_profile::openstack::heat_db
+# == Class: dc_profile::openstack::heat
 #
 class dc_profile::openstack::heat {
+
+  $containers = hiera('containers')
+
+  dc_docker::run { 'heat':
+    * => $containers['heat']
 
   service { [ 'heat-api', 'heat-api-cfn', 'heat-engine' ]:
     ensure =>  stopped,
