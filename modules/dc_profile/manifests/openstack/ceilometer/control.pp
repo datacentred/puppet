98626fc525a35e2a74329f3f652de41be0485cfe
Mariusz Karpiarz <mkarpiarz@users.noreply.github.com>
2017-06-22 11:20:00 +0100

    Revert back to Kilo Ceilometer: (#3502)
    This gets us back to using the Kilo version of Ceilometer.
    
    DON'T MERGE, I'll go through the control nodes one by one.
    
    PD-3136

--- modules/dc_profile/manifests/openstack/ceilometer/control.pp
+++ modules/dc_profile/manifests/openstack/ceilometer/control.pp
@@ -2,12 +2,46 @@
 #
 # Main Ceilometer components for deployment on a cloud control node
 #
+# Parameters:
+#
+# Actions:
+#
+# Requires: StackForge Ceilometer
+#
+# Sample Usage:
+#
 class dc_profile::openstack::ceilometer::control {
 
-  $containers = hiera('containers')
+  include ::ceilometer
+  include ::ceilometer::api
+  include ::ceilometer::wsgi::apache
+  include ::ceilometer::agent::auth
+  include ::ceilometer::agent::central
+  include ::ceilometer::agent::notification
+  include ::ceilometer::collector
+  include ::ceilometer::expirer
+  include ::ceilometer::db
+  include ::ceilometer::alarm::evaluator
+  include ::ceilometer::alarm::notifier
 
-  dc_docker::run { 'telemetry':
-    * => $containers['telemetry']
+  ceilometer_config { 'database/use_tpool':
+    value => true,
+  }
+
+  file { '/etc/ceilometer/event_definitions.yaml':
+    source  => 'puppet:///modules/dc_openstack/event_definitions.yaml',
+    owner   => 'ceilometer',
+    group   => 'ceilometer',
+    require => Class['::ceilometer'],
+    notify  => Service['ceilometer-collector', 'ceilometer-agent-notification', 'ceilometer-api'],
+  }
+
+  file { '/etc/ceilometer/event_pipeline.yaml':
+    source  => 'puppet:///modules/dc_openstack/event_pipeline.yaml',
+    owner   => 'ceilometer',
+    group   => 'ceilometer',
+    require => Class['::ceilometer'],
+    notify  => Service['ceilometer-collector', 'ceilometer-agent-notification', 'ceilometer-api'],
   }
 
 }
