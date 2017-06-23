ac6281e07acc18ccb37fc5844c024eb47d91eae6
Peter Tar <peter.tar@datacentred.co.uk>
2017-06-23 16:18:28 +0100

    [Logging] Add GeoIp coordinates as a field in ELK (#3510)
    Adding geoip coordinates to the ips returned from the fail2ban logs in
    order to create a geographical visualisation in Kibana. Also adds a cron
    job that periodically updates the geoip database.

--- modules/dc_profile/manifests/log/logstash.pp
+++ modules/dc_profile/manifests/log/logstash.pp
@@ -17,4 +17,21 @@ class dc_profile::log::logstash {
 
   include ::dc_icinga::hostgroup_http
 
+  cron { 'geoip_data_update':
+    command  => '/usr/bin/geoipupdate',
+    user     => 'root',
+    monthday => 15,
+  }
+
+  ensure_packages('geoipupdate')
+
+  Package['geoipupdate'] ->
+
+  file { '/etc/GeoIP.conf':
+    ensure => present,
+    source => 'puppet:///modules/dc_profile/GeoIP.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
 }
