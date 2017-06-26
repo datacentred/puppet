b460765cacf97b7b24d909719c4f88f33b4cb26b
Joel Merrick <joelio@users.noreply.github.com>
2017-06-26 12:19:39 +0100

    [Logging] Set correct port for beats (#3512)
    No idea, really.. no idea. This is the correct port anyway.

--- modules/dc_logstash/manifests/params.pp
+++ modules/dc_logstash/manifests/params.pp
@@ -14,7 +14,7 @@ class dc_logstash::params {
   # Generic options
   $grok_patterns_dir = '/etc/logstash/patterns/grok'
 
-  $beats_port = '5504'
+  $beats_port = '5044'
 
   # Riemann options
   $riemann_plugin_version = '2.0.5'
