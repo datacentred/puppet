835347d9ed580c352dc73dd8e8a50ea8e787581e
Joel Merrick <joelio@users.noreply.github.com>
2017-07-03 12:53:39 +0100

    [General] Reduce irqbalance spam (#3530)
    Enables the `dc_profile::hardware::irqbalance` class across the estate to reduce log-spam.
    
    Addresses PD-3169

--- modules/dc_profile/manifests/hardware/irqbalance.pp
+++ modules/dc_profile/manifests/hardware/irqbalance.pp
@@ -1,8 +1,6 @@
 #
 # == Class: dc_profile::hardware::irqbalance
 #
-# Fix for bug present in irqbalance that only manifests itself in X9
-# and X10-based SuperMicro nodes
 # See https://bugs.launchpad.net/ubuntu/+source/irqbalance/+bug/1321425
 #
 class dc_profile::hardware::irqbalance {
