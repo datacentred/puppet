95ea9a9452fcea6a0b9ba4d6d400f0a66768d5fe
Joel Merrick <joelio@users.noreply.github.com>
2017-07-13 15:35:49 +0100

    [Galera] Xtrabackup Rollout (#3563)
    This adds Percona's xtrabackup to run at the default (1AM). Also cleans up some of the existing code. This will be ran in conjunction with the legacy mysqldump solution until confident the sytems (backups and restores) work properly and more importantly, don't impact the resource contention as they currently do at 12AM.
    
    Addresses PD-3192

--- modules/dc_profile/manifests/openstack/backups/galera.pp
+++ modules/dc_profile/manifests/openstack/backups/galera.pp
@@ -4,29 +4,30 @@
 #
 class dc_profile::openstack::backups::galera {
 
-  $osdbmq_galera_backup_user = hiera(osdbmq_galera_backup_user)
-  $osdbmq_galera_backup_pw = hiera(osdbmq_galera_backup_pw)
-  $backupuser     = hiera(osdbmq_galera_backup_user)
-  $backuppassword = hiera(osdbmq_galera_backup_pw)
+  $_backup_user = hiera(osdbmq_galera_backup_user)
+  $_backup_pw   = hiera(osdbmq_galera_backup_pw)
+  $_backup_dir  = hiera(osdbmq_galera_backup_dir)
 
-  dc_backup::dc_duplicity_job { "${::fqdn}_galera" :
-    source_dir     => '/var/local/backup',
+  include ::xtrabackup
+
+  dc_backup::dc_duplicity_job { "${::fqdn}_galera":
+    source_dir     => $_backup_dir,
     backup_content => 'galera',
   }
 
-  mysql_user { "${backupuser}@localhost":
+  mysql_user { "${_backup_user}@localhost":
     ensure        => present,
-    password_hash => mysql_password($backuppassword),
+    password_hash => mysql_password($_backup_pw),
     provider      => 'mysql',
     require       => Class['mysql::server::root_password'],
   }
 
-  mysql_grant { "${backupuser}@localhost/*.*":
+  mysql_grant { "${_backup_user}@localhost/*.*":
     ensure     => present,
-    user       => "${backupuser}@localhost",
+    user       => "${_backup_user}@localhost",
     table      => '*.*',
     privileges => [ 'SELECT', 'RELOAD', 'LOCK TABLES', 'SHOW VIEW' ],
-    require    => Mysql_user["${backupuser}@localhost"],
+    require    => Mysql_user["${_backup_user}@localhost"],
   }
 
   cron { "${::fqdn}_galera_dump_for_duplicity":
@@ -38,19 +39,19 @@ class dc_profile::openstack::backups::galera {
     minute   => '0',
   }
 
-  file { '/var/local/backup':
+  file { $_backup_dir:
     ensure => directory,
     mode   => '0700',
   }
 
   file { '/usr/local/sbin/galera_dump_for_duplicity.sh':
     ensure  => file,
-    content => "#!/bin/bash\n/usr/bin/mysqldump -u ${osdbmq_galera_backup_user} -p${osdbmq_galera_backup_pw} --opt --all-databases | bzcat -zc > /var/local/backup/galera-`date +%Y%m%d-%H%M%S`.sql.bz2",
+    content => "#!/bin/bash\n/usr/bin/mysqldump -u ${_backup_user} -p${_backup_pw} --opt --all-databases | bzcat -zc > /var/local/backup/galera-`date +%Y%m%d-%H%M%S`.sql.bz2", # lint:ignore:140chars
     mode    => '0700',
   }
 
   tidy { 'galera_dumps':
-    path    => '/var/local/backup',
+    path    => $_backup_dir,
     age     => '2D',
     recurse => true,
     rmdirs  => true,
