c96a202b3243969698aa7bb3ef16ffa3e439b69a
Joel Merrick <joel.merrick@datacentred.co.uk>
2017-07-14 13:22:49 +0100

    [Elasticsearch] Add Monit to ensure uptime of service
    Due to recent issues with elasticsearch falling over, I'm taking a belt and braces approach. This change adds the monit upstream module, a profile and a monit check to make sure elasticsearch is always running. We'll get monitoring if it does fall over, but at least it'll kick it back to life.

---
+++ modules/dc_monit/manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: dc_monit
+#
+# Installs and configures monit scanner
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_monit {
+
+  include ::monit
+
+}
---
+++ modules/dc_profile/manifests/mon/monit.pp
@@ -0,0 +1,18 @@
+# Class: dc_profile::mon::monit
+#
+# Sets up monit in order to ensure daemons are kept running  
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class dc_profile::mon::monit {
+
+  include ::dc_monit
+
+}
+
