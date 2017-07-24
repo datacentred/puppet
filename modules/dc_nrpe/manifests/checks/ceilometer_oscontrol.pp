f212b08ac968296b725650c737e43abbfde7bd21
Simon <spjmurray@yahoo.co.uk>
2017-07-24 11:11:04 +0100

    [Icinga] Fix Ceilometer Checks (#3579)
    Usernames aren't relevant in the docker world, so delete them.  The 'central'
    process check isn't even used.

--- modules/dc_nrpe/manifests/checks/ceilometer_oscontrol.pp
+++ modules/dc_nrpe/manifests/checks/ceilometer_oscontrol.pp
@@ -4,17 +4,12 @@ class dc_nrpe::checks::ceilometer_oscontrol {
 
   dc_nrpe::check { 'check_ceilometer_notification_proc':
     path => '/usr/lib/nagios/plugins/check_procs',
-    args => '-c 1: -u ceilometer -a ceilometer-agent-notification',
-  }
-
-  dc_nrpe::check { 'check_ceilometer_central_proc':
-    path => '/usr/lib/nagios/plugins/check_procs',
-    args => '-c 1: -u ceilometer -a ceilometer-agent-central',
+    args => '-c 1: -a ceilometer-agent-notification',
   }
 
   dc_nrpe::check { 'check_ceilometer_collector_proc':
     path => '/usr/lib/nagios/plugins/check_procs',
-    args => '-c 1: -u ceilometer -a ceilometer-collector',
+    args => '-c 1: -a ceilometer-collector',
   }
 
 }
