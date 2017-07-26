b51c5d21c627bde0c6e499e9c4c49f15c3f2a99b
Simon <spjmurray@yahoo.co.uk>
2017-07-26 15:33:23 +0100

    [Nginx] Update Cert on WWW (#3601)
    Our friends in the government have correctly noticed that the apex for datacentred.io
    is CNAMED (and squished into an A record) to datacentred.co.uk.  First thing that
    happens is your browser barfs that the certificate SAN is wrong.  This change adds in
    SANs for all of the domains that point at the server.
    
    I could add in a vhost raising a 301 for all the other domains, but we'd still need
    the multi-domain SSL cert anyway if user X tried to connect to https://datacentred.org
    directly.

--- modules/dc_profile/manifests/util/wordpress/ssl.pp
+++ modules/dc_profile/manifests/util/wordpress/ssl.pp
@@ -2,6 +2,20 @@
 #
 class dc_profile::util::wordpress::ssl {
   letsencrypt::certonly { 'www.datacentred.co.uk':
+    domains              => [
+      'www.datacentred.co.uk',
+      'www.datacentred.io',
+      'www.datacentred.net',
+      'www.datacentred.com',
+      'www.datacentred.org',
+      'www.datacentred.org.uk',
+      'datacentred.co.uk',
+      'datacentred.io',
+      'datacentred.net',
+      'datacentred.com',
+      'datacentred.org',
+      'datacentred.org.uk',
+    ],
     manage_cron          => true,
     cron_before_command  => '/bin/systemctl stop nginx.service',
     cron_success_command => '/bin/systemctl start nginx.service',
