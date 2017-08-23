e8ad3fd20eb4702e91b37b79f8e57555e9e763f0
Peter Tar <peter.tar@datacentred.co.uk>
2017-08-23 13:26:51 +0100

    [Logging] Change fail2ban logrotate rules (#3642)
    Logrotate breaks rsyslog's redirects of the fail2ban logs when it
    rotates the logfile and creates a new one with the wrong owner/group
    settings. Adding custom settings to logrotate for the fail2ban logs
    instead of the default to fix it.

--- modules/dc_profile/manifests/util/fail2ban.pp
+++ modules/dc_profile/manifests/util/fail2ban.pp
@@ -7,4 +7,17 @@ class dc_profile::util::fail2ban {
 
   include ::fail2ban
 
+  logrotate::rule { 'fail2ban':
+    path          => '/var/log/fail2ban.log',
+    rotate        => 4,
+    rotate_every  => 'week',
+    compress      => true,
+    delaycompress => true,
+    create_owner  => 'syslog',
+    create_group  => 'adm',
+    create_mode   => '640',
+    create        => true,
+    missingok     => true,
+    postrotate    => 'fail2ban-client flushlogs 1>/dev/null',
+  }
 }
